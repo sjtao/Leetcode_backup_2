1204e0c8721342600f3199791f1768459ec30425
sjtao <61641368+sjtao@users.noreply.github.com>
2021-10-30 22:55:06 -0400

    Create Contains_Duplicate.cpp

---
+++ 217_Contains Duplicate/Contains_Duplicate.cpp
@@ -0,0 +1,40 @@
+
+//brutal force cause time exceed limits
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return 0;
+        }
+        else{
+            bool dup = 0;
+            int i, j; 
+            for(i = 0; i < n-1; i++){
+                for(j = i + 1; j < n; j++){
+                    if(nums[i] == nums[j]){
+                        dup = 1;
+                        break;
+                    }
+                }
+            }
+            return dup; 
+        }
+};
+
+//Runtime: 39 ms, faster than 48.85% of C++ online submissions for Contains Duplicate.
+//Memory Usage: 15.6 MB, less than 71.64% of C++ online submissions for Contains Duplicate.  
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool dup = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] == nums[i+1]){
+                dup = 1;
+                break;
+            }
+        }
+        return dup;
+    }
+};
