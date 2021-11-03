e64dc1d965783d33ddae4ba2fce050bc8202e39c
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-02 21:57:42 -0400

    Update 88_Merge_Sorted_Array.c

--- 0088_Merge_Sorted_Array/88_Merge_Sorted_Array.c
+++ 0088_Merge_Sorted_Array/88_Merge_Sorted_Array.c
@@ -18,3 +18,33 @@ public:
         return;
     }
 };
+
+
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Merge Sorted Array.
+//Memory Usage: 9.1 MB, less than 72.74% of C++ online submissions for Merge Sorted Array.
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> nums3;
+        int i = 0, j = 0;
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+                nums3.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                nums3.push_back(nums2[j]);
+                j++;
+            } 
+        }
+        while(i < m){
+            nums3.push_back(nums1[i]);
+            i++;
+        }
+        while(j < n){
+            nums3.push_back(nums2[j]);
+            j++;
+        }
+        nums1 = nums3;
+    }
+};
