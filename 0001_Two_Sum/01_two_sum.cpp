36061e8c2fa3952558dabd92e28adc2d32f33956
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-03 22:22:30 -0400

    Update 01_two_sum.cpp

--- 0001_Two_Sum/01_two_sum.cpp
+++ 0001_Two_Sum/01_two_sum.cpp
@@ -1,6 +1,6 @@
 //Runtime: 744 ms, faster than 6.89% of C++ online submissions for Two Sum.
 //Memory Usage: 10.1 MB, less than 80.30% of C++ online submissions for Two Sum.
-
+//O(N^2)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -18,5 +18,42 @@ public:
             if (find) break;
         }
         return index;
+    }
+};
+
+
+
+//Runtime: 4 ms, faster than 99.54% of C++ online submissions for Two Sum.
+//Memory Usage: 10.5 MB, less than 61.39% of C++ online submissions for Two Sum.
+//O(N)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> s, ind, ans;
+        s = nums;
+     
+        sort(nums.begin(), nums.end());
+        
+        int l = 0;
+        int r = nums.size()-1;
+        while(l <= r){
+            if(nums[l] + nums[r] == target){
+                ind.push_back(nums[l]);
+                ind.push_back(nums[r]);
+                break;
+            }
+            else if(nums[l] + nums[r] > target)
+                r--;
+            else
+                l++;
+        }
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(s[i] == ind[0] || s[i] == ind[1]) {
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
     }
 };
