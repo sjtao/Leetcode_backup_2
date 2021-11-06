a49f2f8e5ed36d1fae925746a787f63dd3285654
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-05 21:27:02 -0400

    Update 260_Single_Number_III.cpp

--- 0260_Single_Number_III/260_Single_Number_III.cpp
+++ 0260_Single_Number_III/260_Single_Number_III.cpp
@@ -27,6 +27,34 @@ public:
     }
 };
 
+//Runtime: 12 ms, faster than 42.79% of C++ online submissions for Single Number III.
+//Memory Usage: 9.8 MB, less than 93.24% of C++ online submissions for Single Number III.
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        if(nums.size() == 2) return nums;
+        
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < nums.size()-1){
+            if(nums[i] != nums[i+1]){
+                ans.push_back(nums[i]);
+                //find += 1;
+                i+=1;
+            }
+            else
+            {
+                i+=2;
+            }
+        }
+ 
+        if (i == nums.size()-1) ans.push_back(nums[i]);
+        return ans;
+    }
+};
+
+
 //Runtime: 8 ms, faster than 85.25% of C++ online submissions for Single Number III.
 //Memory Usage: 9.8 MB, less than 99.94% of C++ online submissions for Single Number III.
 class Solution {
@@ -53,3 +81,4 @@ public:
         return ans;
     }
 };
+
