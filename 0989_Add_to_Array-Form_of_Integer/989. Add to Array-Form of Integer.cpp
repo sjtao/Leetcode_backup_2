94ceb9ddea5b566f44342c5c429af0343dbd93d4
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-07 16:05:07 -0500

    Update 989. Add to Array-Form of Integer.cpp

--- 0989_Add_to_Array-Form_of_Integer/989. Add to Array-Form of Integer.cpp
+++ 0989_Add_to_Array-Form_of_Integer/989. Add to Array-Form of Integer.cpp
@@ -32,3 +32,51 @@ public:
         return num;
     }
 };
+
+
+//Runtime: 24 ms, faster than 85.09% of C++ online submissions for Add to Array-Form of Integer.
+//Memory Usage: 29.4 MB, less than 5.66% of C++ online submissions for Add to Array-Form of Integer.
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int> ans;
+        
+        //save k to a vector
+        while(k > 0){
+            ans.push_back(k % 10);
+            k = k / 10;
+        }
+        reverse(ans.begin(), ans.end());
+        
+        int m = ans.size();
+        int n = num.size();
+        
+        if(m > n){
+            //k is longer then num
+            int d = m - n;
+            vector<int> temp(d, 0);
+            temp.insert(temp.end(), num.begin(), num.end());
+            num = temp;
+        }
+        else if(n > m){
+            int d = n - m;
+            vector<int> temp(d, 0);
+            temp.insert(temp.end(), ans.begin(), ans.end());
+            ans = temp;
+        }
+        
+        int carry = 0;
+        int s;
+        for(int i = ans.size()-1; i >= 0; i--){
+            s = ans[i] + num[i] + carry;
+            ans[i] = s % 10;
+            carry  = s / 10;
+        }
+        
+        if(carry){
+            ans.insert(ans.begin(), carry);
+        }
+        
+        return ans;
+    }
+};
