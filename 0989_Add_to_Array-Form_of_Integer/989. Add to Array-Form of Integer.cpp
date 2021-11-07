5754e38054f281467085550c6746618e92dde78f
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-07 16:14:27 -0500

    Update 989. Add to Array-Form of Integer.cpp

--- 0989_Add_to_Array-Form_of_Integer/989. Add to Array-Form of Integer.cpp
+++ 0989_Add_to_Array-Form_of_Integer/989. Add to Array-Form of Integer.cpp
@@ -80,3 +80,33 @@ public:
         return ans;
     }
 };
+
+//Runtime: 20 ms, faster than 94.43% of C++ online submissions for Add to Array-Form of Integer.
+//Memory Usage: 27.4 MB, less than 84.34% of C++ online submissions for Add to Array-Form of Integer.
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int i = num.size()-1;
+        int carry = 0;
+        int s;
+        while(i >= 0 || k > 0){
+            if(i >= 0){
+                s = num[i] + k % 10 + carry;
+                carry = s / 10;
+                num[i] = s % 10;
+            }
+            else{
+                s = k % 10 + carry;
+                carry = s / 10;
+                num.insert(num.begin(),s % 10);
+            }
+            
+            k /= 10;
+            i--; 
+        }
+        if(carry){
+            num.insert(num.begin(),carry);
+        }
+        return num;
+    }
+};
