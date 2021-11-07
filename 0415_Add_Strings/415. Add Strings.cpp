7a8a65ca1a8d89ca1b26193ca0c4b9f94e7aa53f
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-07 16:25:53 -0500

    Create 415. Add Strings.cpp

---
+++ 0415_Add_Strings/415. Add Strings.cpp
@@ -0,0 +1,45 @@
+//Runtime: 12 ms, faster than 36.89% of C++ online submissions for Add Strings.
+//Memory Usage: 56.1 MB, less than 9.58% of C++ online submissions for Add Strings.
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int n1 = num1.length() - 1;
+        int n2 = num2.length() - 1;
+        string ans = "\0";
+        
+        int carry = 0;
+        int d1, d2, s;
+        char t;
+        while(n1 >= 0 || n2 >= 0){
+            if(n1 < 0){
+                d1 = 0;
+            }
+            else{
+                d1 = num1[n1] - '0';
+            }
+            
+            if(n2 < 0){
+                d2 = 0;
+            }
+            else{
+                d2 = num2[n2] - '0';
+            }
+            
+            s = d1 + d2 + carry;
+            t = s % 10 + '0';
+            ans = t + ans;
+            carry = s / 10;
+            
+            n1 --;
+            n2 --;
+        }
+        
+        if(carry){
+            t = carry + '0';
+            ans = t + ans;
+        }
+         
+        return ans;
+    }
+};
