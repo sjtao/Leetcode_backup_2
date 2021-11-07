26a9fc7a2ed1417f14f025f6c6d26c11a333fc0c
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-06 21:18:20 -0400

    Update 43_MultiplyStrings.cpp

--- 0043_Multiply_Strings/43_MultiplyStrings.cpp
+++ 0043_Multiply_Strings/43_MultiplyStrings.cpp
@@ -16,3 +16,27 @@ public:
         return to_string(prod);
     }
 };
+
+
+//Line 17: Char 33: runtime error: signed integer overflow: 498828660196 * 840477629533 cannot be represented in type 'long long' (solution.cpp)
+//SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior prog_joined.cpp:26:33
+class Solution {
+public:
+    long long int ConvertToInt(string num){
+        long long int s = 0;
+        for(int i = 0; i < num.length(); i++){
+            s = s * 10 + (num[i] - '0');
+        }
+        return s;
+    }
+    string multiply(string num1, string num2) {
+        //convert to integer
+        long long int n1 = ConvertToInt(num1);
+        long long int n2 = ConvertToInt(num2);
+        //product
+        long long int prod = n1 * n2;
+        //convert to string
+        return to_string(prod);
+    }
+};
+
