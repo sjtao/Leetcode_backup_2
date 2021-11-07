884932921b2210888320ae48625afc3841059cb9
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-06 22:22:15 -0400

    Update 67_Add_Binary.cpp

--- 0067_Add_Binary/67_Add_Binary.cpp
+++ 0067_Add_Binary/67_Add_Binary.cpp
@@ -44,3 +44,48 @@ public:
         return ans;
     }
 };
+
+
+//Runtime: 6 ms, faster than 17.81% of C++ online submissions for Add Binary.
+//Memory Usage: 7.1 MB, less than 14.10% of C++ online submissions for Add Binary.
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+        int ai = a.length()-1;
+        int bi = b.length()-1;
+        
+        string c = "\0";
+        char t;
+        int cr = 0;
+        int s  = 0;
+        while(ai >= 0 && bi >= 0){
+            s = a[ai] - '0' + b[bi] - '0' + cr;
+            t = (s%2 + '0');
+            c = t + c;
+            cr = s / 2;
+            ai --;
+            bi --;
+        }
+        
+        while(ai >= 0){
+            s = a[ai] - '0' + cr;
+            t = (s%2 + '0');
+            c = t + c;
+            cr = s / 2;
+            ai --;
+        }
+        
+        while(bi >= 0){
+            s = b[bi] - '0' + cr;
+            t = (s%2 + '0');
+            c = t + c;
+            cr = s / 2;
+            bi --;
+        }
+        
+        if(cr == 1) c = '1' + c;
+         
+        return c;
+    }
+};
