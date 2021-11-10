294b46e3c5a4e3d167cc292faa94311c169b1165
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-09 22:25:50 -0500

    Update 190. Reverse Bits.cpp

--- 0190_Reverse_Bits/190. Reverse Bits.cpp
+++ 0190_Reverse_Bits/190. Reverse Bits.cpp
@@ -10,3 +10,17 @@ public:
         return ans;
     }
 };
+
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Reverse Bits.
+//Memory Usage: 6 MB, less than 49.01% of C++ online submissions for Reverse Bits.
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        for(int i = 0; i < 32; i++){
+            ans += (n & 1) << (31 - i);
+            n = n >> 1;
+        }
+        return ans;
+    }
+};
