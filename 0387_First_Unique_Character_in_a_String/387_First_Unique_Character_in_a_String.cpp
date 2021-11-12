1c1c5c1eceda0a56af6c4455b2e9a880e5091d31
sjtao <61641368+sjtao@users.noreply.github.com>
2021-11-11 23:22:55 -0500

    Create 387_First_Unique_Character_in_a_String.cpp

---
+++ 0387_First_Unique_Character_in_a_String/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,24 @@
+//Runtime: 24 ms, faster than 86.71% of C++ online submissions for First Unique Character in a String.
+//Memory Usage: 10.6 MB, less than 68.97% of C++ online submissions for First Unique Character in a String.
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        if(s.length() == 1) return 0;
+        
+        vector<int> repeat(26, 0);
+        
+        for(int i = 0; i < s.length(); i++){
+            repeat[s[i]-'a'] += 1;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            if(repeat[s[i]-'a'] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
