5c2bed2e69ed5e33658205e74edc0f0b422318c1
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-12 22:06:02 -0500

    Create 205. Isomorphic Strings.cpp

---
+++ 0205_Isomorphic_Strings/205. Isomorphic Strings.cpp
@@ -0,0 +1,22 @@
+//Runtime: 4 ms, faster than 93.79% of C++ online submissions for Isomorphic Strings.
+//Memory Usage: 7 MB, less than 41.62% of C++ online submissions for Isomorphic Strings.
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()==1) return true;
+        //two strings have the same style: *****+++&&&**
+        //number of ascii code: 256(0-255)
+        vector<int> smap(256, 0);
+        vector<int> tmap(256, 0);
+        
+        for(int i = 0; i < s.length(); i++){
+            if(smap[s[i]] != tmap[t[i]])
+                return false;
+            smap[s[i]] = i+1; //at the same position
+            tmap[t[i]] = i+1;
+        }
+        
+        return true;
+    }
+};
