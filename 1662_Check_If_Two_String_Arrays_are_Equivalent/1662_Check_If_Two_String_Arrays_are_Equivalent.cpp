eff4ad7dd394ebb53ab120b34b529736e4510f45
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-14 16:44:11 -0500

    Update 1662_Check_If_Two_String_Arrays_are_Equivalent.cpp

--- 1662_Check_If_Two_String_Arrays_are_Equivalent/1662_Check_If_Two_String_Arrays_are_Equivalent.cpp
+++ 1662_Check_If_Two_String_Arrays_are_Equivalent/1662_Check_If_Two_String_Arrays_are_Equivalent.cpp
@@ -16,3 +16,19 @@ public:
         return (a==b);
     }
 };
+
+//Runtime: 4 ms, faster than 80.47% of C++ online submissions for Check If Two String Arrays are Equivalent.
+//Memory Usage: 11.7 MB, less than 19.89% of C++ online submissions for Check If Two String Arrays are Equivalent.
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string a, b;
+        
+        //Concatenate all strings in the array into a single string in the given order
+        for (string x : word1) a += x;
+        for (string x : word2) b += x;
+        
+        //compare
+        return (a==b);
+    }
+};
