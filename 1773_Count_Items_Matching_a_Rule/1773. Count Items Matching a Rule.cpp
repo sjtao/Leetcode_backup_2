f62d203e5ceb405b5c899f0f31c6855771b02f41
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-14 19:57:36 -0500

    Update 1773. Count Items Matching a Rule.cpp

--- 1773_Count_Items_Matching_a_Rule/1773. Count Items Matching a Rule.cpp
+++ 1773_Count_Items_Matching_a_Rule/1773. Count Items Matching a Rule.cpp
@@ -21,3 +21,23 @@ public:
         return count;
     }
 };
+
+
+//Runtime: 76 ms, faster than 54.72% of C++ online submissions for Count Items Matching a Rule.
+//Memory Usage: 31 MB, less than 40.44% of C++ online submissions for Count Items Matching a Rule.
+
+class Solution {
+private:
+    unordered_map<string, int> mp = {{"type", 0}, {"color", 1}, {"name", 2}};
+
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int count = 0;
+        int index = mp[ruleKey];
+        for(int i = 0; i < items.size(); i++){
+            if(items[i][index] == ruleValue)
+                count += 1;
+        }
+        return count;
+    }
+};
