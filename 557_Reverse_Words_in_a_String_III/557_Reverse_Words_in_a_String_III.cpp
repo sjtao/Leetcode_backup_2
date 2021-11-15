025cf7782ec7f2d1a8d2793340f920222603fe06
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-15 13:42:26 -0500

    Update 557_Reverse_Words_in_a_String_III.cpp

--- 557_Reverse_Words_in_a_String_III/557_Reverse_Words_in_a_String_III.cpp
+++ 557_Reverse_Words_in_a_String_III/557_Reverse_Words_in_a_String_III.cpp
@@ -21,3 +21,22 @@ public:
         return r;
     }
 };
+
+
+//Runtime: 8 ms, faster than 99.18% of C++ online submissions for Reverse Words in a String III.
+//Memory Usage: 9.8 MB, less than 46.11% of C++ online submissions for Reverse Words in a String III.
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int st = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ' '){
+                reverse(s.begin()+st, s.begin()+i);
+                st = i+1;
+            }
+        }
+        reverse(s.begin()+st, s.end());
+        return s;
+    }
+};
