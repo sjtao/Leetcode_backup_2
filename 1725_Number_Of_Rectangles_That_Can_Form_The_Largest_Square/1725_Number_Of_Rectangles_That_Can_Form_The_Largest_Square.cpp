cefe84b0a15ad081f7a1c4f4dc1aa19330d9c1d9
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-15 14:14:35 -0500

    Update 1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square.cpp

--- 1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square/1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square.cpp
+++ 1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square/1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square.cpp
@@ -27,3 +27,32 @@ public:
         return count;
     }
 };
+
+//Runtime: 40 ms, faster than 77.04% of C++ online submissions for Number Of Rectangles That Can Form The Largest Square.
+//Memory Usage: 18.4 MB, less than 55.09% of C++ online submissions for Number Of Rectangles That Can Form The Largest Square.
+    
+class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        
+        int m, t;
+        int s = rectangles.size();
+        //find the max(min(l,w))
+        for(int i = 0; i < s; i++){
+            t = min(rectangles[i][1], rectangles[i][0]);
+            m = (m > t) ? m : t;
+        }
+        
+        int count = 0;
+        
+        for(int i = 0; i < s; i++){
+            t = min(rectangles[i][1], rectangles[i][0]);
+            if(t == m)
+            {
+                count ++;
+            }
+        }
+        
+        return count;
+    }
+};
