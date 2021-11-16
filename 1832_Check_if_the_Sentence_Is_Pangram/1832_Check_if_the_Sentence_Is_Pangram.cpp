0d91ca7adac4f0cf77a6b3425e9670eb084aeccb
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-15 20:53:27 -0500

    Create 1832_Check_if_the_Sentence_Is_Pangram.cpp

---
+++ 1832_Check_if_the_Sentence_Is_Pangram/1832_Check_if_the_Sentence_Is_Pangram.cpp
@@ -0,0 +1,19 @@
+
+//Runtime: 4 ms, faster than 64.48% of C++ online submissions for Check if the Sentence Is Pangram.
+//Memory Usage: 6.6 MB, less than 42.49% of C++ online submissions for Check if the Sentence Is Pangram.
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        if(sentence.length()<26) return false;
+        unordered_map<char, int> alphabet;
+        for(auto s : sentence){
+            alphabet[s] = 1;
+        }
+        for(int i = 0; i < 26; i++){
+            if(alphabet[i+'a']==0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
