7f93074b15c5488fb991b758aa805830f50d9cd4
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-16 23:17:10 -0500

    Update 2006_Count_Number_of_Pairs_With_Absolute_Difference_K.cpp

--- 2006_Count_Number_of_Pairs_With_Absolute_Difference_K/2006_Count_Number_of_Pairs_With_Absolute_Difference_K.cpp
+++ 2006_Count_Number_of_Pairs_With_Absolute_Difference_K/2006_Count_Number_of_Pairs_With_Absolute_Difference_K.cpp
@@ -14,3 +14,20 @@ public:
         return pair;
     }
 };
+
+//Runtime: 16 ms, faster than 73.50% of C++ online submissions for Count Number of Pairs With Absolute Difference K.
+//Memory Usage: 14.4 MB, less than 9.81% of C++ online submissions for Count Number of Pairs With Absolute Difference K.
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int pair = 0;
+        unordered_map<int,int> map;
+        for(int i = 0; i < nums.size(); i++ ){
+            if(map[nums[i]+k]) pair+=map[nums[i]+k]; 
+            if(map[nums[i]-k]) pair+=map[nums[i]-k]; 
+            map[nums[i]]++;
+        }
+
+        return pair;
+    }
+};
