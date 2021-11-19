19dfa9c769edf9823b4b4c1537b6c5f31a7846bf
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-18 20:58:29 -0500

    Update 477_Total_Hamming_Distance.cpp

--- 0477_Total_Hamming_Distance/477_Total_Hamming_Distance.cpp
+++ 0477_Total_Hamming_Distance/477_Total_Hamming_Distance.cpp
@@ -23,3 +23,37 @@ public:
     }
 };
 
+//Runtime: 60 ms, faster than 39.45% of C++ online submissions for Total Hamming Distance.
+//Memory Usage: 19.1 MB, less than 54.65% of C++ online submissions for Total Hamming Distance.
+class Solution {
+public:
+    
+    int totalHammingDistance(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> count(32, 0);
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<32; j++)
+            {
+			    // storing every set bit of each number in the count array.
+                if((nums[i]&(1<<j)) != 0)
+                count[j] ++;
+            }
+        }
+		// after saving all set bits in count array, we have got the count 
+		// of all set bits of every number
+		// like in example nums[] = {4, 14, 2}
+		// count array will be like { ... 0, 1, 2, 2, 0}
+		// means for every bit,
+		// count[i] numbers will make pair with the n-count[i] numbers \
+        // as 0 ^ 1 = 1
+        int ans=0;
+        for(int i=0; i<32; i++)
+        {
+            ans += count[i] * (n-count[i]);
+        }
+        return ans;
+    }
+};
