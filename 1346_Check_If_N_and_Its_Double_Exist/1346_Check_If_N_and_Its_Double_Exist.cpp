1609cbf68ceb75ebb1cbc7805ba7938f29988442
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-28 12:07:00 -0500

    Update 1346_Check_If_N_and_Its_Double_Exist.cpp

--- 1346_Check_If_N_and_Its_Double_Exist/1346_Check_If_N_and_Its_Double_Exist.cpp
+++ 1346_Check_If_N_and_Its_Double_Exist/1346_Check_If_N_and_Its_Double_Exist.cpp
@@ -19,3 +19,30 @@ public:
         return false;
     }
 };
+
+
+/**
+Runtime: 4 ms, faster than 94.14% of C++ online submissions for Check If N and Its Double Exist.
+Memory Usage: 10.6 MB, less than 21.67% of C++ online submissions for Check If N and Its Double Exist.
+*/
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int, int> marr;
+        //save all the even numbers to a map
+        for(int a: arr){
+            if(a % 2 == 0)
+                marr[a]++;
+        }
+        //find
+        for(int a:arr){
+            if(a != 0 && marr.find(2*a) != marr.end()){
+                return true;
+            }
+            if(a == 0 && marr[0] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
