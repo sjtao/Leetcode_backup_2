d63000d183d00b1f768b2b4db5ea92cea3770396
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-27 19:55:29 -0500

    Update 102. Binary Tree Level Order Traversal.cpp

--- 0102_Binary_Tree_Level_Order_Traversal/102. Binary Tree Level Order Traversal.cpp
+++ 0102_Binary_Tree_Level_Order_Traversal/102. Binary Tree Level Order Traversal.cpp
@@ -39,3 +39,33 @@ public:
         return ans;
     }
 };
+
+/**
+Runtime: 8 ms, faster than 37.18% of C++ online submissions for Symmetric Tree.
+Memory Usage: 16.6 MB, less than 17.91% of C++ online submissions for Symmetric Tree.
+*/
+class Solution {
+public:
+    
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        
+        queue<TreeNode *> q;
+        q.push(root->left);
+        q.push(root->right);
+        while(!q.empty()){
+            TreeNode* t1 = q.front();
+            q.pop();
+            TreeNode* t2 = q.front();
+            q.pop();
+            if(!t1 && !t2) continue;
+            if(!t1 || !t2) return false;
+            if(t1->val != t2->val) return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+    }
+};
