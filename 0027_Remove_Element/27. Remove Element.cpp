2fd8682063970f34d0f88692b6c6df132d24b2f4
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-28 10:51:56 -0500

    Update 27. Remove Element.cpp

--- 0027_Remove_Element/27. Remove Element.cpp
+++ 0027_Remove_Element/27. Remove Element.cpp
@@ -18,3 +18,38 @@ public:
         return i;
     }
 };
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Remove Element.
+Memory Usage: 8.7 MB, less than 92.62% of C++ online submissions for Remove Element.
+*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        //two pointers from the different direction
+        int len = nums.size();
+        int l = 0;
+        int r = len-1;
+        int cnt = 0;
+        while(l <= r){
+            if(nums[r] == val){
+                cnt ++;
+                r--;
+            }
+            else{
+                if(nums[l] == val){
+                    nums[l] = nums[r];
+                    nums[r] = nums[l];
+                    cnt++;
+                    r--;
+                    l++;
+                }
+                else{
+                    l++;
+                }
+            }
+        }
+       
+        return len-cnt;
+    }
+};
