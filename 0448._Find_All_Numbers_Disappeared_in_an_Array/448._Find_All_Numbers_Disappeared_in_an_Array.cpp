ac2b1576fd19d4235918fdc0dde6162592392ecc
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-29 15:28:42 -0500

    Update 448._Find_All_Numbers_Disappeared_in_an_Array.cpp

--- 0448._Find_All_Numbers_Disappeared_in_an_Array/448._Find_All_Numbers_Disappeared_in_an_Array.cpp
+++ 0448._Find_All_Numbers_Disappeared_in_an_Array/448._Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -20,3 +20,27 @@ public:
         return missed;        
     }
 };
+
+/**
+Runtime: 48 ms, faster than 79.26% of C++ online submissions for Find All Numbers Disappeared in an Array.
+Memory Usage: 33.8 MB, less than 34.51% of C++ online submissions for Find All Numbers Disappeared in an Array.
+*/
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> missed;
+        for(int i = 0; i < len; i++){
+            int index = abs(nums[i]) - 1;
+            if(nums[index] > 0)
+                nums[index] = -nums[index];
+        }
+        for(int i = 1; i <= len; i++){
+            if(nums[i-1] > 0){
+                missed.push_back(i);
+            }
+        }
+
+        return missed;        
+    }
+};
