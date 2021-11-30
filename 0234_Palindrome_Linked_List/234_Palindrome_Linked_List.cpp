e28222012fb949772909e5cf967af81cce405097
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-30 16:53:14 -0500

    Update 234_Palindrome_Linked_List.cpp

--- 0234_Palindrome_Linked_List/234_Palindrome_Linked_List.cpp
+++ 0234_Palindrome_Linked_List/234_Palindrome_Linked_List.cpp
@@ -34,3 +34,51 @@ public:
         return true;
     }
 };
+
+
+/**
+Runtime: 172 ms, faster than 94.18% of C++ online submissions for Palindrome Linked List.
+Memory Usage: 116.3 MB, less than 63.48% of C++ online submissions for Palindrome Linked List.
+*/
+class Solution {
+public:
+    
+    ListNode *FindFirstHalf(ListNode* head){
+        ListNode *slow = head, *fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    
+    ListNode * ReverseList(ListNode * head){
+        if (!head || !head->next) return head;
+        
+        ListNode *node = ReverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return node;        
+    }
+    
+    
+    bool isPalindrome(ListNode* head) {
+        //reverse the seond half and compare to the first half
+        if(!head) return true;
+        
+        ListNode* firstend = FindFirstHalf(head);
+        ListNode* rvsecond = ReverseList(firstend->next);
+        
+        ListNode *f = head, *s = rvsecond;
+        while(s){
+            if(f->val != s->val){
+                return false;
+            }
+            f = f->next;
+            s = s->next;
+        }
+        
+        return true;
+    }
+};
