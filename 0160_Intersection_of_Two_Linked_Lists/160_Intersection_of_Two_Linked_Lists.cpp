81bac14aaf5f969763f17189ab38f050451fd3aa
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-29 21:06:49 -0500

    Update 160_Intersection_of_Two_Linked_Lists.cpp

--- 0160_Intersection_of_Two_Linked_Lists/160_Intersection_of_Two_Linked_Lists.cpp
+++ 0160_Intersection_of_Two_Linked_Lists/160_Intersection_of_Two_Linked_Lists.cpp
@@ -34,3 +34,28 @@ public:
         return pA;
     }
 };
+
+/**
+Runtime: 56 ms, faster than 37.52% of C++ online submissions for Intersection of Two Linked Lists.
+Memory Usage: 17.8 MB, less than 10.57% of C++ online submissions for Intersection of Two Linked Lists.
+*/
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        set<ListNode*> nodeB;
+        
+        while(headB != NULL){
+            nodeB.insert(headB);
+            headB = headB->next;
+        }
+        
+        while(headA != NULL){
+            if(nodeB.find(headA) != nodeB.end()){
+                return headA;
+            }
+            headA = headA->next;
+        }
+        
+        return NULL;
+    }
+};
