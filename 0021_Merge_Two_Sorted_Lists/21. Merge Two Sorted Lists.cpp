5b772ef92bcf6ada54e3197ef7542f4e6ee429c3
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-30 21:26:49 -0500

    Update 21. Merge Two Sorted Lists.cpp

--- 0021_Merge_Two_Sorted_Lists/21. Merge Two Sorted Lists.cpp
+++ 0021_Merge_Two_Sorted_Lists/21. Merge Two Sorted Lists.cpp
@@ -66,3 +66,36 @@ public:
         
     }
 };
+
+
+/**
+Runtime: 8 ms, faster than 74.05% of C++ online submissions for Merge Two Sorted Lists.
+Memory Usage: 14.9 MB, less than 17.44% of C++ online submissions for Merge Two Sorted Lists.
+*/
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode * dummy = new ListNode(0);
+        ListNode *cur = dummy;
+        ListNode *p1 = list1;
+        ListNode *p2 = list2;
+        while(p1 && p2){
+            if(p1->val < p2->val){
+                cur->next = new ListNode(p1->val);
+                p1 = p1->next;
+            }
+            else{
+                cur->next = new ListNode(p2->val);
+                p2 = p2->next;
+            }
+            cur = cur->next;
+        }
+        if(p1){
+            cur->next = p1;
+        }
+        if(p2){
+            cur->next = p2;
+        }
+        return dummy->next;
+    }
+};
