3e402a2528a59183d742000bf2a8bf9f508157fe
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-11-30 21:58:53 -0500

    Update 2. Add Two Numbers.cpp

--- 0002_Add_Two_Numbers/2. Add Two Numbers.cpp
+++ 0002_Add_Two_Numbers/2. Add Two Numbers.cpp
@@ -48,3 +48,35 @@ public:
         return dummy->next;
     }
 };
+
+/**
+Runtime: 20 ms, faster than 94.90% of C++ online submissions for Add Two Numbers.
+Memory Usage: 71.5 MB, less than 47.74% of C++ online submissions for Add Two Numbers.
+*/
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode * dummy = new ListNode(0);
+        ListNode *cur = dummy;
+        int carry = 0, lx, ly, res;
+        while(l1 || l2){
+            if(l1 == nullptr) lx = 0;
+            else  lx = l1->val;
+            if(l2 == nullptr) ly = 0;
+            else  ly = l2->val;
+            res = carry + lx + ly;
+            carry = res / 10;
+            cur->next = new ListNode(res%10);
+            cur = cur->next;
+            if(l1!=nullptr) l1 = l1->next;
+            if(l2!=nullptr) l2 = l2->next;
+        }
+
+        if(carry){
+            cur->next = new ListNode(carry);
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
