6209207da7d77b455f1b122c125afcad169f9283
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-04 21:06:08 -0500

    Update 1137_N-th_Tribonacci_Number.cpp

--- 1137_N-th_Tribonacci_Number/1137_N-th_Tribonacci_Number.cpp
+++ 1137_N-th_Tribonacci_Number/1137_N-th_Tribonacci_Number.cpp
@@ -19,3 +19,26 @@ public:
         return triHelper(n);
     }
 };
+
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for N-th Tribonacci Number.
+Memory Usage: 6.1 MB, less than 26.26% of C++ online submissions for N-th Tribonacci Number.
+*/
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2)  return 1;
+        
+        vector<int> m(38,0);
+        m[0] = 0;
+        m[1] = 1;
+        m[2] = 1;
+        for(int i = 3; i < n+1; i++){
+            m[i] = m[i-1] + m[i-2] + m[i-3];
+        }
+        
+        return m[n];
+    }
+};
