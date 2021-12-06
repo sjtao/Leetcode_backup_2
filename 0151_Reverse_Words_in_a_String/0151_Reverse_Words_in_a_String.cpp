fd8c684c8f1947c7e59c6f843e7ff3481a31a195
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-06 16:43:29 -0500

    Update 0151_Reverse_Words_in_a_String.cpp

--- 0151_Reverse_Words_in_a_String/0151_Reverse_Words_in_a_String.cpp
+++ 0151_Reverse_Words_in_a_String/0151_Reverse_Words_in_a_String.cpp
@@ -36,3 +36,49 @@ public:
         return ans;
     }
 };
+
+
+/**
+Runtime: 4 ms, faster than 82.98% of C++ online submissions for Reverse Words in a String.
+Memory Usage: 8.4 MB, less than 28.76% of C++ online submissions for Reverse Words in a String.
+*/
+
+class Solution {
+public:
+    
+    vector<string> parseString(string s){
+        vector<string> par;
+        int n = s.length();
+        int i = 0;
+        string t = "";
+        while(i < n){
+            if(s[i] != ' '){
+                t += s[i];
+            }
+            else{
+                if(t!= ""){
+                    par.push_back(t);
+                    t = "";
+                }
+            }
+            i++; 
+        }
+        
+        if(t!= "")
+            par.push_back(t);
+            
+        return par;
+    }
+    
+    string reverseWords(string s) {
+        string ans = "";  
+        vector<string> par = parseString(s);
+        int n = par.size();
+        int i;
+        for(i = n-1; i>=0; i--){
+            ans += par[i] + ' ';
+        }
+        ans = ans.substr(0, ans.length()-1);
+        return ans;
+    }
+};
