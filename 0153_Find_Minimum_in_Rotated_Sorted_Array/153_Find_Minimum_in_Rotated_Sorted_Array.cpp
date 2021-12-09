74aea5f0ccecb38a33c4b2b596a2aba24c4d4314
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-09 11:48:09 -0500

    Update 153_Find_Minimum_in_Rotated_Sorted_Array.cpp

--- 0153_Find_Minimum_in_Rotated_Sorted_Array/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ 0153_Find_Minimum_in_Rotated_Sorted_Array/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -25,3 +25,29 @@ public:
         return -1;
     }
 };
+
+/**
+Runtime: 4 ms, faster than 60.37% of C++ online submissions for Find Minimum in Rotated Sorted Array.
+Memory Usage: 10.1 MB, less than 70.62% of C++ online submissions for Find Minimum in Rotated Sorted Array.
+*/
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, high = n-1, mid;
+        if (nums[high] >= nums[0]) {
+            return nums[0];
+        }
+        int ans = INT_MAX;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(nums[mid]<nums[high]){
+                high = mid-1;
+                ans = min(ans, nums[mid]);
+            }
+            else
+                low = mid+1;
+        }
+        return min(ans, nums[mid]);
+    }
+};
