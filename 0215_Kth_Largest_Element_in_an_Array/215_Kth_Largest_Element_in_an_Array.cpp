3dc7737c71f7ca034cdb492dd87a1b261f86d82d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-10 22:55:58 -0500

    Update 215_Kth_Largest_Element_in_an_Array.cpp

--- 0215_Kth_Largest_Element_in_an_Array/215_Kth_Largest_Element_in_an_Array.cpp
+++ 0215_Kth_Largest_Element_in_an_Array/215_Kth_Largest_Element_in_an_Array.cpp
@@ -10,3 +10,44 @@ public:
         return nums[n-k];
     }
 };
+
+/**
+Runtime: 8 ms, faster than 77.01% of C++ online submissions for Kth Largest Element in an Array.
+Memory Usage: 10.2 MB, less than 33.27% of C++ online submissions for Kth Largest Element in an Array.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Creates a max heap
+        priority_queue <int> maxq(nums.begin(), nums.end());
+        
+        while(k > 1){
+            maxq.pop();
+            k--;
+        }
+        
+        return maxq.top();
+        
+    }
+};
+
+/**
+Runtime: 8 ms, faster than 77.01% of C++ online submissions for Kth Largest Element in an Array.
+Memory Usage: 10.3 MB, less than 17.98% of C++ online submissions for Kth Largest Element in an Array.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Creates a min heap
+        priority_queue <int, vector<int>, greater<int>> minq;
+        
+        for(auto n:nums){
+            minq.push(n);
+            if(minq.size()>k)
+                minq.pop();
+        }
+        
+        return minq.top();
+        
+    }
+};
