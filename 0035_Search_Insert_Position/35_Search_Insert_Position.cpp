f9a8c37d428b9b6ac567a5610bb457b84392729a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-11 22:48:00 -0500

    Update 35_Search_Insert_Position.cpp

--- 0035_Search_Insert_Position/35_Search_Insert_Position.cpp
+++ 0035_Search_Insert_Position/35_Search_Insert_Position.cpp
@@ -22,3 +22,28 @@ public:
         return left;
     }
 };
+
+/**
+Runtime: 8 ms, faster than 24.14% of C++ online submissions for Search Insert Position.
+Memory Usage: 9.8 MB, less than 22.75% of C++ online submissions for Search Insert Position.
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0, right = n-1, mid;
+        if (nums[left] > target) return left;
+        if (nums[right] < target) return n;
+        while(left <= right){
+            mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                right = mid-1;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+};
