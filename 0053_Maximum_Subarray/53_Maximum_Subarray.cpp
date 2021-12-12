a1e562d5d7aeae25b9c3fd25bee2400f733d2fb0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-11 23:11:10 -0500

    Update 53_Maximum_Subarray.cpp

--- 0053_Maximum_Subarray/53_Maximum_Subarray.cpp
+++ 0053_Maximum_Subarray/53_Maximum_Subarray.cpp
@@ -18,3 +18,23 @@ public:
       
     }
 };
+
+/**
+Runtime: 104 ms, faster than 71.96% of C++ online submissions for Maximum Subarray.
+Memory Usage: 73.9 MB, less than 6.63% of C++ online submissions for Maximum Subarray.
+*/
+class Solution {
+public:
+    int globalmax;
+    int maxHelper(vector<int>& nums, int n){
+        if(n == 1) return nums[0];
+        int currMaxSum = max(nums[n - 1], maxHelper(nums, n - 1) + nums[n - 1]);
+        globalmax = max(globalmax, currMaxSum);
+        return currMaxSum;
+    }
+    int maxSubArray(vector<int>& nums) {
+        globalmax = nums[0];
+        maxHelper(nums, nums.size());
+        return globalmax;
+    }
+};
