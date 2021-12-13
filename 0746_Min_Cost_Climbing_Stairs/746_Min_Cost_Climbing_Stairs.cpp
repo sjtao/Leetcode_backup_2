91cde5cde631b86a21ec080fb44cf4856859bfe1
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-13 12:21:40 -0500

    Update 746_Min_Cost_Climbing_Stairs.cpp

--- 0746_Min_Cost_Climbing_Stairs/746_Min_Cost_Climbing_Stairs.cpp
+++ 0746_Min_Cost_Climbing_Stairs/746_Min_Cost_Climbing_Stairs.cpp
@@ -22,3 +22,22 @@ public:
         return min(dp[n-1],dp[n-2]);
     }
 };
+
+/**
+Runtime: 12 ms, faster than 16.96% of C++ online submissions for Min Cost Climbing Stairs.
+Memory Usage: 15.5 MB, less than 5.53% of C++ online submissions for Min Cost Climbing Stairs.
+*/
+class Solution {
+private:
+    unordered_map<int,int> mp;
+public:
+    int CostHelper(vector<int>& cost, int stair){
+        if(stair == 0 || stair == 1) return 0;
+        if(mp.find(stair) == mp.end())
+            mp[stair] = min(CostHelper(cost, stair-1) + cost[stair-1], CostHelper(cost, stair-2) + cost[stair-2]);
+        return mp[stair];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        return CostHelper(cost, cost.size());
+    }
+};
