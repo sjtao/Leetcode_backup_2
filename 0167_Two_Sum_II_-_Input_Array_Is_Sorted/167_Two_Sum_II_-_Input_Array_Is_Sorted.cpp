f39b8875b9528d2f7af0fef4dd60b70a6b5c643c
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-14 13:49:39 -0500

    Update 167_Two_Sum_II_-_Input_Array_Is_Sorted.cpp

--- 0167_Two_Sum_II_-_Input_Array_Is_Sorted/167_Two_Sum_II_-_Input_Array_Is_Sorted.cpp
+++ 0167_Two_Sum_II_-_Input_Array_Is_Sorted/167_Two_Sum_II_-_Input_Array_Is_Sorted.cpp
@@ -26,3 +26,34 @@ public:
         return ans;
     }
 };
+
+/**
+Runtime: 11 ms, faster than 9.54% of C++ online submissions for Two Sum II - Input Array Is Sorted.
+Memory Usage: 9.9 MB, less than 5.24% of C++ online submissions for Two Sum II - Input Array Is Sorted.
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> mp;
+        
+        int n = numbers.size();
+        for(int i = 0; i < n; i++){
+            mp[numbers[i]] = i;
+        }
+        
+        int r;
+        vector<int> ans(2);
+        for(int i = 0; i < n; i++){
+            r = target - numbers[i];
+            if(mp.find(r) != mp.end()){
+                if(mp[r] != i){
+                    ans[0] = i+1;//1-indexed array
+                    ans[1] = mp[r]+1;
+                    break;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
