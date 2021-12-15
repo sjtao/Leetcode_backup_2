9964230ed867399b9522082a3514025474b798e9
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-14 21:22:05 -0500

    Create 116_Populating_Next_Right_Pointers_in_Each_Node.cpp

---
+++ 0116_Populating_Next_Right_Pointers_in_Each_Node/116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+/**
+Runtime: 36 ms, faster than 8.35% of C++ online submissions for Populating Next Right Pointers in Each Node.
+Memory Usage: 17.2 MB, less than 36.84% of C++ online submissions for Populating Next Right Pointers in Each Node.
+*/
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+        //Level Order Traversal, BFS
+        queue<Node*> qe;
+        qe.push(root);
+        while(qe.size() > 0){
+            int s = qe.size();
+            for(int i = 0; i < s; i++){
+                Node *n = qe.front();
+                qe.pop();
+                if(i != s-1) n->next = qe.front();
+                if(n->left)  qe.push(n->left);
+                if(n->right)  qe.push(n->right);
+            }
+        }
+        return root;
+    }
+};
+
+/**
+Runtime: 28 ms, faster than 15.99% of C++ online submissions for Populating Next Right Pointers in Each Node.
+Memory Usage: 16.9 MB, less than 66.34% of C++ online submissions for Populating Next Right Pointers in Each Node.
+*/
