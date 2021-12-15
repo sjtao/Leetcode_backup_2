7db429b138a55519f177249a1ed74058e3765084
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-15 09:49:40 -0500

    Update 566_Reshape_the_Matrix.cpp

--- 0566_Reshape_the_Matrix/566_Reshape_the_Matrix.cpp
+++ 0566_Reshape_the_Matrix/566_Reshape_the_Matrix.cpp
@@ -31,3 +31,24 @@ public:
         return ans;
     }
 };
+
+/**
+Runtime: 14 ms, faster than 18.88% of C++ online submissions for Reshape the Matrix.
+Memory Usage: 10.6 MB, less than 81.57% of C++ online submissions for Reshape the Matrix.
+*/
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int mr = mat.size();
+        int mc = mat[0].size();
+        if((mr == 0) || (mr == r && mc == c) || (mc * mr != r * c)) 
+            return mat;
+        
+        vector<vector<int>> ans(r, vector<int>(c,0));
+        for(int i = 0; i < r*c; i++){
+            ans[i / c][ i % c] = mat[i / mc][i % mc];
+        }
+        
+        return ans;
+    }
+};
