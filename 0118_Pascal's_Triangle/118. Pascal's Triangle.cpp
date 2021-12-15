3fd05ef779bd16fd604cff8c3d9ac7fa12134b95
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-15 10:04:18 -0500

    Update 118. Pascal's Triangle.cpp

--- 0118_Pascal's_Triangle/118. Pascal's Triangle.cpp
+++ 0118_Pascal's_Triangle/118. Pascal's Triangle.cpp
@@ -21,3 +21,33 @@ public:
         return pascal;
     }
 };
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Pascal's Triangle.
+Memory Usage: 6.3 MB, less than 99.65% of C++ online submissions for Pascal's Triangle.
+*/
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> ans(numRows);
+        int i = 0;
+        while(i < numRows){
+            ans[i] = vector<int>(i+1);
+            if(i == 0)          
+                ans[0][0] = 1;
+            else{
+                ans[i][0] = 1;
+                ans[i][i] = 1;
+                for(int j = 1; j < i; j++){
+                    ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+                }
+            }
+            i++;
+                
+        }
+
+        return ans;
+        
+    }
+};
