6149b4e0f34e32eca7c0af26b07825e1998ee8f1
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-16 11:48:34 -0500

    Update 19_Remove_Nth_Node_From_End_of_List.cpp

--- 0019_Remove_Nth_Node_From_End_of_List/19_Remove_Nth_Node_From_End_of_List.cpp
+++ 0019_Remove_Nth_Node_From_End_of_List/19_Remove_Nth_Node_From_End_of_List.cpp
@@ -36,3 +36,33 @@ public:
         return dummy->next;
     }
 };
+/**
+Runtime: 4 ms, faster than 78.76% of C++ online submissions for Remove Nth Node From End of List.
+Memory Usage: 10.7 MB, less than 31.29% of C++ online submissions for Remove Nth Node From End of List.
+*/
+class Solution {
+public:
+    int countNode(ListNode* head){
+        int c = 0;
+        while(head != nullptr){
+            head = head->next;
+            c++;
+        }
+        return c;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {     
+        int size = countNode(head);
+        if(n == size) return head->next;
+        
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        int i = 1;
+        while(i < size - n){
+            fast = fast->next;
+            slow = slow->next;
+            i++;
+        }
+        slow->next = (fast == nullptr ? nullptr: fast->next);
+        return head;
+    }
+};
