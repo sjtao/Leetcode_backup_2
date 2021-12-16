b82ed65302a21303aa9bc37e223756bd02730c0a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-16 16:56:49 -0500

    Update 593_Valid_Square.cpp

--- 0593_Valid_Square/593_Valid_Square.cpp
+++ 0593_Valid_Square/593_Valid_Square.cpp
@@ -22,3 +22,31 @@ public:
         return squareHelper(p1, p2, p3) && squareHelper(p2, p3, p4) && squareHelper(p1, p3, p4);
     }
 };
+
+/**
+Runtime: 8 ms, faster than 54.00% of C++ online submissions for Valid Square.
+Memory Usage: 26.7 MB, less than 35.75% of C++ online submissions for Valid Square.
+*/
+class Solution {
+public:
+    int len(vector<int>& p1, vector<int>& p2){
+        return (p1[0] - p2[0])*(p1[0] - p2[0]) + (p1[1] - p2[1])*(p1[1] - p2[1]);
+    }
+    
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        unordered_set<int> side;
+        
+        side.insert(len(p1, p2));
+        side.insert(len(p1, p3));
+        side.insert(len(p1, p4));
+        side.insert(len(p2, p3));
+        side.insert(len(p2, p4));
+        side.insert(len(p3, p4));
+        
+        //side and diagonal: 2 non-zero numbers
+        if(side.size() == 2 && side.find(0) == side.end())
+            return true;
+        
+        return false;
+    }
+};
