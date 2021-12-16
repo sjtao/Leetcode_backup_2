a773e1c8b24123dc1c9d94ff5b7209ad6f753dc7
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-16 10:35:00 -0500

    Update 74_Search_a_2D_Matrix.cpp

--- 0074_Search_a_2D_Matrix/74_Search_a_2D_Matrix.cpp
+++ 0074_Search_a_2D_Matrix/74_Search_a_2D_Matrix.cpp
@@ -27,3 +27,27 @@ public:
         return false;
     }
 };
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Search a 2D Matrix.
+Memory Usage: 9.5 MB, less than 50.65% of C++ online submissions for Search a 2D Matrix.
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int n = row * col;
+        //binary search: treat as a sorted list
+        int l = 0, r = n-1;
+        while(l <= r){
+            if(matrix[l/col][l%col] == target || matrix[r/col][r%col] == target)
+                return true;
+            if(matrix[l/col][l%col] < target)
+                l++;
+            if(matrix[r/col][r%col] > target)
+                r--;
+        }
+        return false;
+    }
+};
