28038dc969093ce9f228a893d5340da158b37358
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-16 11:29:16 -0500

    Create 876_Middle_of_the_Linked_List.cpp

---
+++ 0876_Middle_of_the_Linked_List/876_Middle_of_the_Linked_List.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ /**
+Runtime: 3 ms, faster than 45.69% of C++ online submissions for Middle of the Linked List.
+Memory Usage: 7.2 MB, less than 21.30% of C++ online submissions for Middle of the Linked List.
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        queue<ListNode*> qe;
+        int len = 0;
+        while(head != nullptr){
+            qe.push(head);
+            head = head->next;
+            len += 1;
+        }
+        int i = 0, mid = len/2;
+        while(i < mid){
+            qe.pop();
+            i++;
+        }
+        return qe.front();
+        
+    }
+};
