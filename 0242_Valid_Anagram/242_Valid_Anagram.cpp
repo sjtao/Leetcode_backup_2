9923ee5242c5ce62f5798c04ddce5154bbb6710a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-17 10:59:53 -0500

    Create 242_Valid_Anagram.cpp

---
+++ 0242_Valid_Anagram/242_Valid_Anagram.cpp
@@ -0,0 +1,51 @@
+/**
+Runtime: 37 ms, faster than 5.47% of C++ online submissions for Valid Anagram.
+Memory Usage: 7.3 MB, less than 43.25% of C++ online submissions for Valid Anagram.
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //An anagram is produced by rearranging the letters of s into t
+        if(s.length() != t.length()) return false;
+        
+        unordered_map<char, int> sp;
+        for(char c: s){
+            sp[c]++;
+        }
+        for(char c:t){
+            if(sp.find(c) == sp.end()) // t has c but s doesn't
+                return false;
+            else if(sp[c] <= 0) //t has more c than s
+                return false;
+            else
+                sp[c]--;
+        }
+        return true;
+    }
+};
+
+/**
+Runtime: 24 ms, faster than 5.47% of C++ online submissions for Valid Anagram.
+Memory Usage: 7.4 MB, less than 43.25% of C++ online submissions for Valid Anagram.
+*/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //An anagram is produced by rearranging the letters of s into t
+        if(s.length() != t.length()) return false;
+        
+        unordered_map<char, int> sp, tp;
+        for(char c: s){
+            sp[c]++;
+        }
+        for(char c:t){
+            tp[c]++;
+        }
+        for(char c:s){
+            if(tp.find(c) == tp.end() || tp[c] != sp[c])
+                return false;
+        }
+        return true;
+    }
+};
+
