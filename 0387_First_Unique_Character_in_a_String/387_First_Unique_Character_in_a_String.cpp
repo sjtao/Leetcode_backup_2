dc6cb918b44c57cf11caf65b00243f554b16520e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-17 10:32:06 -0500

    Update 387_First_Unique_Character_in_a_String.cpp

--- 0387_First_Unique_Character_in_a_String/387_First_Unique_Character_in_a_String.cpp
+++ 0387_First_Unique_Character_in_a_String/387_First_Unique_Character_in_a_String.cpp
@@ -22,3 +22,24 @@ public:
         return -1;
     }
 };
+
+/**
+Runtime: 62 ms, faster than 17.44% of C++ online submissions for First Unique Character in a String.
+Memory Usage: 10.8 MB, less than 14.33% of C++ online submissions for First Unique Character in a String.
+*/
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> mp;
+        for(char c : s){
+            mp[c]++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(mp[s[i]] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
