a302aa6e6c7e4b2ef884b1f49e9d202fdb21d613
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-18 21:01:22 -0500

    Update 695_Max_Area_of_Island.cpp

--- 0695_Max_Area_of_Island/695_Max_Area_of_Island.cpp
+++ 0695_Max_Area_of_Island/695_Max_Area_of_Island.cpp
@@ -25,3 +25,32 @@ public:
         return mx;
     }
 };
+
+/**
+Runtime: 24 ms, faster than 38.98% of C++ online submissions for Max Area of Island.
+Memory Usage: 23.1 MB, less than 87.78% of C++ online submissions for Max Area of Island.
+*/
+class Solution {
+public:
+    int area(vector<vector<int>>& grid, int m, int n, int r, int c){
+        if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] != 1)
+            return 0;
+        grid[r][c] = -1;
+        return 1 + area(grid, m, n, r-1, c) + area(grid, m, n, r+1, c) 
+            + area(grid, m, n, r, c+1) + area(grid, m, n, r, c-1);
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int mx = 0;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if (grid[i][j])
+                    mx = max(mx, area(grid, m, n, i, j));
+            }
+        }
+        
+        return mx;
+    }
+};
