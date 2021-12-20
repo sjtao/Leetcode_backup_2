4fd4c613be35922dcb641c405a71a7adf2be0a4f
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-20 15:59:24 -0500

    Update 42_Trapping_Rain_Water.cpp

--- 0042_Trapping_Rain_Water/42_Trapping_Rain_Water.cpp
+++ 0042_Trapping_Rain_Water/42_Trapping_Rain_Water.cpp
@@ -26,3 +26,32 @@ public:
         
     }
 };
+
+/**
+Runtime: 8 ms, faster than 80.06% of C++ online submissions for Trapping Rain Water.
+Memory Usage: 16 MB, less than 30.70% of C++ online submissions for Trapping Rain Water.
+*/
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n <= 2) return 0;
+        
+        int water = 0;
+        vector<int> leftmax(n, 0), rightmax(n,0);
+        leftmax[0] = height[0];
+        rightmax[n-1] = height[n-1];
+        for(int i = 1; i < n; i++){
+            leftmax[i] = max(leftmax[i-1], height[i]);
+        }
+        for(int i = n-2; i>= 0; i--){
+            rightmax[i] = max(rightmax[i+1], height[i]);
+        }
+        for(int i = 0; i < n; i++){
+            water += (min(rightmax[i], leftmax[i]) - height[i]);
+        }
+        
+        return water;
+        
+    }
+};
