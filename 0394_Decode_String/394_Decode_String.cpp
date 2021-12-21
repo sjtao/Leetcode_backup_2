1b76500ab2a61003786ffe3c649572ed868a3786
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-21 17:20:11 -0500

    Create 394_Decode_String.cpp

---
+++ 0394_Decode_String/394_Decode_String.cpp
@@ -0,0 +1,50 @@
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Decode String.
+Memory Usage: 6.5 MB, less than 75.87% of C++ online submissions for Decode String.
+*/
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        int n = s.size();
+        
+        for(int i = 0; i < n; i++){
+            string temp;
+            if(s[i] != ']'){
+                if(s[i] >= '0' && s[i] <= '9'){
+                    if(!st.empty() && "0" <= st.top() && st.top() <= "9"){ //multiple-digit number
+                        temp = st.top() + s[i];
+                        st.pop();
+                        st.push(temp);
+                    }
+                    else
+                        st.push(temp + s[i]); //1-digit number
+                }
+                else
+                    st.push(temp + s[i]);             
+            }
+            else{
+                string c;
+                while(st.top() != "["){
+                    c = st.top(); st.pop();
+                    temp = c+temp;
+                }
+                st.pop(); //pop '['
+                c = st.top(); st.pop();
+                int r = stoi(c);
+                string tp = "";
+                while(r){
+                    tp += temp; r--;
+                }
+                st.push(tp);
+            }
+        }
+        
+        string ans;
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
