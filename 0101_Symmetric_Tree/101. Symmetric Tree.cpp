db5f718ab14eaa58ea0c759bb0c27fdeb972f1da
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-21 22:35:38 -0500

    Update 101. Symmetric Tree.cpp

--- 0101_Symmetric_Tree/101. Symmetric Tree.cpp
+++ 0101_Symmetric_Tree/101. Symmetric Tree.cpp
@@ -28,3 +28,56 @@ public:
         return isSymmetricHelper(root->left, root->right);
     }
 };
+
+/**
+Runtime: 8 ms, faster than 37.18% of C++ online submissions for Symmetric Tree.
+Memory Usage: 16.6 MB, less than 17.91% of C++ online submissions for Symmetric Tree.
+*/
+class Solution {
+public:
+    
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        
+        queue<TreeNode *> q;
+        q.push(root->left);
+        q.push(root->right);
+        while(!q.empty()){
+            TreeNode* t1 = q.front();
+            q.pop();
+            TreeNode* t2 = q.front();
+            q.pop();
+            if(!t1 && !t2) continue;
+            if(!t1 || !t2) return false;
+            if(t1->val != t2->val) return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return true;
+    }
+};
+
+/**
+Runtime: 4 ms
+Memory Usage: 16.5 MB
+*/
+class Solution {
+public:
+    bool helper(TreeNode *root1, TreeNode *root2){
+        if((!root1 && root2) || (root1 && !root2)) return false;
+        if(!root1 && !root2) return true;
+        
+        bool sym = false;
+        if(root1->val == root2->val)
+            sym = true;
+        
+        sym = sym && helper(root1->left, root2->right) && helper(root2->left, root1->right);
+        
+        return sym;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return helper(root->left, root->right);
+    }
+};
