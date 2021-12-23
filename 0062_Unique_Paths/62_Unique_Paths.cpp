59e8fe8c9770f19ca012d0f6ec4b489f68d40548
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-23 16:53:39 -0500

    Update 62_Unique_Paths.cpp

--- 0062_Unique_Paths/62_Unique_Paths.cpp
+++ 0062_Unique_Paths/62_Unique_Paths.cpp
@@ -21,3 +21,20 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Paths.
+Memory Usage: 6.4 MB, less than 37.24% of C++ online submissions for Unique Paths.
+*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
