0105dad76dcb4021dfc902383737d3e5b8920ebe
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-23 17:16:00 -0500

    Create 63_Unique_Paths_II.cpp

---
+++ 0063_Unique_Paths_II/63_Unique_Paths_II.cpp
@@ -0,0 +1,41 @@
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Paths II.
+Memory Usage: 7.8 MB, less than 36.96% of C++ online submissions for Unique Paths II.
+*/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < n; i++){
+            if(obstacleGrid[0][i] == 0)
+                dp[0][i] = 1;
+            else
+                break;
+        }
+        for(int i = 0; i < m; i++){
+            if(obstacleGrid[i][0] == 0)
+                dp[i][0] = 1;
+            else
+                break;
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i-1][j] == 1 && obstacleGrid[i][j-1] == 0)
+                    dp[i][j] = dp[i][j-1];
+                if(obstacleGrid[i-1][j] == 0 && obstacleGrid[i][j-1] == 1)
+                    dp[i][j] = dp[i-1][j];
+                if(obstacleGrid[i-1][j] == 1 && obstacleGrid[i][j-1] == 1)
+                    dp[i][j] = 0;
+                if(obstacleGrid[i-1][j] == 0 && obstacleGrid[i][j-1] == 0)
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
