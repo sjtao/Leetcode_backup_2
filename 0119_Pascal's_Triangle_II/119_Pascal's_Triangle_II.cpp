c16bacc228dbd1d8afbcc284850bd4f3a94bd67d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-22 21:47:38 -0500

    Update 119_Pascal's_Triangle_II.cpp

--- 0119_Pascal's_Triangle_II/119_Pascal's_Triangle_II.cpp
+++ 0119_Pascal's_Triangle_II/119_Pascal's_Triangle_II.cpp
@@ -13,3 +13,22 @@ public:
         return ans;
     }
 };
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Pascal's Triangle II.
+Memory Usage: 6.4 MB, less than 78.90% of C++ online submissions for Pascal's Triangle II.
+*/
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> dp(rowIndex+1, 1);
+        int r = 2;
+        while(r <= rowIndex){
+            for(int i = r-1; i > 0; i--){
+                dp[i] += dp[i-1];
+            }
+            r++;
+        }
+        return dp;
+    }
+};
