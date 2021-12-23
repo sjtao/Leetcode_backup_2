01933cc2af914c7eeb81122f96639e72ca512949
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-23 17:28:53 -0500

    Create 64_Minimum_Path_Sum.cpp

---
+++ 0064_Minimum_Path_Sum/64_Minimum_Path_Sum.cpp
@@ -0,0 +1,25 @@
+/**
+Runtime: 8 ms, faster than 83.32% of C++ online submissions for Minimum Path Sum.
+Memory Usage: 10.2 MB, less than 23.17% of C++ online submissions for Minimum Path Sum.
+*/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < n; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for(int i = 1; i < m; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
