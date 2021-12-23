12fbde24ea0f51d74ebe9cdfc0974190c50961e7
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-23 11:47:18 -0500

    Update 599_Minimum_Index_Sum_of_Two_Lists.cpp

--- 0599_Minimum_Index_Sum_of_Two_Lists/599_Minimum_Index_Sum_of_Two_Lists.cpp
+++ 0599_Minimum_Index_Sum_of_Two_Lists/599_Minimum_Index_Sum_of_Two_Lists.cpp
@@ -29,3 +29,42 @@ public:
         return ans[mi];
     }
 };
+
+/**
+Runtime: 68 ms, faster than 96.44% of C++ online submissions for Minimum Index Sum of Two Lists.
+Memory Usage: 37 MB, less than 57.13% of C++ online submissions for Minimum Index Sum of Two Lists.
+*/
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        
+        int n1 = list1.size();
+        int n2 = list2.size();
+        
+        unordered_map<string, int> mp, rp;
+        vector<string> ans;
+        
+        for(int i = 0; i < n1; i++){
+            mp[list1[i]] = i;
+        }
+        
+        int mi = INT_MAX, sum;
+        for(int i = 0; i < n2; i++){
+            if(mp.find(list2[i]) != mp.end()){
+                sum = mp[list2[i]] + i; //list2[i] shows in list1 and list2
+                if(sum < mi){
+                    mi = sum;
+                    ans.clear();
+                    ans.push_back(list2[i]);
+                }
+                else if(sum == mi){
+                    ans.push_back(list2[i]);
+                }
+                else{} //do nothing        
+            }
+                
+        }
+        
+        return ans;
+    }
+};
