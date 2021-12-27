fe7b193a150b5e3dba59d0b5e90bb65b30483adc
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-27 17:00:04 -0500

    Update 169_Majority_Element.cpp

--- 0169_Majority_Element/169_Majority_Element.cpp
+++ 0169_Majority_Element/169_Majority_Element.cpp
@@ -22,3 +22,27 @@ public:
         return ans;
     }
 };
+
+/**
+Runtime: 24 ms, faster than 24.42% of C++ online submissions for Majority Element.
+Memory Usage: 19.7 MB, less than 8.37% of C++ online submissions for Majority Element.
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int m = n / 2;
+        unordered_map<int, int> mp;
+        int ans;
+        
+        for(int n : nums){
+            mp[n]++;
+            if(mp[n] > m){
+                ans = n;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
