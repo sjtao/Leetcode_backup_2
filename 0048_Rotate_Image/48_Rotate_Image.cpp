d5a12cc445f271bbf5244fee39e0c73c10c5f08e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-27 22:18:49 -0500

    Update 48_Rotate_Image.cpp

--- 0048_Rotate_Image/48_Rotate_Image.cpp
+++ 0048_Rotate_Image/48_Rotate_Image.cpp
@@ -26,3 +26,25 @@ public:
         return;
     }
 };
+
+/**
+Runtime: 4 ms, faster than 55.70% of C++ online submissions for Rotate Image.
+Memory Usage: 7.1 MB, less than 30.50% of C++ online submissions for Rotate Image.
+*/
+class Solution {
+public:
+void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int a = 0;
+        int b = n-1;
+        while(a<b){
+            for(int i=0;i<(b-a);++i){
+                swap(matrix[a][a+i], matrix[a+i][b]);
+                swap(matrix[a][a+i], matrix[b][b-i]);
+                swap(matrix[a][a+i], matrix[b-i][a]);
+            }
+            ++a;
+            --b;
+        }
+    }
+};
