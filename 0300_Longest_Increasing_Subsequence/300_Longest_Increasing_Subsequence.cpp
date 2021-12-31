be90a17fe245e4750e8fa0aca66b890c737dbc6b
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-30 20:51:28 -0500

    Update 300_Longest_Increasing_Subsequence.cpp

--- 0300_Longest_Increasing_Subsequence/300_Longest_Increasing_Subsequence.cpp
+++ 0300_Longest_Increasing_Subsequence/300_Longest_Increasing_Subsequence.cpp
@@ -17,3 +17,27 @@ public:
         return *max_element(dp.begin(), dp.end());
     }
 };
+
+
+/**
+Runtime: 20 ms, faster than 64.31% of C++ online submissions for Longest Increasing Subsequence.
+Memory Usage: 10.4 MB, less than 62.87% of C++ online submissions for Longest Increasing Subsequence.
+*/
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i = 1; i < n; i++){
+            if(nums[i] > ans.back())
+                ans.push_back(nums[i]);
+            else{
+                auto it = lower_bound(ans.begin(), ans.end(),nums[i]); 
+                //find the 1st element that does not less than val
+                *it = nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
