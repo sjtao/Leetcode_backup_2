7651683ca75b751f9e543194f32a5ad37fffe1b1
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2021-12-30 21:13:08 -0500

    Create 392_Is_Subsequence.cpp

---
+++ 0392_Is_Subsequence/392_Is_Subsequence.cpp
@@ -0,0 +1,29 @@
+/**
+Runtime: 8 ms, faster than 10.52% of C++ online submissions for Is Subsequence.
+Memory Usage: 7.9 MB, less than 9.59% of C++ online submissions for Is Subsequence.
+*/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        //DP
+        int ns = s.length();
+        int nt = t.length();
+        if(ns == 0) return true;
+        if(nt < ns) return false;
+        
+        vector<vector<int>> dp(ns+1, vector<int>(nt+1, 0));
+        
+        for(int i = 1; i < nt+1; i++){
+            for(int j = 1; j < ns+1; j++){
+                if(s[j-1] == t[i-1])
+                    dp[j][i] = dp[j-1][i-1]+1;
+                else
+                    dp[j][i] = max(dp[j][i-1], dp[j-1][i]);
+            }
+            if(dp[ns][i] == ns) return true;
+        }
+        
+        return false;
+    }
+};
