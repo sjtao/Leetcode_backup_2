addc997431f8b1f1d2a3d69882b005c88f0195dd
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-04 17:01:14 -0500

    Update 47_Permutations_II.cpp

--- 0047_Permutations_II/47_Permutations_II.cpp
+++ 0047_Permutations_II/47_Permutations_II.cpp
@@ -34,3 +34,31 @@ public:
         
     }
 };
+
+/**
+Runtime: 4 ms, faster than 92.85% of C++ online submissions for Permutations II.
+Memory Usage: 9 MB, less than 64.33% of C++ online submissions for Permutations II.
+*/
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> perms;
+        permute(nums, 0, perms);
+        return perms;
+    }
+private:
+    void permute(vector<int> nums, int start, vector<vector<int>>& perms) {
+        int n = nums.size();
+        if (start == n - 1) {
+            perms.push_back(nums);
+        } else {
+            for (int i = start; i < n; i++) {
+                if ((i == start) || (nums[i] != nums[start])) {
+                    swap(nums[i], nums[start]);
+                    permute(nums, start + 1, perms);
+                }
+            }
+        }
+    }
+};
