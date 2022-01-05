741faa9e50857a2dc1ae4da1ac1673d66b3828d5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-04 21:36:33 -0500

    Update 213_House_Robber_II.cpp

--- 0213_House_Robber_II/213_House_Robber_II.cpp
+++ 0213_House_Robber_II/213_House_Robber_II.cpp
@@ -29,3 +29,28 @@ public:
         return max(r1, r2);
     }
 };
+
+/**
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for House Robber II.
+Memory Usage: 7.9 MB, less than 35.76% of C++ online submissions for House Robber II.
+*/
+class Solution {
+public:
+    int n;
+    int helper(vector<int>& nums, int s, int e){
+        vector<int> dp(n, 0);
+        dp[s] = nums[s];
+        dp[s+1] = max(nums[s], nums[s+1]);
+        for(int i = s+2; i < e; i++){
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[e-1];
+    }
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        return max(helper(nums, 0, n-1), helper(nums, 1, n));
+    }
+};
