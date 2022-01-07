7315cba61f7174a262870ed846f926ae805a0a07
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-07 14:24:18 -0500

    Time: 41 ms (17.12%), Space: 11.6 MB (51.94%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -3,26 +3,25 @@ public:
     string minRemoveToMakeValid(string s) {
         int n = s.length();
         stack<int> st;
-        vector<int> rm(n, 0);
         for(int i = 0; i < n; i++){
             if(s[i] == '(')
                 st.push(i);
             if(s[i] == ')'){
                 if(st.empty())
-                    rm[i] = 1;
+                    s[i] = '#';
                 else
                     st.pop();
             }
         }
         
         while(!st.empty()){
-            rm[st.top()] = 1;
+            s[st.top()] = '#';
             st.pop();
         }
         
         string a = "";
         for(int i = 0; i < n; i++){
-            if(rm[i] == 0)
+            if(s[i] != '#')
                 a += s[i];
         }
         
