2d7742b23e93b290bb12d46b4f35f4b9f8f21b64
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-12 11:14:31 -0500

    Time: 0 ms (100.00%), Space: 11.2 MB (48.64%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sold = INT_MIN, hold = INT_MIN, reset = 0;
+        int presold;
+        for(int p : prices){
+            presold = sold;
+            sold = hold + p;
+            hold = max(hold, reset-p);//hold or buy a new one if no hold before
+            reset = max(reset, presold);//no transactions 
+        }
+        return max(sold, reset);
+        
+    }
+};
