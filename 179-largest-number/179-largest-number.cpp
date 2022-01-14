5d84647a1128409e4a58c625946395dce79c37fb
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-14 18:00:20 -0500

    Time: 12 ms (50.43%), Space: 11.3 MB (53.60%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -8,13 +8,11 @@ public:
             nm[i] = to_string(nums[i]);
         
         sort(nm.begin(), nm.end(), [](string &s1, string &s2){ return s1+s2>s2+s1; });
+        if (nm[0] == "0") return "0";
         
         string t = "";
         for(int i = 0; i< n; i++)
             t += nm[i];
-        
-        while(t[0] == '0' && t.length() > 1)
-            t.erase(0,1);
         
         return t;
     }
