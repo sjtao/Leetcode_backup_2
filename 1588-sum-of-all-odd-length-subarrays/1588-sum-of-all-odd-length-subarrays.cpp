1807680407e9fceca7e1a39df948d9a9fb3b73a2
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-15 21:51:45 -0500

    Time: 0 ms (100.00%), Space: 8.4 MB (39.47%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
-    int vectorsum(vector<int>& arr){
-        int sum = 0;
-        if(arr.size() % 2 == 1){
-            for(int a:arr)
-                sum += a;
-        }
-        return sum;
-    }
-    
     int sumOddLengthSubarrays(vector<int>& arr) {
         int n = arr.size();
+        //count the frequency of each number showing in an odd-length array
         int sum = 0;
-        vector<int> sub;
-        for(int i = 0; i < n; i++){
-            int loc_sum = arr[i];
-            sub.push_back(arr[i]);
-            for(int j = i+1; j < n; j++){
-                sub.push_back(arr[j]);
-                loc_sum += vectorsum(sub);
-            }
-            sub.clear();
-            sum += loc_sum;
+        for(int k = 0; k < n; ++k){
+            sum += ((n-k)*(k+1) + 1) / 2 * arr[k];
         }
         return sum;
     }
