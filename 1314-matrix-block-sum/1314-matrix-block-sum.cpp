97758cc3d9bcbd7679f627ca0c192e4c4c379323
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-18 16:38:52 -0500

    Time: 867 ms (8.61%), Space: 9.4 MB (54.89%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int m, n;
+    int blocksum(vector<vector<int>>& mat, int i, int j, int k){
+        int s = 0;
+        for(int r = max(0, i-k); r <= min(i+k, m-1); r++){
+            for(int c = max(0, j-k); c <= min(j+k, n-1); c++){
+                s += mat[r][c];
+            }
+        } 
+        return s;
+    }
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        m = mat.size();
+        n = mat[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = blocksum(mat, i, j, k);
+            }
+        }
+        
+        return dp;        
+    }
+};
