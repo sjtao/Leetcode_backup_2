8e55541adecac9ae7283ff3e7c13182bbf956f8c
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-19 23:04:07 -0500

    Time: 8 ms (76.29%), Space: 6.1 MB (75.22%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -3,21 +3,26 @@ private:
     const int modu = pow(10, 9) + 7;
 public:
     int countVowelPermutation(int n) {
-        vector<vector<long>> dp(n, vector<long>(5, 1));
+        vector<long> dp(5, 1);
         
         //n = 1: a  e  i   o  u
         //n = 2: e ai aeou iu a
-        for(int i = 1; i < n; i++){
-            dp[i][0] = dp[i-1][1] % modu;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % modu;
-            dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][3] + dp[i-1][4]) % modu;
-            dp[i][3] = (dp[i-1][2] + dp[i-1][4]) % modu;
-            dp[i][4] = dp[i-1][0] % modu;
+        for(int k = 1; k < n; k++){
+            long a = dp[0];
+            long e = dp[1];
+            long i = dp[2];
+            long o = dp[3];
+            long u = dp[4];
+            dp[0] = e % modu;
+            dp[1] = (a + i) % modu;
+            dp[2] = (a + e + o + u) % modu;
+            dp[3] = (i + u) % modu;
+            dp[4] = a % modu;
         }
         
         long ans = 0;
         for(int i = 0; i < 5; i++)
-            ans += dp[n-1][i];
+            ans += dp[i];
         
         return ans % modu;
     }
