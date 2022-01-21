31a1800b027d87816c94510755bf210cdac1a474
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-21 11:46:35 -0500

    Time: 20 ms (36.29%), Space: 13.5 MB (11.29%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int> row, col;
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row.insert(i);
+                    col.insert(j);
+                }
+            }
+        }
+        
+        for(auto i : row){
+            for(int j = 0; j < n; j++)
+                matrix[i][j] = 0;
+        }
+        
+        for(auto j : col){
+            for(int i = 0; i < m; i++)
+                matrix[i][j] = 0;
+        }
+        
+        return;
+    }
+};
