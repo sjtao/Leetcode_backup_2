d8603dd748da10bcca2e4b92c6c6434bf8c5532a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-21 20:42:17 -0500

    Time: 6 ms (17.42%), Space: 6.6 MB (91.69%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<unsigned int> dp(target+1, 0);
+        dp[0] = 1;
+        
+        for(int j = 1; j <= target; ++j){
+            for(int i = 0; i < n; ++i){
+                if(j >= nums[i])
+                    dp[j] += dp[j - nums[i]];
+            }
+        }
+        
+        return dp[target];
+    }
+};
