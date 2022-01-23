a520174362259d4770fb642d4becccce5dd2d217
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-23 14:58:24 -0500

    Time: 193 ms (17.72%), Space: 48.6 MB (23.64%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode * getmid(ListNode* head){
+        ListNode *midpre = nullptr;
+        while(head && head->next){
+            midpre = (midpre == nullptr) ? head : midpre->next;
+            head = head->next->next;
+        }
+        ListNode *mid = midpre->next;
+        midpre->next = nullptr;
+        return mid;
+    }
+    
+    ListNode* merge(ListNode *list1, ListNode *list2){
+        ListNode *dummy = new ListNode(0);
+        ListNode *p = dummy;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                p->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                p->next = list2;
+                list2 = list2->next;
+            }
+            p = p->next;
+        }
+        if(list1) p->next = list1;
+        if(list2) p->next = list2;
+        
+        return dummy->next;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next) return head;
+        //merge sort
+        //find the middle of the linked list
+        ListNode *mid = getmid(head);
+        ListNode *left = sortList(head);
+        ListNode *right = sortList(mid);
+        //merge left and right
+        return merge(left, right);
+    }
+};
