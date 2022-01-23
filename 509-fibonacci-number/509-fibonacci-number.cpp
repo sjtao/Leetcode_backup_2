2bab48c4a9442ec4b731115418c91c8f3d53ad97
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-22 21:29:50 -0500

    Time: 0 ms (100.00%), Space: 6.4 MB (9.65%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    unordered_map<int, int> mp;
+    int helper(int n){
+        if(n <= 1) return n;
+        if(mp.find(n) == mp.end())
+            mp[n] = helper(n-1) + helper(n-2);
+        return mp[n];
+    }
+    int fib(int n) {
+        return helper(n);
+    }
+};
