3db4d3a5d112f0cd2c8936eb304d188d461f0971
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-26 19:56:00 -0500

    Time: 21 ms (38.46%), Space: 20.4 MB (24.41%) - LeetHub

--- 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.cpp
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.cpp
@@ -15,21 +15,24 @@ public:
         vector<int> ans;
         queue<TreeNode*> q;
         q.push(root);
+        
         while(!q.empty()){
-            TreeNode * node = q.front(); q.pop();
-            if(node->left != nullptr && node->right != nullptr){
-                q.push(node->left);
-                q.push(node->right);
-            }
-            else if(node->left != nullptr && node->right == nullptr){
-                ans.push_back(node->left->val);
-                q.push(node->left);
-            }
-            else if(node->left == nullptr && node->right != nullptr){
+            TreeNode* node = q.front();
+            q.pop();
+            if(!node->left && node->right){
                 ans.push_back(node->right->val);
                 q.push(node->right);
             }
+            else if(node->left && !node->right){
+                ans.push_back(node->left->val);
+                q.push(node->left);
+            }
+            else if(node->left && node->right){
+                q.push(node->right);
+                q.push(node->left);
+            }
         }
+        
         return ans;
     }
 };
