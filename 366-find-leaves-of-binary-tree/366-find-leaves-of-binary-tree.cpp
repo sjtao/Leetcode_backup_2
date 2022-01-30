ea44873f946dc3bbe57540996507dbdfbd88909d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-01-29 23:41:59 -0500

    Time: 4 ms (47.07%), Space: 8.3 MB (87.77%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -1,27 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>> ans;
-    int height(TreeNode* root){
-        if(!root) return 0;
-        int level = max(height(root->left), height(root->right))+1;
-        if(ans.size() < level) ans.push_back(vector<int>());
-        ans[level-1].push_back(root->val);
-        return level;
-    }
+    vector<vector<int>> res;
+    vector<int> temp;
     vector<vector<int>> findLeaves(TreeNode* root) {
-        //dfs
-        height(root); 
-        return ans;
+        if(!root) return res;
+        while(root){
+            root = helper(root);
+            res.push_back(temp);
+            temp.resize(0);
+        }
+        return res;
+    }
+    
+    TreeNode* helper(TreeNode* root){
+        if(!root) return NULL;
+        if(!root->left && !root->right){
+            temp.push_back(root->val);
+            return NULL;
+        }
+        root->left = helper(root->left);
+        root->right = helper(root->right);
+        return root;
     }
 };
