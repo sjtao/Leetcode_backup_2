72015cac111b8820422d3756f773e1be103e9c91
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-01 22:25:26 -0500

    Time: 1304 ms (6.26%), Space: 349.7 MB (5.61%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
+    int n;
+    void twosum(vector<int>& nums, int i, vector<vector<int>>& ans){
+        unordered_set<int> s;
+        for(int j = i+1; j < n; j++){
+            int t = -(nums[i] + nums[j]);
+            if(s.count(t)){
+                ans.push_back({nums[i], t, nums[j]});
+                while(j < n-1 && nums[j] == nums[j+1]) j++;
+            }
+            s.insert(nums[j]);
+        }
+    }
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //set<vector<int>> com;
+        n = nums.size();
         vector<vector<int>> ans;
-        int n = nums.size();
+        
         if(n < 3) return ans;
         
         sort(nums.begin(), nums.end());
@@ -11,28 +24,8 @@ public:
         for(int i = 0; i < n; i++){
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            int t = nums[i];
-            int l = i+1;
-            int r = n-1;
-            while(l < r){
-                int s = nums[l] + nums[r];
-                if (s + t == 0){
-                    ans.push_back({nums[i], nums[l], nums[r]});
-                    r--; l++;
-                    while(l < r && nums[r] == nums[r+1]) r--;
-                    while(l < r && nums[l] == nums[l-1]) l++;
-                }
-                else if(s + t > 0){
-                    r--;
-                }
-                else{
-                    l++;
-                }
-            }
+            twosum(nums, i, ans);
         }
-        
-        //for(auto p : com)
-        //    ans.push_back(p);
         
         return ans;
     }
