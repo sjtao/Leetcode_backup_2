d4afca254ed04de3b2fe715f8d63bf87ff1223f3
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-01 21:38:55 -0500

    Time: 87 ms (74.21%), Space: 19.9 MB (96.48%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> com;
+        //set<vector<int>> com;
         vector<vector<int>> ans;
         int n = nums.size();
         if(n < 3) return ans;
@@ -9,22 +9,30 @@ public:
         sort(nums.begin(), nums.end());
         
         for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
             int t = nums[i];
             int l = i+1;
             int r = n-1;
             while(l < r){
                 int s = nums[l] + nums[r];
                 if (s + t == 0){
-                    com.insert({nums[i], nums[l], nums[r]});
-                    l++; r--;
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    r--; l++;
+                    while(l < r && nums[r] == nums[r+1]) r--;
+                    while(l < r && nums[l] == nums[l-1]) l++;
                 }
-                else if(s + t > 0)  r--;
-                else l++;
+                else if(s + t > 0){
+                    r--;
+                }
+                else{
+                    l++;
+                }
             }
         }
         
-        for(auto p : com)
-            ans.push_back(p);
+        //for(auto p : com)
+        //    ans.push_back(p);
         
         return ans;
     }
