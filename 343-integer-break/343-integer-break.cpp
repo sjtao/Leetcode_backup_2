05a54717392ceae490566d8054b9fdafce3a38dc
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-04 23:21:36 -0500

    Time: 0 ms (100.00%), Space: 6 MB (59.52%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        int dp;
-        int mx = 1; //k=n
-        for(int k = 2; k <= n/2+1; ++k){
-            dp = 1;
-            int res = n / k;
-            int mod = n % k;
-            dp *= pow(res, k - mod);
-            dp *= pow(res+1, mod);
+        int mx = INT_MIN;
+        for(int k = 2; k <= n/2+1; k++){
+            int dp = 1;
+            int q = n / k;
+            int r = n % k;
+            dp *= pow(q, k-r);
+            dp *= pow(q+1, r);
             mx = max(mx, dp);
         }
-        
         return mx;
     }
 };
