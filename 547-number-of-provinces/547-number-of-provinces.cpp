274f7dd952ba8fe4ac98520171a02fb38caaa8bf
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-04 22:54:27 -0500

    Time: 49 ms (21.23%), Space: 13.7 MB (87.82%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
-    int find(vector<int>& parent, int x){
-        if(parent[x] == x)
+    vector<int> root, rank;
+    int find(int x){
+        if (x == root[x])
             return x;
-        return find(parent, parent[x]);
+        return root[x] = find(root[x]);
+    }
+    
+    void unionset(int x, int y){
+        int rootx = find(x);
+        int rooty = find(y);
+        if(rootx != rooty){
+            if(rank[rootx] > rank[rooty])
+                root[rooty] = rootx;
+            else if(rank[rootx] < rank[rooty])
+                root[rootx] = rooty;
+            else{
+                root[rooty] = rootx;
+                rank[rootx] ++;
+            }
+        }
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> parent(n);
         
-        for(int i = 0; i < n; i++)
-            parent[i] = i;
+        for(int i = 0; i < n; ++i){
+            root.push_back(i);
+            rank.push_back(1);
+        }
         
-        int group = n;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(isConnected[i][j]){
-                    int p1 = find(parent, i);
-                    int p2 = find(parent, j);
-                    if(p1 != p2){ //belong to different groups and merge them
-                        parent[p1] = p2;
-                        group--;
-                    }
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                if(isConnected[i][j] && i != j){
+                    int x = find(i);
+                    int y = find(j);
+                    unionset(x, y);
                 }
             }
         }
         
+        int group = 0;
+        for(int i = 0; i < n; i++){
+            if(root[i] == i)
+                group++;
+        }
+        
         return group;
+        
     }
 };
