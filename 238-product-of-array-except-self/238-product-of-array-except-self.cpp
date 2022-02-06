853b96e74861a1b912f8b8c7309c94559f97d3e0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-05 20:18:37 -0500

    Time: 27 ms (63.21%), Space: 25.1 MB (25.46%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -6,18 +6,13 @@ public:
         vector<int> left(n, 1);
         vector<int> right(n, 1);
         
-        //left 0 - i-1;
+        //left: 0 - i-1;
+        //right: i+1, n-1
         for(int i = 1; i < n; i++){
             left[i] = left[i-1] * nums[i-1];
-        }
-        
-        //right i+1, n-1
-        for(int i = n-2; i >= 0; i--){
-            right[i] = right[i+1] * nums[i+1];
-        }
-        
-        for(int i = 0; i < n; i++){
-            ans[i] = left[i] * right[i];
+            right[n-1-i] = right[n-i] * nums[n-i];
+            ans[i] *= left[i];
+            ans[n-1-i] *= right[n-1-i];
         }
         
         return ans;
