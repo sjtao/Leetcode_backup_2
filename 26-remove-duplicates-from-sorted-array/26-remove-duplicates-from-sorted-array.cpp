6e0772233ee190a8c600d16f94cabeacc4076d34
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-05 23:14:32 -0500

    Time: 4 ms (99.08%), Space: 18.5 MB (37.68%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,17 +4,9 @@ public:
         int n = nums.size();
         if(n == 0) return 0;
         int j = 1;
-        int count = 1;
         for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i-1]){
-                count++;
-            }
-            else{
-                count = 1;
-            }
-            if(count < 2){
+            if(nums[i] != nums[i-1])
                 nums[j++] = nums[i];
-            }
         }
         return j;
     }
