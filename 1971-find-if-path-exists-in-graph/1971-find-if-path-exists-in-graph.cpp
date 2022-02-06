fd0abcf425a686dcaab334e51a449a4d4808f53d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-06 14:45:23 -0500

    Time: 486 ms (94.23%), Space: 116.7 MB (93.48%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
+    vector<int> root, rank;
+    
+    int find(int x){
+        if(x == root[x])
+            return x;
+        return root[x] = find(root[x]);
+    }
+    
+    void unionset(int x, int y){
+        int ux = find(x);
+        int uy = find(y);
+        if(ux != uy){
+            if(rank[ux] > rank[uy])
+                root[uy] = ux;
+            else if(rank[uy] > rank[ux])
+                root[ux] = uy;
+            else{
+                root[uy] = ux;
+                rank[ux]++;
+            }
+        }
+    }
+    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        int ed = edges.size();
-        vector<vector<int>> adj(n);
-        for(int i = 0; i < ed; i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
+        for(int i = 0; i < n; i++){
+            root.push_back(i);
+            rank.push_back(1);
         }
         
-        stack<int> s;
-        s.push(source);
-        vector<bool> visited(n);
-        
-        while(!s.empty()){
-            int a = s.top(); s.pop();
-            
-            if(a == destination)
-                return true;
-            
-            if(visited[a] == true)
-                continue;
-            
-            visited[a] = true;
-            for(int i : adj[a])
-                s.push(i);
+        for(int i = 0; i < edges.size(); i++){
+            unionset(edges[i][0], edges[i][1]);
         }
-        return false;
+        
+        return find(source) == find(destination);
     }
 };
