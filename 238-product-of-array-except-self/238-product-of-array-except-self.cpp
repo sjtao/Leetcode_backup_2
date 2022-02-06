41a5b8925555a4761deb277b0e58f0bac6ec4a8a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-05 20:19:48 -0500

    Time: 26 ms (64.09%), Space: 24 MB (56.74%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n,1);
-        vector<int> left(n, 1);
-        vector<int> right(n, 1);
         
         //left: 0 - i-1;
         //right: i+1, n-1
+        int left = 1, right = 1;
         for(int i = 1; i < n; i++){
-            left[i] = left[i-1] * nums[i-1];
-            right[n-1-i] = right[n-i] * nums[n-i];
-            ans[i] *= left[i];
-            ans[n-1-i] *= right[n-1-i];
+            left *= nums[i-1];
+            right *= nums[n-i];
+            ans[i] *= left;
+            ans[n-1-i] *= right;
         }
         
         return ans;
