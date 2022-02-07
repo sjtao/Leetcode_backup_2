142efcd3e156c1ca3288b8e93c92182f5f537cc8
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-06 22:03:43 -0500

    Time: 396 ms, Memory: 57.7 MB - LeetHub

--- min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,39 +1,58 @@
+class Edge{
+public:
+    int point1;
+    int point2;
+    int cost;
+    Edge(int point1, int point2, int cost): point1(point1), point2(point2), cost(cost){}
+};
+
+bool operator<(const Edge& edge1, const Edge& edge2){
+    return edge1.cost > edge2.cost;
+}
+
 class Solution {
 public:
-    int find(vector<int> &ds, int i) {
-        return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
-    }
     
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<array<int, 3>> arr;
-        vector<int> ds(n, -1);
+        // Prim's algorithm
+        int sz = points.size();
+        if(sz == 0)
+            return 0;
         
-        int d;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                arr.push_back({d, i, j});
-            }
+        priority_queue<Edge> pq;
+
+        for(int i = 1; i < sz; i++){
+            int cost = abs(points[i][0] -points[0][0]) + 
+                abs(points[i][1] -points[0][1]);
+            Edge edge(0, i, cost);
+            pq.push(edge);
         }
-        make_heap(arr.begin(), arr.end(), greater<array<int, 3>>());
+        
+        vector<bool> visited(sz);
+        visited[0] = true;
         
         int result = 0;
-        while(!arr.empty()){
-            pop_heap(begin(arr), end(arr), greater<array<int, 3>>());
-            auto [d, i, j] = arr.back();
-            arr.pop_back();
-            i = find(ds, i);
-            j = find(ds, j);
-            if(i != j){
-                result += d;
-                ds[i] += ds[j];
-                ds[j] = i;
-                if(ds[i] == -n)
-                    break;
+        int count = sz - 1;
+        while(!pq.empty() && count>0){
+            Edge edge = pq.top();
+            pq.pop();
+            int p1 = edge.point1;
+            int p2 = edge.point2;
+            int cost = edge.cost;
+            if(!visited[p2]){
+                result += cost;
+                visited[p2] = true;
+                for(int j = 0; j < sz; j++){
+                    if(!visited[j]){
+                        int cost = abs(points[j][0] -points[p2][0]) + 
+                            abs(points[j][1] -points[p2][1]);
+                        Edge edge(p2, j, cost);
+                        pq.push(edge);
+                    }
+                }
+                count--;
             }
         }
-        
         return result;
     }
 };
