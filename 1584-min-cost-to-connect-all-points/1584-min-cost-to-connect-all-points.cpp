d3f79b046b4e6ac13069f09d4b52c3fb16efdc01
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-06 22:03:22 -0500

    Time: 434 ms (57.22%), Space: 57.5 MB (81.18%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -10,71 +10,46 @@ bool operator<(const Edge& edge1, const Edge& edge2){
     return edge1.cost > edge2.cost;
 }
 
-class UnionFind{
-private:
-    vector<int> root, rank;
-    
-public:
-    UnionFind(int sz): root(sz), rank(sz){
-        for(int i = 0; i < sz; i++){
-            root[i] = i;
-            rank[i] = 1;
-        }
-    }
-    
-    int find(int x){
-        if(x == root[x])
-            return x;
-        return root[x] = find(root[x]);
-    }
-    
-    void unionSet(int x, int y){
-        int rootX = find(x);
-        int rootY = find(y);
-        if(rootX != rootY){
-            if(rank[rootX] > rank[rootY])
-                root[rootY] = rootX;
-            else if(rank[rootX] < rank[rootY])
-                root[rootX] = rootY;
-            else{
-                root[rootY] = rootX;
-                rank[rootX]++;
-            }
-        }
-    }
-    
-    bool connected(int x, int y){
-        return find(x) == find(y);
-    }
-};
-
 class Solution {
 public:
+    
     int minCostConnectPoints(vector<vector<int>>& points) {
+        // Prim's algorithm
         int sz = points.size();
         if(sz == 0)
             return 0;
         
         priority_queue<Edge> pq;
-        UnionFind uf(sz);
-        
-        for(int i = 0; i < sz; i++){
-            for(int j = i+1; j < sz; j++){
-                int cost = abs(points[i][0] -points[j][0]) + 
-                    abs(points[i][1] -points[j][1]);
-                Edge edge(i, j, cost);
-                pq.push(edge);
-            }
+
+        for(int i = 1; i < sz; i++){
+            int cost = abs(points[i][0] -points[0][0]) + 
+                abs(points[i][1] -points[0][1]);
+            Edge edge(0, i, cost);
+            pq.push(edge);
         }
+        
+        vector<bool> visited(sz);
+        visited[0] = true;
         
         int result = 0;
         int count = sz - 1;
         while(!pq.empty() && count>0){
             Edge edge = pq.top();
             pq.pop();
-            if(!uf.connected(edge.point1, edge.point2)){
-                uf.unionSet(edge.point1, edge.point2);
-                result += edge.cost;
+            int p1 = edge.point1;
+            int p2 = edge.point2;
+            int cost = edge.cost;
+            if(!visited[p2]){
+                result += cost;
+                visited[p2] = true;
+                for(int j = 0; j < sz; j++){
+                    if(!visited[j]){
+                        int cost = abs(points[j][0] -points[p2][0]) + 
+                            abs(points[j][1] -points[p2][1]);
+                        Edge edge(p2, j, cost);
+                        pq.push(edge);
+                    }
+                }
                 count--;
             }
         }
