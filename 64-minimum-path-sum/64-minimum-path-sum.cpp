b18b34e19c0211427228d51b1cf36e79b6a7e93f
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-08 21:21:15 -0500

    Time: 22 ms (14.18%), Space: 9.6 MB (86.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,12 +4,12 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        for(int i = 1; i < n; i++){
-            grid[0][i] += grid[0][i-1];
-        }
-        for(int i = 1; i < m; i++){
+        for(int i = 1; i < m; i++)
             grid[i][0] += grid[i-1][0];
-        }
+        
+        for(int i = 1; i < n; i++)
+            grid[0][i] += grid[0][i-1];
+        
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
