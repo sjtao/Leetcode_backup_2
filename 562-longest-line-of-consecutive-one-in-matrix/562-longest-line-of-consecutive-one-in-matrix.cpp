da5e712d39f5666b382b894ed1faf54dcd905860
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-08 22:47:51 -0500

    Time: 90 ms (58.96%), Space: 22.5 MB (69.01%) - LeetHub

--- 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.cpp
+++ 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.cpp
@@ -3,16 +3,25 @@ public:
     int longestLine(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(4, 0)));
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        
         int mx = 0;
         for(int i = 0; i < m; i++){
+            int old = 0;
             for(int j = 0; j < n; j++){
                 if(mat[i][j] == 1){
-                    dp[i][j][0] = i > 0 ? dp[i-1][j][0]+1 : 1; //horizontal
-                    dp[i][j][1] = j > 0 ? dp[i][j-1][1]+1 : 1; //vertical
-                    dp[i][j][2] = i > 0 && j > 0 ? dp[i-1][j-1][2]+1 : 1; //diagonal
-                    dp[i][j][3] = i > 0 && j < n-1 ? dp[i-1][j+1][3]+1 : 1; //anti-diagonal
-                    mx = max(mx, *max_element(dp[i][j].begin(), dp[i][j].end()));
+                    dp[j][0] = j > 0 ? dp[j-1][0]+1 : 1; //horizontal
+                    dp[j][1] = i > 0 ? dp[j][1]+1 : 1; //vertical
+                    int prev = dp[j][2];
+                    dp[j][2] = i > 0 && j > 0 ? old+1 : 1; //diagonal
+                    old = prev;
+                    dp[j][3] = i > 0 && j < n-1 ? dp[j+1][3]+1 : 1; //anti-diagonal
+                    mx = max(mx, *max_element(dp[j].begin(), dp[j].end()));
+                }
+                else{
+                    old = dp[j][2];
+                    for(int k = 0; k < 4; k++)
+                        dp[j][k] = 0;
                 }
             }
         }
