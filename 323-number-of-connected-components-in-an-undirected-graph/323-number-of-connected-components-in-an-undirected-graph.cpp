c11c52f2b267e31b85646ecec7024e68ea82d821
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-10 22:36:20 -0500

    Time: 18 ms (83.91%), Space: 12.9 MB (50.15%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    int find(vector<int>& parent, int x){
-        return parent[x] == x ? x : find(parent, parent[x]);
+    vector<int> root, rank;
+    int find(int x){
+        if(x == root[x])
+            return x;
+        return root[x] = find(root[x]);
     }
-    int countComponents(int n, vector<vector<int>>& edges) {
-        int m = edges.size();
-        int group = n;
-        
-        vector<int> parent(n);
-        for(int i = 0; i < n; i++)
-            parent[i] = i;
-        
-        for(int i = 0; i < m; i++){
-            int p1 = find(parent, edges[i][0]);
-            int p2 = find(parent, edges[i][1]);
-            if(p1 != p2){
-                parent[p1] = p2;
-                group--;
+    
+    void unionset(int x, int y, int& group){
+        int rx = find(x);
+        int ry = find(y);
+        if(rx != ry){
+            if(rank[rx] > rank[ry])
+                root[ry] = rx;
+            else if(rank[rx] < rank[ry])
+                root[rx] = ry;
+            else{
+                root[rx] = ry;
+                rank[ry]++;
             }
+            group--;
+        }
+    }
+    
+    bool connection(int x, int y){
+        return find(x) == find(y);
+    }
+    
+    int countComponents(int n, vector<vector<int>>& edges) {
+        for(int i = 0; i < n; i++){
+            root.push_back(i);
+            rank.push_back(1);
+        }
+        int group = n;
+        for(auto e : edges){
+            unionset(e[0], e[1], group);
         }
         return group;
     }
