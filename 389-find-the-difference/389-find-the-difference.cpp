a0d3227cd2d1c833d42e454ce1f9b4125fa6ac00
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-10 21:49:21 -0500

    Time: 5 ms (36.11%), Space: 6.5 MB (87.33%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> v(26,0);
-        for(char c : s)
-            v[c-'a']++;
+        int sum = 0;
         for(char c : t)
-            v[c-'a']--;
-        int i;
-        for(i = 0; i < 26; i++){
-            if(v[i]!=0)
-                break;
-        }
-        return i+'a';
+            sum += c;
+        for(char c : s)
+            sum -= c;
+        
+        return (char)sum;
     }
 };
