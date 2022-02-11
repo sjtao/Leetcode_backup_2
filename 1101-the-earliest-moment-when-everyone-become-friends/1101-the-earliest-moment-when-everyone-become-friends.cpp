7c5f4b11a4da0b605747ea51aac4f1f55b6757bd
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-10 22:32:57 -0500

    Time: 42 ms (48.28%), Space: 13 MB (65.32%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    int find(vector<int>& parent, int x){
-        return parent[x] == x ? x : find(parent, parent[x]);
+    vector<int> root, rank;
+    int find(int x){
+        if(x == root[x])
+            return x;
+        return root[x] = find(root[x]);
+    }
+    
+    void unionset(int x, int y, int& group){
+        int rx = find(x);
+        int ry = find(y);
+        if(rx != ry){
+            if(rank[rx] > rank[ry])
+                root[ry] = rx;
+            else if(rank[rx] < rank[ry])
+                root[rx] = ry;
+            else{
+                root[rx] = ry;
+                rank[ry]++;
+            }
+            group--;
+        }
+    }
+    
+    bool connection(int x, int y){
+        return find(x) == find(y);
     }
     
     int earliestAcq(vector<vector<int>>& logs, int n) {
-        int m = logs.size();
+        int ln = logs.size();
         int group = n;
- 
+        
         sort(logs.begin(), logs.end());
         
-        vector<int> parent(n);
-        for(int i = 0; i < n; i++)
-            parent[i] = i;
-        
-        for(int i = 0; i < m; i++){
-            int p1 = find(parent, logs[i][1]);
-            int p2 = find(parent, logs[i][2]);
-            if(p1 != p2){
-                parent[p1] = p2;
-                group--;
-                if(group == 1){
-                    return logs[i][0];
-                }
-            }
+        for(int i = 0; i < n; i++){
+            root.push_back(i);
+            rank.push_back(1);
+        }
+
+        for(auto t : logs){
+            unionset(t[1], t[2], group);
+            if(group == 1)
+                return t[0];
         }
         
         return -1;
