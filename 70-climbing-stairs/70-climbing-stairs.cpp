f3b111c062c3b10158256bf95c69d04128297224
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-13 17:27:32 -0500

    Time: 0 ms (100.00%), Space: 6.7 MB (15.19%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,13 +4,11 @@ public:
     int dp_helper(int n){
         if(n <= 1)
             mp[n] = 1;
-        
-        for(int i = 2; i <= n; i++){
-            if(mp.count(i) == 0){
-                mp[i] = dp_helper(i-1) + dp_helper(i-2);
-            }
+           
+        if(mp.count(n) == 0){
+            mp[n] = dp_helper(n-1) + dp_helper(n-2);
         }
-        
+
         return mp[n];
     }
     int climbStairs(int n) {
