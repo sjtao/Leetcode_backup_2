d4d47d779fcb6e13bfeadb11009459b44b10742a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-13 17:20:43 -0500

    Time: 0 ms (100.00%), Space: 9.1 MB (29.81%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,10 +4,24 @@ public:
         if(m == 0){ nums1 = nums2; return;}
         if(n == 0) return;
             
-        for(int i = 0; i < n; i++)
-            nums1[m+i] = nums2[i];
-        
-        sort(nums1.begin(), nums1.end());
+        int p1 = m-1;
+        int p2 = n-1;
+        int i = m+n-1;
+        while(i>=0){
+            int a = p1 >= 0 ? nums1[p1] : INT_MIN;
+            int b = p2 >= 0 ? nums2[p2] : INT_MIN;
+            
+            if(a < b){
+                nums1[i] = b;
+                p2--;
+            }
+            else{
+                nums1[i] = a;
+                p1--;
+            }
+            i--;
+        }
+
         return;
     }
 };
