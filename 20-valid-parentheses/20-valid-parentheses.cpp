c7cb64af249d16d9355634341dab30b513b25271
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-13 16:50:37 -0500

    Time: 2 ms (36.79%), Space: 6.2 MB (96.34%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        int n = s.length();  
+        int n = s.length();
+        if(n <= 1) return false;
+        
         st.push(s[0]);
         int i = 1;
         while(i < n){
-            if(st.size() > 0){
+            if(s[i] == ')' || s[i] == '}' || s[i] == ']'){
+                if(st.size() == 0) return false;
                 char t = st.top();
                 if((s[i] == ')' && t == '(') ||
                    (s[i] == '}' && t == '{') ||
