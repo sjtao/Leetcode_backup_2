30741d8764bc3cd93813a498886c2734ace82762
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-14 20:14:49 -0500

    Time: 12 ms (86.27%), Space: 5.7 MB (89.10%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,6 +3,9 @@ public:
     bool isPowerOfThree(int n) {
         //n = 3^x, log(n) = x log(3)
         if (n <= 0) return false;
-        return fmod(log10(n)/log10(3), 1) == 0;
+        while(n%3 ==0){
+            n /= 3;
+        }
+        return n==1;
     }
 };
