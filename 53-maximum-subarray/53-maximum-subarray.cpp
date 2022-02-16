b6c53bf8689cd3893608a97d0aa848863879cab8
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-16 08:56:45 -0500

    Time: 167 ms (39.64%), Space: 67.7 MB (91.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
-    int devideconquer(vector<int>& nums, int l, int r){
-        if(l > r)
-            return INT_MIN;
-        
-        int m = l + (r - l)/2;
-        int cur = 0;
-        int bestleft = 0;
-        int bestright = 0;
-        for(int i = m-1; i >= l; i--){
-            cur += nums[i];
-            bestleft = max(cur, bestleft);
-        }
-        
-        cur = 0;
-        for(int i = m+1; i <= r; i++){
-            cur += nums[i];
-            bestright = max(cur, bestright);
-        }
-        
-        int bestsum = nums[m] + bestright + bestleft;
-        
-        int lefthalf = devideconquer(nums, l, m-1);
-        int righthalf = devideconquer(nums, m+1, r);
-        
-        return max(max(lefthalf, righthalf), bestsum);
-    }
-    
     int maxSubArray(vector<int>& nums) {
-        //Divide and Conquer
-        return devideconquer(nums, 0, nums.size()-1);
+        int cur = 0;
+        int best = INT_MIN;
+        for(int n : nums){
+            cur = max(cur+n, n);
+            best = max(cur, best);
+        }
+        return best == INT_MIN ? 0 : best;
     }
 };
