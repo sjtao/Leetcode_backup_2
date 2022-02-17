5eb2dd1e46829c7d72191811a5bdc38e461f788e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-16 21:50:40 -0500

    Time: 4 ms (96.24%), Space: 7.3 MB (46.94%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -7,12 +7,12 @@ public:
         vector<int> v(26, 0);
         for(char c : s)
             v[c-'a']++;
-        for(char c : t)
+        for(char c : t){
             v[c-'a']--;
-        for(int i = 0; i < 26; i++){
-            if(v[i] != 0)
+            if(v[c-'a'] < 0)
                 return false;
         }
+            
         return true;
     }
 };
