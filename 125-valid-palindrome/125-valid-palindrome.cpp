7d150754e511deefbd73ff9b6899e042753839c4
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-16 20:43:21 -0500

    Time: 4 ms (90.51%), Space: 8 MB (8.09%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int l = 0;
-        int r = s.length()-1;
-        //number ascii 48-57
-        //upper ascii 65-90
-        //lower ascii 97-122
-        while(l < r){
-            //removing all non-alphanumeric characters
-            while(l < r && !(isalnum(s[l])))
-                l++;
-            while(l < r && !(isalnum(s[r])))
-                r--;
-            //converting all uppercase letters into lowercase letters 
-            if(s[l] - 'A' >= 0 && s[l] - 'A' < 26)
-                s[l] += ('a' - 'A');
-            if(s[r] - 'A' >= 0 && s[r] - 'A' < 26)
-                s[r] += ('a' - 'A');
-            if(s[l] == s[r]){
-                l++; 
-                r--;
-            }
-            else
-                return false;
+        string f = "";
+        for(char c : s){
+            if(isalnum(c))
+                f += tolower(c);
         }
-        return true;
+        
+        string r = f;
+        reverse(f.begin(), f.end());
+        return f == r;
     }
 };
