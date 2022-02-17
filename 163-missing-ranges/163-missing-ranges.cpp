37679cfb2981584e8440c2598cfaba9760912483
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-16 21:33:11 -0500

    Time: 0 ms (100.00%), Space: 7 MB (83.14%) - LeetHub

--- 163-missing-ranges/163-missing-ranges.cpp
+++ 163-missing-ranges/163-missing-ranges.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
-    void push(vector<string>& ans, int a, int b){
-        if(a == b)
-            ans.push_back(to_string(a));
-        else
-            ans.push_back(to_string(a) + "->" + to_string(b));
+    void compare(int n1, int n2, vector<string>& ans){
+        int d = n2 - n1;
+        string s;
+        if(d <= 1)
+            return;
+        else if (d == 2)
+            s = to_string(n2-1);
+        else if(d > 2){
+            s = to_string(n1+1) + "->" + to_string(n2-1);
+        }
+        ans.push_back(s);
         return;
     }
     
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
-        int n = nums.size();
         vector<string> ans;
+        int n = nums.size();
+        
         if(n == 0){
-            push(ans, lower, upper);
+            if(lower == upper)
+                ans.push_back(to_string(lower));
+            else
+                ans.push_back(to_string(lower) + "->" + to_string(upper));
             return ans;
         }
         
-        if(lower < nums[0])
-            push(ans, lower, nums[0] - 1);
-        
-        for(int i = 1; i < n; ++i){
-            if(nums[i] - nums[i-1] == 1)
-                continue;
-            
-            push(ans, nums[i-1]+1, nums[i]-1);
+        if(lower < nums[0]){
+            if(lower == nums[0]-1)
+                ans.push_back(to_string(lower));
+            else
+                ans.push_back(to_string(lower) + "->" + to_string(nums[0]-1));
         }
+            
         
-        if(nums[n-1] < upper)
-            push(ans, nums[n-1]+1, upper);
-
+        for(int i = 1; i < n; i++)
+            compare(nums[i-1], nums[i], ans);
+        
+        if(nums[n-1] < upper){
+            if(nums[n-1]+1 == upper)
+                ans.push_back(to_string(upper));
+            else
+                ans.push_back(to_string(nums[n-1]+1) + "->" + to_string(upper));
+        }
+            
         return ans;
     }
 };
