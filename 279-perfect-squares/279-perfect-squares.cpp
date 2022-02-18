3e2a0e3a780391933fd104f9e6baea4843c36437
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-18 09:40:05 -0500

    Time: 245 ms (45.08%), Space: 9.1 MB (43.48%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> sq;
-    
-        int i = 0;
-        while(i * i <= n){
-            sq.push_back(i*i);
-            i++;
-        }
-        
-        vector<int> dp(n+1, INT_MAX);
+        int m = sqrt(n);
+        vector<int> dp(n+1, n+1);
         dp[0] = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j < sq.size(); j++){
-                if(i < sq[j])
-                    break;
-                dp[i] = min(dp[i], dp[i - sq[j]]+1);
+        for(int i = 1; i < n+1; ++i){
+            for(int j = 1; j*j <= i; ++j){
+                dp[i] = min(dp[i], dp[i-j*j]+1);
             }
         }
-        
         return dp[n];
-        
     }
 };
