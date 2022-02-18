46ff84a9e34ea91fcdae060d297246699f9e3599
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-18 11:21:50 -0500

    Time: 61 ms (17.48%), Space: 24.9 MB (92.40%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -4,10 +4,21 @@ public:
         int n = nums.size();
         k = k % n;
         if(k == 0) return;
-        
-        reverse(nums.begin(), nums.end()-k);
-        reverse(nums.end()-k, nums.end());
-        reverse(nums.begin(), nums.end());
+        int update = 0;
+        for(int i = 0; i < k && update < n; i++){
+            int oldi = nums[i];
+            int oldn;
+            int start = i;
+            do{
+                int next = (start+k) % n;
+                int oldn = nums[next];
+                nums[next] = oldi;
+                
+                oldi = oldn;
+                start = next;
+                update++;
+            }while(start != i);
+        }
         
         return;
     }
