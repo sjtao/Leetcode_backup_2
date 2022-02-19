c19820c389458890567368a7e4218a2186e22ea8
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-19 13:52:34 -0500

    Time: 3 ms (68.85%), Space: 7.2 MB (64.76%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& t, int start){
-        if(start == nums.size()){
+    int n;
+    void dfshelper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& t, int d){
+        if(d == n){
             return;
         }
-        for(int i = start; i < nums.size(); i++){
+        
+        for(int i = d; i < n; i++){
             t.push_back(nums[i]);
             ans.push_back(t);
-            helper(nums, ans, t, i+1);
+            dfshelper(nums, ans, t, i+1);
             t.pop_back();
         }
+        return;
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> t;
+        n = nums.size();
         ans.push_back(t);
-        helper(nums, ans, t, 0);
+        dfshelper(nums, ans, t, 0);
         return ans;
     }
 };
