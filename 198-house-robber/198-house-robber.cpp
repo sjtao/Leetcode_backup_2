126131e0b974bb39ee8b858dfa39798e14f499f6
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-20 15:06:04 -0500

    Time: 4 ms (36.16%), Space: 8.1 MB (6.24%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return nums[0];
+    unordered_map<int,int> mp;
+    int helper(vector<int>& nums, int n){
+        if(n == 0)
+           return nums[0];
+        if(n == 1)
+            return max(nums[0], nums[1]);
         
-        vector<int> dp(n+1, 0);
-        dp[1] = nums[0];
-        dp[2] = max(nums[0], nums[1]);
-        for(int i = 3; i <= n; i++){
-            dp[i] = max(dp[i-2] + nums[i-1], dp[i-1]);
+        if(mp.find(n) == mp.end()){
+            mp[n] = max(helper(nums, n-1), nums[n] + helper(nums,n-2));
         }
-        return dp[n];
+        
+        return mp[n];
+        
+    }
+    int rob(vector<int>& nums) {
+        return helper(nums, nums.size()-1);
     }
 };
