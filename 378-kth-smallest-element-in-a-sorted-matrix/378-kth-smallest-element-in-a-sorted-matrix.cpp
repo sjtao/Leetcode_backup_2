6667f8de28fee55d7b4acfb6490e2718db236d2c
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-20 14:38:44 -0500

    Time: 37 ms (69.92%), Space: 13 MB (99.36%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    int n;
-    int numbersmallerthanm(vector<vector<int>>& matrix, int m){
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            if(matrix[i][n-1] <= m)
-                cnt += n;
-            else if(matrix[i][0] > m)
-                continue;
-            else{
-                int j = n-1;
-                while(j >= 0 && matrix[i][j] > m){
-                    j--;
-                }
-                cnt += j+1;
-            }
-        }
-        return cnt;
-    }
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        n = matrix.size();
-        int l = matrix[0][0];
-        int r = matrix[n-1][n-1];
-        int m, count;
-        while(l <= r){
-            m = l + (r - l) / 2;
-            count = numbersmallerthanm(matrix, m);
-            if(count >= k)
-                r = m-1;
-            else if(count < k)
-                l = m+1;
-        }
+        int n = matrix.size();
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
         
-        return l;
+        while (low < high){
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+            }
+            if (cnt < k) low = mid + 1;
+            else high = mid;
+        }
+        return low;
         
     }
 };
