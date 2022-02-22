e6d98302f152b4eea5619f76b2038676937473b4
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-21 21:24:00 -0500

    Time: 64 ms (38.49%), Space: 20.6 MB (56.75%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    /*
+    int hashnum = 100;
+    int hashing(string s){
+        int num = 0;
+        for(char c : s){
+            num += (c - 'a' + 1) % hashnum;
+            num %= hashnum;
+        }
+        return num;
+    }
+    */
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+        
+        for(string s : strs){
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
