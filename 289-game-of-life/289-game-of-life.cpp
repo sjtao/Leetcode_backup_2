aca7a285338f924d452e018467b4b2bbaff333bd
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-21 21:52:11 -0500

    Time: 2 ms (45.18%), Space: 7 MB (59.02%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    pair<int, int> dirs[8] = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>> cb(m, vector<int>(n));
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                int count = 0;
+                for(int k = 0; k < 8; ++k){
+                    int x = i + dirs[k].first;
+                    int y = j + dirs[k].second;
+                    if(x < 0 || y < 0 || x > m-1 || y > n-1)
+                        continue;
+                    count += board[x][y];
+                }
+                if(board[i][j]){
+                    if(count < 2 || count > 3) cb[i][j] = 0;
+                    else if(count == 2 || count == 3) cb[i][j] = 1;
+                }
+                else{
+                    if(count == 3) cb[i][j] = 1;
+                    else cb[i][j] = 0;
+                }
+            }
+        }
+        
+        board = cb;
+        return;
+    }
+};
