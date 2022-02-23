d09efb312800333fb222daf4dc1f7e3476498015
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-22 20:54:03 -0500

    Time: 352 ms (53.53%), Space: 24.3 MB (96.41%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        //a+b+c+d = 0 --> a+b = -(c+d)
         unordered_map<int,int> mp;
-        for(int a : nums1)
-            for(int b : nums2)
-                mp[a+b]++;
-        
         int ans = 0;
-        for(int c : nums3){
-            for(int d : nums4){
-                auto it = mp.find(-c-d);
-                if(it != mp.end())
-                    ans += it->second;
+        for(int a : nums1){
+            for(int b : nums2){
+                mp[a+b]++;
             }
         }
-        
+        for(int c : nums3){
+            for(int d : nums4){
+                if(mp.count(-c-d))
+                    ans += mp[-c-d];
+            }
+        }
         return ans;
-                
     }
 };
