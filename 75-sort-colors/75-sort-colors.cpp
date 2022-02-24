9246409e57682f16334a4ba7d424cd94eb900138
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-23 20:41:30 -0500

    Time: 11 ms (5.54%), Space: 8.2 MB (70.52%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return;
+        
+        int zero = 0;
+        int two = n-1;
+        
+        for(int i = 0; i < n; ++i){
+            if(i == 0 && nums[i] == 0){
+                zero++;
+                continue;
+            }
+            if(nums[i] == 0){
+                nums[i] = nums[zero];
+                nums[zero] = 0;
+                zero++;
+            }
+        }
+        
+        for(int i = n-1; i >= 0; --i){
+            if(i == n-1 && nums[i] == 2){
+                two--;
+                continue;
+            }
+            if(nums[i] == 2){
+                nums[i] = nums[two];
+                nums[two] = 2;
+                two--;
+            }
+        }
+        
+        for(int i = zero; i <= two; i++)
+            nums[i] = 1;
+        
+        return;
+    }
+};
