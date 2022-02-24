614f7a95c669975213c422d51d13a4341d2541fb
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-23 20:28:17 -0500

    Time: 4 ms (96.40%), Space: 13.6 MB (65.33%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-
+        vector<int> ans(2, -1);
         int n = nums.size();
         int l = 0;
         int r = n-1;
@@ -9,19 +9,28 @@ public:
         
         while(l <= r){
             m = l + (r - l) / 2;
-            if(nums[m] > target)
+            if(nums[m] >= target)
                 r = m-1;
-            else if(nums[m] < target)
+            else
                 l = m+1;
-            else{
-                //find target
-                int first = m, last = m;
-                while(first >= 0 && nums[first] == target) first--;
-                while(last < n && nums[last] == target) last++;
-                return {first+1, last-1};
-            }
+            
+            if(nums[m] == target)
+                ans[0] = m;
         }
         
-        return {-1, -1};
+        l = 0;
+        r = n - 1;
+        while(l <= r){
+            m = l + (r - l) / 2;
+            if(nums[m] > target)
+                r = m-1;
+            else
+                l = m+1;
+            
+            if(nums[m] == target)
+                ans[1] = m;
+        }
+        
+        return ans;
     }
 };
