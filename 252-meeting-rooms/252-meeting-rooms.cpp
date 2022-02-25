f062132eae696426c41e3946758bbc63c2886f47
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-25 15:46:16 -0500

    Time: 42 ms (17.50%), Space: 11.7 MB (78.16%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.cpp
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -4,13 +4,10 @@ public:
         if(intervals.size() == 0) return true;
         
         sort(intervals.begin(), intervals.end());
-        int t = intervals[0][1];
+
         for(int i = 1; i < intervals.size(); ++i){
-            if(intervals[i][0] < t)
+            if(intervals[i][0] < intervals[i-1][1])
                 return false;
-            else{
-                t = intervals[i][1];
-            }
         }
         
         return true;
