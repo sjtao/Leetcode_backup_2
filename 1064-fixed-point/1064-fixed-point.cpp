a1da6f8f1f9bd2e85f7d1cce685f9c59e0924bc9
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-25 16:13:48 -0500

    Time: 9 ms (75.08%), Space: 10.6 MB (16.65%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.cpp
+++ 1064-fixed-point/1064-fixed-point.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     int fixedPoint(vector<int>& arr) {
-        for(int i = 0; i < arr.size(); i++){
-            if(i == arr[i])
-                return i;
+        int l = 0;
+        int r = arr.size()-1;
+        int m;
+        int ans = arr.size();
+        while(l <= r){
+            m = l + (r-l)/2;
+            if(arr[m] == m){
+                ans = min(ans, m);
+                r = m-1;
+            }
+            else if(arr[m] > m){
+                r = m-1;
+            }
+            else{
+                l = m+1;
+            }
         }
-        return -1;
+        return ans == arr.size() ? -1 : ans;
     }
 };
