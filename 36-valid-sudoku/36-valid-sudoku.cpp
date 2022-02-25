76e84dd41139592e98fe91c9784714dd0541146d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-24 21:16:01 -0500

    Time: 12 ms (99.31%), Space: 18.1 MB (65.19%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,47 +3,24 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = board.size();
         if(n != 9) return false;
-        unordered_set<char> st;
-        //row
+        bool row[9][9] = {false};
+        bool col[9][9] = {false};
+        bool box[9][9] = {false};
+
         for(int i = 0; i < 9; ++i){
-            st.clear();
             for(int j = 0; j < 9; ++j){
                 if(board[i][j] != '.'){
-                    if(st.count(board[i][j]))
+                    int x = board[i][j] - '0';
+                    x -= 1; //sudoku values: 1 - 9 --> index 0-8
+                    if(row[i][x] || col[j][x] || box[i/3*3+j/3][x])//already existed
                         return false;
-                    st.insert(board[i][j]);
+                    row[i][x] = true;
+                    col[j][x] = true;
+                    box[i/3*3+j/3][x] = true;
                 }
             }
         }
-        
-        //col
-        for(int i = 0; i < 9; ++i){
-            st.clear();
-            for(int j = 0; j < 9; ++j){
-                if(board[j][i] != '.'){
-                    if(st.count(board[j][i]))
-                        return false;
-                    st.insert(board[j][i]);
-                }
-            }
-        }
-        
-        //3*3 box
-        for(int i = 0; i < 9; i += 3){
-            for(int j = 0; j < 9; j+=3){
-                st.clear();
-                for(int r = i; r < i+3; ++r){
-                    for(int l = j; l < j+3; ++l){
-                        if(board[r][l] != '.'){
-                            if(st.count(board[r][l]))
-                                return false;
-                            st.insert(board[r][l]);
-                        }
-                    }
-                }
-            }
-        }
-        
+
         return true;
     }
 };
