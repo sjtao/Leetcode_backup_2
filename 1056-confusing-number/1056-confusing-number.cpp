d41c5caa0ecdd3986fde9d7e2905278e956c4de0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-24 23:27:12 -0500

    Time: 4 ms (15.82%), Space: 6.2 MB (7.14%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.cpp
+++ 1056-confusing-number/1056-confusing-number.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    unordered_map<int, int> mp = {{0, 0}, {1,1},{2,-1},{3,-1},{4,-1},
-                                  {5,-1},{6,9},{7,-1},{8,8},{9,6}};
+    unordered_map<int, int> mp = {{0, 0}, {1,1},{6,9},{8,8},{9,6}};
     bool confusingNumber(int n) {
         int k = n;
         int r = 0;
         while(k > 0){
             int m = k % 10;
-            if(mp[m] == -1)
+            if(mp.count(m) == 0)
                 return false;
             k /= 10;
             r = 10 * r +  mp[m];
