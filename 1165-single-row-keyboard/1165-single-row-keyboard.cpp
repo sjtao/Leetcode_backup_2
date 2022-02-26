23941d2c360e5c5ec85221eaef8dab325fa6a8f6
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-26 11:16:37 -0500

    Time: 7 ms (68.86%), Space: 6.9 MB (57.14%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.cpp
+++ 1165-single-row-keyboard/1165-single-row-keyboard.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int calculateTime(string keyboard, string word) {
-        unordered_map<char, int> mp;
+        vector<int> mp(26, -1);
         for(int i = 0; i < keyboard.length(); ++i){
-            mp[keyboard[i]] = i; 
+            mp[keyboard[i]-'a'] = i; 
         }
         
         int dis = 0;
         int i = 0, j = 0;
         for(char s : word){
-            j = mp[s];
+            j = mp[s-'a'];
             dis += abs(i - j);
             i = j;
         }
