e98d86e76a2bf7c8b4e9ea53036921772d31228e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-26 11:48:44 -0500

    Time: 4 ms (60.63%), Space: 8.7 MB (88.98%) - LeetHub

--- 760-find-anagram-mappings/760-find-anagram-mappings.cpp
+++ 760-find-anagram-mappings/760-find-anagram-mappings.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> anagramMappings(vector<int>& nums1, vector<int>& nums2) {
+        /*
+        //if don't distinguish duplicates
         unordered_map<int,int> mp;
         int n = nums1.size();
         for(int j = 0; j < n; ++j){
@@ -9,6 +11,19 @@ public:
         vector<int> map(n, 0);
         for(int i = 0; i < n; i++){
             map[i] = mp[nums1[i]];
+        }
+        return map;
+        */
+        //if distinguish duplicates
+        unordered_map<int,vector<int>> mp;
+        int n = nums1.size();
+        for(int j = 0; j < n; ++j){
+            mp[nums2[j]].push_back(j);
+        }
+        vector<int> map(n, 0);
+        for(int i = 0; i < n; i++){
+            map[i] = mp[nums1[i]].front();
+            mp[nums1[i]].erase(mp[nums1[i]].begin());
         }
         return map;
     }
