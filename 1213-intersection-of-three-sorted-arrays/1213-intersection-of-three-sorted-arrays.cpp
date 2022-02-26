4081ec6ad59830918202e0232674c4123b9672a3
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-25 23:26:48 -0500

    Time: 23 ms (29.28%), Space: 11.8 MB (59.01%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
     vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
-        for(int a : arr1)
-            mp[a]++;
-        for(int a : arr2)
-            mp[a]++;
-        for(int a : arr3)
-            mp[a]++;
-        
         vector<int> ans;
-        for(int a : arr1){
-            if(mp[a] == 3)
-                ans.push_back(a);
+        int p1 = 0, p2 = 0, p3 = 0;
+        while(p1 < arr1.size() && p2 < arr2.size() && p3 < arr3.size()){
+            if(arr1[p1] == arr2[p2] && arr2[p2] == arr3[p3]){
+                ans.push_back(arr1[p1]);
+                p1++;
+                p2++;
+                p3++;
+            }
+            else{
+                if(arr1[p1] < arr2[p2])
+                    p1++;
+                else if(arr2[p2] < arr3[p3]){
+                    p2++;
+                }
+                else
+                    p3++;
+            }
         }
         
         return ans;
