e194cd8e58eb18d0d852628166b77b47b25e13ef
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-02-25 22:35:48 -0500

    Time: 5 ms (49.35%), Space: 8.1 MB (85.71%) - LeetHub

--- 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.cpp
+++ 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.cpp
@@ -4,12 +4,16 @@ public:
         int n = arr.size();
         int d = (arr[n-1] - arr[0])/n;
         if (d == 0) return arr[0];
-        int e;
-        for(int i = 1; i < n; i++){
-            e = arr[i-1] + d;
-            if(e != arr[i])
-                break;
-        }
-        return e;
+        int l = 0;
+        int r = n-1;
+        int m;
+        while(l < r){
+            m = l + (r-l)/2;
+            if(arr[m]==arr[0]+m*d)
+                l = m+1;
+            else
+                r = m;
+      }
+        return arr[0] + l * d;
     }
 };
