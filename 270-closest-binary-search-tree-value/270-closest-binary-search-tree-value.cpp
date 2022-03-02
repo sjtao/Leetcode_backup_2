b37c72b3808bfea7cd27be414a3082df39a8d770
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-01 21:45:24 -0500

    Time: 20 ms (32.47%), Space: 20.8 MB (95.41%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.cpp
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.cpp
@@ -11,24 +11,18 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, double target, double& distance, int& ans){
-        if(!root) return;
-        
-        if(root->left) helper(root->left, target, distance, ans);
-        
-        double d = abs(root->val - target);
-        if(d < distance){
-            ans = root->val;
-            distance = d;
-        }
-        
-        if(root->right) helper(root->right, target, distance, ans);
-        return;
-    }
     int closestValue(TreeNode* root, double target) {
-        double distance = 1e9;
-        int ans;
-        helper(root, target, distance, ans);
+        int ans = root->val;
+        double dis = abs(root->val - target);
+        double d;
+        while(root){
+            d = abs(root->val - target);
+            if(d < dis){
+                dis = d;
+                ans = root->val;
+            }
+            root = root->val < target ? root->right : root->left;
+        }
         return ans;
     }
 };
