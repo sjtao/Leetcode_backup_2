b0879a4332c51c330971bedb67da1821790b5fcf
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-04 15:25:03 -0500

    Time: 22 ms (38.65%), Space: 7.8 MB (46.90%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,31 +1,18 @@
 class Solution {
-private:
-    unordered_map<char, int> mp ={{'I', 1}, 
-    {'V', 5}, 
-    {'X', 10}, 
-    {'L', 50}, 
-    {'C', 100}, 
-    {'D', 500}, 
-    {'M', 1000}};
-
 public:
+    unordered_map<char, int> mp = {{'I',1}, {'V',5}, {'X',10}, {'L', 50}, \
+    {'C',100}, {'D',500},{'M',1000}};
+
     int romanToInt(string s) {
         int n = s.length();
         int ans = 0;
-        int i = 0;
-        while(i < n){
-            int a = mp[s[i]];
-            if(i+1 < n){
-                if((s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) ||
-                  (s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) ||
-                  (s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M'))){
-                    a = mp[s[i+1]] - a;
-                    i++;
-                }     
-            }
-            ans += a;
-            i++;
+        for(int i = 0; i < n; i++){
+            if(i < n-1 && mp[s[i]] < mp[s[i+1]])
+                ans -= mp[s[i]];
+            else
+                ans += mp[s[i]];
         }
+        
         return ans;
     }
 };
