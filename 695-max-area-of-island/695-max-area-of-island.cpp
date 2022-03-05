8b6cbb7783b9e7b2bc699cd83c157280bb5ee5eb
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-05 11:21:54 -0500

    Time: 19 ms (82.97%), Space: 23.2 MB (89.02%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
     int m, n;
-    int cnt;
     pair<int,int> dirs[4] = {{0,1}, {0,-1},{1,0},{-1,0}};
     
-    void dfs(vector<vector<int>>& grid, int x, int y){
+    int dfs(vector<vector<int>>& grid, int x, int y){
         if(x < 0 || y < 0 || x >= m || y >= n)
-            return;
+            return 0;
         
         if(grid[x][y] != 1)
-            return;
+            return 0;
         
-        grid[x][y] = cnt;
+        grid[x][y] = 2;
+        
+        int num = 1;
         for(int k = 0; k < 4; ++k){
             int kx = x + dirs[k].first;
             int ky = y + dirs[k].second;
-            dfs(grid, kx, ky);
+            num += dfs(grid, kx, ky);
         }
         
-        return;
+        return num;
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         //dfs
         m = grid.size();
         n = grid[0].size();
-        cnt = 2;
+        int ans = -1;
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
                 if(grid[i][j] == 1){
-                    dfs(grid, i, j);
-                    cnt++;
+                    ans = max(ans, dfs(grid, i, j));
                 }
             }
         }
-        
-        vector<int> rep(cnt, 0);
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                rep[grid[i][j]]++;
-            }
-        }
-        
-        int ans = -1;
-        for(int c = 2; c < cnt; ++c)
-            ans = max(rep[c], ans);
         
         return ans == -1 ? 0 : ans;
         
