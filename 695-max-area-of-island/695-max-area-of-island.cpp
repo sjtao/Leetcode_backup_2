7b2d75d4e1593374f756822497bbb9b9b4e8b8b3
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-05 12:11:58 -0500

    Time: 45 ms (18.31%), Space: 25.9 MB (20.48%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,41 +1,71 @@
 class Solution {
 public:
-    int m, n;
-    pair<int,int> dirs[4] = {{0,1}, {0,-1},{1,0},{-1,0}};
-    
-    int dfs(vector<vector<int>>& grid, int x, int y){
-        if(x < 0 || y < 0 || x >= m || y >= n)
-            return 0;
-        
-        if(grid[x][y] != 1)
-            return 0;
-        
-        grid[x][y] = 2;
-        
-        int num = 1;
-        for(int k = 0; k < 4; ++k){
-            int kx = x + dirs[k].first;
-            int ky = y + dirs[k].second;
-            num += dfs(grid, kx, ky);
-        }
-        
-        return num;
+    vector<int> root, rank, size;
+    int find(int x){
+        if(x == root[x])
+            return x;
+        return root[x] = find(root[x]);
     }
     
+    void unionset(int x, int y){
+        int rx = find(x);
+        int ry = find(y);
+        if(rx != ry){
+            if(rank[rx] > rank[ry]){
+                root[ry] = rx;
+                size[rx] += size[ry];
+            }   
+            else if(rank[rx] < rank[ry]){
+                root[rx] = ry;
+                size[ry] += size[rx];
+            }     
+            else{
+                root[rx] = ry;
+                rank[ry] ++;
+                size[ry] += size[rx];
+            }
+        }
+
+        return;
+    }
+    
+    pair<int,int> dirs[4] = {{0,1},{0,-1},{1,0},{-1,0}};
+    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        //dfs
-        m = grid.size();
-        n = grid[0].size();
-        int ans = -1;
+        int m = grid.size();
+        int n = grid[0].size();
+        
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
                 if(grid[i][j] == 1){
-                    ans = max(ans, dfs(grid, i, j));
+                    root.push_back(i * n + j);
+                    rank.push_back(1);
+                    size.push_back(1);
                 }
+                else{
+                    root.push_back(-1);
+                    rank.push_back(-1);
+                    size.push_back(-1);
+                }
+                
             }
         }
         
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid[i][j] == 1){
+                    for(int k = 0; k < 4; ++k){
+                        int kx = i + dirs[k].first;
+                        int ky = j + dirs[k].second;
+                        if(kx < 0 || ky < 0 || kx >= m || ky >= n)
+                            continue;
+                        if(grid[kx][ky] == 1)
+                            unionset(i*n+j, kx*n+ky);
+                    }
+                }
+            }
+        }
+        int ans = *max_element(size.begin(), size.end());
         return ans == -1 ? 0 : ans;
-        
     }
 };
