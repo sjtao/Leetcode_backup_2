8dedff38475d1277256f810eed279a768841f6e4
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-06 21:11:16 -0500

    Time: 44 ms (67.24%), Space: 21.9 MB (23.12%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
@@ -7,7 +7,7 @@ public:
         int p = 0;
         for(int i = n-1; i >= 2; --i){
             if(nums[i-2] + nums[i-1] > nums[i]){
-                p = max(p, nums[i-2] + nums[i-1] + nums[i]);
+                p = nums[i-2] + nums[i-1] + nums[i];
                 break;
             }
         }
