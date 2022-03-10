cfb019175a5aaed42f8cb0a38bd58db1b46b668c
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-09 20:55:10 -0500

    Time: 111 ms (21.99%), Space: 21.6 MB (88.47%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     pair<int,int> dirs[4] = {{0,1},{0,-1},{1,0},{-1,0}};
+    int m, n;
+    
+    void dfs(vector<vector<int>>& grid, int x, int y){
+        if(x < 0 || y < 0 || x >= m || y >= n || grid[x][y] != 1)
+            return;
+        
+        grid[x][y] = 0;
+        for(int k = 0; k < 4; ++k){
+            int kx = x + dirs[k].first;
+            int ky = y + dirs[k].second;
+            dfs(grid, kx, ky);
+        }
+        
+        return;
+    }
     
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int,int>> q;
+        m = grid.size();
+        n = grid[0].size();       
         
         for(int i = 0; i < m; ++i){
-            if(grid[i][0] == 1)
-                q.push({i,0});
-            if(grid[i][n-1] == 1)
-                q.push({i,n-1});
-        }
-        
-        for(int i = 1; i < n-1; ++i){
-            if(grid[0][i] == 1)
-                q.push({0,i});
-            if(grid[m-1][i] == 1)
-                q.push({m-1,i});
-        }
-        
-        while(!q.empty()){
-            auto p = q.front();
-            q.pop();
-            int x = p.first;
-            int y = p.second;
-            if(grid[x][y] == 0)
-                continue;
-            grid[x][y] = 0;
-            for(int k = 0; k < 4; ++k){
-                int kx = x + dirs[k].first;
-                int ky = y + dirs[k].second;
-                if(kx < 0 || ky < 0 || kx >= m || ky >= n || grid[kx][ky] == 0)
-                    continue;
-                q.push({kx, ky});
+            for(int j = 0; j < n; ++j){
+                if((i == 0 || i == m-1 || j == 0 || j == n-1) && grid[i][j] == 1)
+                    dfs(grid, i, j);
             }
         }
         
