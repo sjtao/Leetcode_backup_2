186aecc4bcac2a0d6d8b2ec6c117cde62992c078
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-11 10:52:20 -0500

    Time: 8 ms, Memory: 13.8 MB - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int mn = 1, mx = 1, mno, mxo;
+        int best = INT_MIN;
+        for(int n : nums){
+            mno = mn;
+            mxo = mx;
+            mn = min(min(mno * n, mxo * n), n);
+            mx = max(max(mno * n, mxo * n), n);
+            best = max(mx, best);
+        }
+        return best;
+    }
+};
