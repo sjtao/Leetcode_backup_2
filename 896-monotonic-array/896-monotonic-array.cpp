88fa58d8fea6dce4c7059612601cd8f0f24bc613
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-15 21:19:25 -0400

    Time: 152 ms (91.29%), Space: 96.5 MB (68.19%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -6,8 +6,8 @@ public:
         bool increase = true;
         bool decrease = true;
         for(int i = 0; i < n-1; ++i){
-            if(nums[i+1] > nums[i]) decrease &= false;
-            if(nums[i+1] < nums[i]) increase &= false;
+            if(nums[i+1] > nums[i]) decrease = false;
+            if(nums[i+1] < nums[i]) increase = false;
             
         }
         return increase || decrease;
