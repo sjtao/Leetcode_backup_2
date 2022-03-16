b0cb2ccf15a1a91f66760da66d0015c407aaa785
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-15 21:28:42 -0400

    Time: 197 ms (45.17%), Space: 368.7 MB (11.98%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -3,18 +3,12 @@ public:
     int strStr(string haystack, string needle) {
         int n1 = haystack.length();
         int n2 = needle.length();
-        if(n1 < n2) return -1;
         if(n2 == 0) return 0;
-
+    
         for(int i = 0; i <= n1-n2; ++i){
-            int j = 0;
-            for(; j < n2; ++j){
-                if(needle[j] != haystack[i+j]){
-                    break;
-                }
-            }
-            if(j == n2)
+            if(haystack.substr(i, n2) == needle){
                 return i;
+            }
         }
         return -1;
     }
