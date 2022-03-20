4a3e5c41d6dc2578243e7aa8704758b862e64914
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-19 21:40:16 -0400

    Time: 8 ms (18.16%), Space: 7 MB (18.60%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -9,10 +9,9 @@ public:
         while(p1 >= 0 || p2 >= 0 || carry > 0){
             int aa = p1 >= 0 ? a[p1--]-'0' : 0;
             int bb = p2 >= 0 ? b[p2--]-'0' : 0;
-            int s = aa + bb + carry;
-            carry = s / 2;
-            s %= 2;
-            ans = (char)(s + '0') + ans;
+            int s = aa ^ bb ^ carry;
+            carry = (aa & bb) | (aa & carry) | (bb & carry);
+            ans = (char)(s+'0') + ans;
         }
         return ans;
     }
