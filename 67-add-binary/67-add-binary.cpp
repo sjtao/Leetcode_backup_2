240bcf5795dfccf0632ae09fd39eb24a23065230
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-19 21:14:17 -0400

    Time: 5 ms (34.25%), Space: 6.9 MB (23.22%) - LeetHub

---
+++ 67-add-binary/67-add-binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int na = a.length();
+        int nb = b.length();
+        int carry = 0;
+        int p1 = na-1, p2 = nb-1;
+        string ans = "";
+        while(p1 >= 0 || p2 >= 0 || carry > 0){
+            int aa = p1 >= 0 ? a[p1--]-'0' : 0;
+            int bb = p2 >= 0 ? b[p2--]-'0' : 0;
+            int s = aa + bb + carry;
+            carry = s / 2;
+            s %= 2;
+            ans = (char)(s + '0') + ans;
+        }
+        return ans;
+    }
+};
