704ba7037009eeecea011c1c4b9b4500f63c246e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-23 21:01:21 -0400

    Time: 80 ms (61.89%), Space: 25.7 MB (71.77%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool arithmetic(vector<int>& nums){
+        //rearrange
+        sort(nums.begin(), nums.end());
+        int d = nums[1] - nums[0];
+        for(int i = 1; i < nums.size()-1; i++){
+            if(nums[i+1] - nums[i] != d)
+                return false;
+        }
+        return true;
+    }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        int n = l.size();
+        vector<bool> ans(n);
+        for(int i = 0; i < n; i++){
+            if(r[i] - l[i] < 2)
+                ans[i] = true;
+            else{
+                vector<int> part(nums.begin()+l[i], nums.begin()+r[i]+1);
+                ans[i] = arithmetic(part);
+            }
+            
+        }
+        return ans;
+    }
+};
