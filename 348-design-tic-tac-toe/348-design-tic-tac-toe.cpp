394f9508c6fe7c7adf54dc3229f237b655f6cd11
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-23 21:52:26 -0400

    Time: 42 ms (41.40%), Space: 19.3 MB (42.96%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
@@ -1,43 +1,30 @@
 class TicTacToe {
 public:
-    vector<vector<int>> board;
+    vector<int> Row;
+    vector<int> Col;
+    int diag, antidiag;
     int n;
     TicTacToe(int n) {
-        board.assign(n, vector<int>(n, 0));
+        Row.assign(n, 0);
+        Col.assign(n, 0);
+        diag = 0;
+        antidiag = 0;
         this->n = n;
     }
     
     int move(int row, int col, int player) {
-        board[row][col] = player;
-        int h = 1, v = 1, d = 1, rd = 1;
-        for(int i = 0; i < n; ++i){
-            if(board[row][i] != player){
-                h = 0;
-                break;
-            }
-        }
-        for(int i = 0; i < n; ++i){
-            if(board[i][col] != player){
-                v = 0;
-                break;
-            }
-        }
-        for(int i = 0; i < n; ++i){
-            if(board[i][i] != player){
-                d = 0;
-                break;
-            }
-        }
-        for(int i = 0; i < n; ++i){
-            if(board[i][n-1-i] != player){
-                rd = 0;
-                break;
-            }
-        }
-        if(h + v + d + rd == 0)
-            return 0;
+        int d = player == 1 ? 1 : -1;
+        Row[row] += d;
+        Col[col] += d;
+        if(row == col)
+            diag += d;
+        if(row + col == n-1)
+            antidiag += d;
         
-        return player;
+        if(abs(Row[row]) == n || abs(Col[col]) == n || abs(diag) == n || abs(antidiag) == n)
+            return player;
+        
+        return 0;
     }
 };
 
