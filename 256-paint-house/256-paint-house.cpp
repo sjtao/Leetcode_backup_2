2656a973ef754208acf4641767c809ef30b283ef
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-24 17:03:29 -0400

    Time: 8 ms (83.57%), Space: 9.5 MB (83.90%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -3,11 +3,13 @@ public:
     
     int minCost(vector<vector<int>>& costs) {  
         int n = costs.size(); //# of houses
+        vector<int> ct = costs[n-1], ctprev = ct;
         for(int i = n-2; i >= 0; i--){
-            costs[i][0] += min(costs[i+1][1], costs[i+1][2]);
-            costs[i][1] += min(costs[i+1][0], costs[i+1][2]);
-            costs[i][2] += min(costs[i+1][0], costs[i+1][1]);
+            ct[0] = costs[i][0] + min(ctprev[1], ctprev[2]);
+            ct[1] = costs[i][1] + min(ctprev[0], ctprev[2]);
+            ct[2] = costs[i][2] + min(ctprev[0], ctprev[1]);
+            ctprev = ct;
         }
-        return *min_element(costs[0].begin(), costs[0].end());
+        return *min_element(ct.begin(), ct.end());
     }
 };
