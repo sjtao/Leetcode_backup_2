3ccb14514d4226e848ce1058aa4e312188c9ea4e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-25 11:03:49 -0400

    Time: 21 ms (20.76%), Space: 21.4 MB (7.19%) - LeetHub

---
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, vector<int>> mp;
+    //0: left
+    //1: leaf
+    //2: right
+    //3: other
+    void traverse(TreeNode* root, int flag){
+        
+        if(!root) return;
+        //leaf
+        if(!root->left && !root->right){
+            mp[1].push_back(root->val);
+            return;
+        }
+        //not leaf
+        //left
+        if(flag == 0 || flag == 2){
+            mp[flag].push_back(root->val);
+        }
+        if(flag == 0){
+            if(root->left) {
+                traverse(root->left, flag);
+                traverse(root->right, 3);
+            }
+            else{
+                traverse(root->right, flag);
+            }
+        }
+        else if(flag == 2){
+            if(root->right) {
+                traverse(root->left, 3);
+                traverse(root->right, flag);
+            }
+            else{
+                traverse(root->left, flag);
+            }
+        }
+        else{
+            traverse(root->left, 3);
+            traverse(root->right, 3);
+        }
+        
+        return;
+    }
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if(!root) return {};
+        
+        vector<int> ans;
+        ans.push_back(root->val);
+        traverse(root->left, 0);
+        traverse(root->right, 2);
+        for(int i = 0; i < mp[0].size(); ++i)
+            ans.push_back(mp[0][i]);
+        for(int i = 0; i < mp[1].size(); ++i)
+            ans.push_back(mp[1][i]);
+        for(int i = mp[2].size()-1; i >= 0; --i)
+            ans.push_back(mp[2][i]);
+        
+        return ans;
+    }
+};
