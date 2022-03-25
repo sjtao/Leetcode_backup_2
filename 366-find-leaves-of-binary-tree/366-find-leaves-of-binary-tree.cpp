39fb76cb17e2ee92ec181ccc564f1f6d27b91ef5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-25 10:15:26 -0400

    Time: 0 ms (100.00%), Space: 9.3 MB (5.09%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -1,25 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<int> temp;
-    vector<vector<int>> findLeaves(TreeNode* root) {
-        if(!root) return res;
-        while(root){
-            root = helper(root);
-            res.push_back(temp);
-            temp.resize(0);
+    unordered_map<int,vector<int>> mp;
+    int findheight(TreeNode* root){
+        if(!root) return 0;
+        if(!root->left && !root->right){
+            mp[0].push_back(root->val);
+            return 0;
         }
-        return res;
+        
+        int height = 1 + max(findheight(root->left), findheight(root->right));
+        mp[height].push_back(root->val);
+        return height;
     }
     
-    TreeNode* helper(TreeNode* root){
-        if(!root) return NULL;
-        if(!root->left && !root->right){
-            temp.push_back(root->val);
-            return NULL;
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        if(!root) return {};
+        
+        int h = findheight(root);
+        vector<vector<int>> ans;
+        for(int i = 0; i <= h; i++){
+            ans.push_back(mp[i]);
         }
-        root->left = helper(root->left);
-        root->right = helper(root->right);
-        return root;
+        return ans;        
     }
 };
