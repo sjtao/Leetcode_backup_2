c32070d907e9557aff18f220fd2bb42683fff89c
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-26 21:44:02 -0400

    Time: 38 ms (80.10%), Space: 20.6 MB (50.03%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    /*
-    int hashnum = 100;
-    int hashing(string s){
-        int num = 0;
-        for(char c : s){
-            num += (c - 'a' + 1) % hashnum;
-            num %= hashnum;
-        }
-        return num;
-    }
-    */
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
- 
         unordered_map<string, vector<string>> mp;
+        string t;
+        for(string s : strs){
+            t = s;
+            sort(s.begin(), s.end());
+            mp[s].push_back(t);
+        }
+        
         vector<vector<string>> ans;
-        
-        for(string& s : strs){
-            string t = s;
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
-        }
-        
-        for(auto& it:mp){
+        for(auto it : mp)
             ans.push_back(it.second);
-        }
         
         return ans;
     }
