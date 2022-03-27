ba0fc68d54d792a421a3cef3721a5c21a20150c9
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-26 21:36:39 -0400

    Time: 592 ms (28.16%), Space: 141.3 MB (26.30%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     unordered_map<int, vector<int>> mp; //manager: <subordinates>
     vector<int> Time, news;
+    int ans = -1;
     
     void findtime(vector<int>& m, int t){
         for(int n : m){
@@ -9,7 +10,7 @@ public:
                 findtime(mp[n], t + Time[n]);
             }
             else{
-                news.push_back(t);
+                ans = max(ans, t);
             }
         }
         return;
@@ -19,15 +20,12 @@ public:
         if(n == 1) return informTime[0];
         if(n == 2) return informTime[headID];
         
-        Time = vector<int>(informTime.begin(), informTime.end());
+        this->Time = informTime;//vector<int>(informTime.begin(), informTime.end());
         
         for(int i = 0; i < n; i++)
             mp[manager[i]].push_back(i);
         
         findtime(mp[headID], informTime[headID]);
-        sort(news.begin(), news.end());
-        
-        return news.back();
-        
+        return ans;
     }
 };
