55557625c1377db0527f4c581de71905762f263a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-03-31 22:13:16 -0400

    Time: 96 ms (90.22%), Space: 83.4 MB (48.89%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.cpp
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -4,28 +4,24 @@ public:
         if(bills[0] != 5) return false;
         
         int five = 0, ten = 0, twenty = 0;
-        for(int i = 0; i < bills.size(); ++i){
-            if(bills[i] == 5){
+        for(int b : bills){
+            if(b == 5)
                 five++;
-            }
-            else if(bills[i] == 10){
+            else if(b == 10){
+                if(five < 0) return false;
                 ten++;
-                five--;
-                if(five < 0)
-                    return false;
+                five--;  
             }
-            else if(bills[i] == 20){
+            else if(b == 20){
                 twenty++;
                 if(ten > 0 && five > 0){
                     ten--;
                     five--;
                 }
-                else if(ten == 0 && five > 2){
+                else if(ten == 0 && five > 2)
                     five -= 3;
-                }
-                else{
-                    return false;
-                }
+                else
+                    return false;  
             }
         }
         return true;
