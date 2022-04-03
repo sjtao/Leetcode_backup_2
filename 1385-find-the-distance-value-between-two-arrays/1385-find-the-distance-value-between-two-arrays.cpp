fea6fe64a2752783aa13df57504351c3a3a391c0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-02 23:03:07 -0400

    Time: 59 ms (9.36%), Space: 13.1 MB (73.30%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -4,13 +4,14 @@ public:
         int n1 = arr1.size();
         int n2 = arr2.size();
         int cnt = 0;
+        sort(arr2.begin(), arr2.end());
         for(int i = 0; i < n1; ++i){
-            int j = 0;
-            for(; j < n2; ++j){
+            int j = n2-1;
+            for(; j >= 0; --j){
                 if(abs(arr1[i] - arr2[j]) <= d)
                     break;
             }
-            if(j == n2)
+            if(j < 0)
                 cnt++;
         }
         return cnt;
