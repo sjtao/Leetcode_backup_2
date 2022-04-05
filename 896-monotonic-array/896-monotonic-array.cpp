2b4d91a2f0f1b1d355c4848e457545560cd9eed9
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-04 22:23:22 -0400

    Time: 223 ms (45.91%), Space: 96.5 MB (66.55%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -3,13 +3,13 @@ public:
     bool isMonotonic(vector<int>& nums) {
         int n = nums.size();
         if(n <= 2) return true;
+        
         bool increase = true;
         bool decrease = true;
-        for(int i = 0; i < n-1; ++i){
+        for(int i = 0; i < n-1; i++){
             if(nums[i+1] > nums[i]) decrease = false;
             if(nums[i+1] < nums[i]) increase = false;
-            
         }
-        return increase || decrease;
+        return decrease || increase;
     }
 };
