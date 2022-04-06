4af4b5121d97cda7d442bb53d8112a2cb7d10c8d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-05 22:51:52 -0400

    Time: 1390 ms (14.29%), Space: 10.9 MB (20.41%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,62 +1,14 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        int md = pow(10, 9) + 7;
-        vector<int> uniq;
-        unordered_map<int, long> mp;
-        for(int a : arr){
-            if(mp.find(a) == mp.end())
-                uniq.push_back(a);
-            mp[a]++;
-        }
-        
-        int n = uniq.size();
-        sort(uniq.begin(), uniq.end());
-        
-        long ans = 0;
-        // all different
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                int s = target - (uniq[i] + uniq[j]); 
-                if(s > uniq[j] && mp.find(s) != mp.end()){
-                    ans += (mp[uniq[i]] * mp[uniq[j]] * mp[s]) % md;
-                    ans %= md;
-                }
+        unordered_map<int, int> m;
+        int res = 0, mod = 1e9 + 7;
+        for (int i = 0; i < arr.size(); i++) {
+            res = (res + m[target - arr[i]]) % mod;
+            for (int j = 0; j < i; j++) {
+                m[arr[i] + arr[j]]++;
             }
         }
-        
-        //all the same
-        for(int i = 0; i < n; i++){
-            if(target % 3 == 0 && target / 3 == uniq[i] && mp[uniq[i]] >= 3){
-                ans += (mp[uniq[i]] * (mp[uniq[i]] - 1) * (mp[uniq[i]] - 2)) / 6;
-                ans %= md;
-            }
-        }
-        
-        // two different xxy
-        for(int i = 0; i < n; i++){
-            if(mp[uniq[i]] >= 2){
-                int s = target - 2 * uniq[i];
-                for(int j = i+1; j < n; j++){
-                    if(s == uniq[j]){
-                        ans += (mp[uniq[i]] * (mp[uniq[i]] - 1)) / 2 * mp[s];
-                        ans %= md;
-                    }
-                }  
-            }
-        }
-        
-        // two different xyy
-        for(int i = 0; i < n; i++){
-            int s = target - uniq[i];
-            for(int j = i+1; j < n; j++){
-                if(mp[uniq[j]] >= 2 && s == 2 * uniq[j]){
-                    ans += (mp[uniq[j]] * (mp[uniq[j]] - 1)) / 2 * mp[uniq[i]];
-                    ans %= md;
-                }
-            }  
-        }
-        
-        return (int) ans;
+        return res;
     }
 };
