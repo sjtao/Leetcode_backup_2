ea715f10c3bdf88d1017272e00829965f4378940
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-06 21:24:45 -0400

    Time: 9 ms (41.73%), Space: 5.8 MB (96.11%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        if(n == 1) return 1;
-        int coin = n;
-        int i = 1;
-        for(; i < n; i++){
-            coin -= i;
-            if(coin <= 0)
-                break;
-        }
-        return coin == 0 ? i : i-1;
+        return (int)(sqrt(2 * (long) n + 0.25) - 0.5);
     }
 };
