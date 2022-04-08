3c65609d4f7d29c57a59776b90952c7236533474
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-08 11:36:59 -0400

    Time: 177 ms (80.75%), Space: 105.6 MB (21.74%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
@@ -2,13 +2,28 @@ class Solution {
 public:
     int minProductSum(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end(), greater<int>());
+       //counting sort
+        vector<int> count1(101,0), count2(101,0);
+        for(int n : nums1)
+            count1[n]++;
+        for(int n : nums2)
+            count2[n]++;
         
-        int sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += (nums1[i] * nums2[i]);
+        int p1 = 0, p2 = 100, sum = 0;
+        while(p1 < 101 && p2 > 0){
+            while(p1 < 101 && count1[p1] == 0)
+                p1++;
+            while(p2 > 0 && count2[p2] == 0)
+                p2--;
+            if(p1 == 101 || p2 == 0)
+                break;
+            
+            int r = min(count1[p1], count2[p2]);
+            sum += r * (p1 * p2);
+            count1[p1] -= r;
+            count2[p2] -= r;
         }
+
         return sum;
     }
 };
