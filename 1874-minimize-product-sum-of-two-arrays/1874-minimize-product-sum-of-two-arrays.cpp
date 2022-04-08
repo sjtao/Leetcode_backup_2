cfc0d8cabe6922fca7ddc1d8fc48785f19d116b8
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-08 11:26:39 -0400

    Time: 390 ms (9.32%), Space: 105.5 MB (67.08%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int minProductSum(vector<int>& nums1, vector<int>& nums2) {
-        priority_queue<int, vector<int>, greater<int>> q(nums1.begin(), nums1.end());
-        priority_queue<int> p(nums2.begin(), nums2.end());
+        int n = nums1.size();
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
         
         int sum = 0;
-        while(!q.empty()){
-            sum += q.top() * p.top();
-            q.pop();
-            p.pop();
+        for(int i = 0; i < n; i++){
+            sum += (nums1[i] * nums2[n-1-i]);
         }
         return sum;
     }
