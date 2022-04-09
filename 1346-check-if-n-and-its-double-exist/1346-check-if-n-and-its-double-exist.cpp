5f6f7ef1261f543638662030a1a1ceaea48ac35f
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-08 22:50:32 -0400

    Time: 24 ms (5.64%), Space: 12.7 MB (7.60%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i < n; i++)
             mp[arr[i]].push_back(i);
         
-        for(int a : arr){
+        for(int& a : arr){
             if(mp.find(2*a) != mp.end()){
                 if(a != 2*a || (a == 2*a && mp[2*a].size()>1))
                     return true;
