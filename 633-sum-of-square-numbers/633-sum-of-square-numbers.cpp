a8951471af807145ab34e6d669d78d8380108f3a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-10 21:06:33 -0400

    Time: 74 ms (11.28%), Space: 5.9 MB (72.11%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    bool binarysearch(long s, long e, int n){
+        if(e < s)
+            return false;
+        
+        long m = s + (e-s)/2;
+        if(m*m == n)
+            return true;
+        if(m*m > n)
+            return binarysearch(s, m-1, n);
+        return binarysearch(m+1, e, n);
+    }
+    
     bool judgeSquareSum(int c) {
-        for(long a = 0; a <= sqrt(c); a++){
-            double b = sqrt(1.0 * (c - a*a));
-            if(b == (int)(b))
+        for(long a = 0; a*a <= c; a++){
+            int b = c - a*a;
+            if(binarysearch(0,b,b))
                 return true;
         }
         return false;
