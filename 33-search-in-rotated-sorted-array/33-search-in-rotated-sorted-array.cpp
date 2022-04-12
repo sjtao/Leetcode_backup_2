6fdbf6c56683bdf5a9be36bf7fe46cdcf6f3bb2a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-11 21:44:35 -0400

    Time: 3 ms (85.75%), Space: 11.1 MB (29.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int binary(vector<int>& nums, int target, int l, int r){
-        if(l > r) return -1;
+    int binary(vector<int>& nums, int target, int low, int high){
         int m;
-        while(l <= r){
-            m = l + (r - l) / 2;
-            if(nums[m] < target){
-                l = m+1;
-            }
-            else if(nums[m] > target){
-                r = m-1;
-            }
-            else
+        while(low <= high){
+            m = low + (high-low)/2;
+            if(nums[m] == target)
                 return m;
+            else if(nums[m] > target)
+                high = m-1;
+            else
+                low = m+1;
         }
         return -1;
     }
@@ -21,39 +18,26 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0] == target ? 0 : -1;
         
-        int l, r, m, k;
-        
-        if(nums[0] < nums[n-1])
-            k = 0;
-        else{
-            l = 0;
-            r = n-1;
-            while(l <= r){
-                m = l + (r - l)/2;
-                if(nums[m] > nums[m+1]){
-                    k = m+1;
-                    break;
-                }
-                else{
-                    if(nums[m] >= nums[l])
-                        l = m+1;
-                    else
-                        r = m-1;
-                }
-            }
+        //find the minimum
+        int l = 0, r = n-1, m;
+        while(l < r){
+            m = l + (r-l)/2;
+            if(nums[m] < nums[r])
+                r = m;
+            else if(nums[m] > nums[r])
+                l = m+1;
         }
         
-        int ans = -1;
-        if(k == 0)
-            ans = binary(nums, target, 0, n-1);
-        else
-        {
-            if(nums[0] <= target && nums[k-1] >= target)
-                ans = binary(nums, target, 0, k-1);
-            else if(nums[k] <= target && nums[n-1] >= target)
-                ans = binary(nums, target, k, n-1);
-        }
+        if (nums[l] == target)
+            return l;
         
-        return ans;
+        if(l == 0)
+            return binary(nums, target, 0, n-1);
+        
+        if(target < nums[0])
+            return binary(nums, target, l, n-1);
+        
+        return binary(nums, target, 0, l);
+
     }
 };
