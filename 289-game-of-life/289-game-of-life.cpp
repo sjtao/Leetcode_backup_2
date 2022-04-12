332f67ce9d62ce9671a46981b35040f36bcdaf6c
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-11 21:09:31 -0400

    Time: 0 ms (100.00%), Space: 7 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    pair<int, int> dirs[8] = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+    pair<int,int> dirs[8] = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
     void gameOfLife(vector<vector<int>>& board) {
         int m = board.size();
         int n = board[0].size();
-        
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                int count = 0;
-                for(int k = 0; k < 8; ++k){
-                    int x = i + dirs[k].first;
-                    int y = j + dirs[k].second;
-                    if(x < 0 || y < 0 || x > m-1 || y > n-1)
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int sum = 0;
+                for(int k = 0; k < 8; k++){
+                    int ki = i + dirs[k].first;
+                    int kj = j + dirs[k].second;
+                    if(ki < 0 || kj < 0 || ki >= m || kj >= n)
                         continue;
-                    if(abs(board[x][y]) == 1)
-                        count += 1;
+                    if(abs(board[ki][kj]) == 1)
+                        sum ++;
                 }
                 if(board[i][j] == 1){
-                    if(count < 2 || count > 3) board[i][j] = -1;
-                }
-                else if(board[i][j] == 0){
-                    if(count == 3) board[i][j] = 2;
+                    if (sum < 2 || sum > 3)
+                        board[i][j] = -1;//to dead
+                } 
+                else{
+                    if(sum == 3)
+                        board[i][j] = 2;//to live
                 }
             }
         }
         
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(board[i][j] == -1)
                     board[i][j] = 0;
                 if(board[i][j] == 2)
                     board[i][j] = 1;
             }
         }
-        
         return;
     }
 };
