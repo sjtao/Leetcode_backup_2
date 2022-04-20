4e299c87b9190857dc0c7742f755662e6ae44be9
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-19 22:48:33 -0400

    Time: 15 ms (36.76%), Space: 6.6 MB (57.59%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    unordered_map<int, string> mp;
     string helper(int n){
         if(n == 1) 
-            mp[n] = "1";
+            return "1";
         
-        if(mp.find(n) == mp.end()){
-            string s = helper(n-1);
-            string ans = "";
-            int cnt = 1;
-            for(int i = 1; i < s.length(); ++i){
-                if(s[i] == s[i-1])
-                    cnt++;
-                else{
-                    ans += to_string(cnt);
-                    ans += s[i-1];
-                    cnt = 1;
-                }
+        string s = helper(n-1);
+        string ans = "";
+        int cnt = 1;
+        for(int i = 1; i < s.length(); ++i){
+            if(s[i] == s[i-1])
+                cnt++;
+            else{
+                ans += to_string(cnt);
+                ans += s[i-1];
+                cnt = 1;
             }
-            ans += to_string(cnt);
-            ans += s[s.length()-1];
-            mp[n] = ans;
         }
-        
-        return mp[n];
+        ans += to_string(cnt);
+        ans += s[s.length()-1];
+        return ans;
     }
+    
     string countAndSay(int n) {
         return helper(n);
     }
