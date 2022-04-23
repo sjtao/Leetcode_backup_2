12e9721db248d89679f84cbb69d707ed707ae5c5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-23 16:05:32 -0400

    Time: 10 ms (78.13%), Space: 11.9 MB (61.47%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
-        int s = tokens.size();
+    int convert(string s){
+        int num = 0;
+        int sign = 1;
+        if(s[0] == '-') sign = -1;
+        int i = sign == 1 ? 0 : 1;
+        for(; i < s.length(); i++){
+            num = num * 10 + (s[i] - '0');
+        }
+        return sign * num;
+    }
+    
+    int evalRPN(vector<string>& token) {
         stack<int> st;
-       
-        for(int i = 0; i < s; i++){
-            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
+        int n = token.size();
+        for(int i = 0; i < n; i++){
+            if (token[i] == "+" || token[i] == "-" || token[i] == "*" || token[i] == "/"){
                 int a = st.top(); st.pop();
                 int b = st.top(); st.pop();
-                if (tokens[i] == "+") st.push(b+a);
-                else if (tokens[i] == "-") st.push(b-a);
-                else if (tokens[i] == "*") st.push(b*a);
-                else if (tokens[i] == "/") st.push(b/a);
+                int c;
+                if(token[i] == "+")
+                    c = a + b;
+                else if(token[i] == "-")
+                    c = b - a;
+                else if(token[i] == "*")
+                    c = a * b;
+                else if(token[i] == "/")
+                    c = b / a;
+                st.push(c);
             }
             else{
-                st.push(stoi(tokens[i]));
+                st.push(convert(token[i]));
             }
         }
-        
         return st.top();
     }
 };
