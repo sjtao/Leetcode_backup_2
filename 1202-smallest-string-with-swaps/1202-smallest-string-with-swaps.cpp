a668b813f647d78a25e9237dc4999bb1e88b156e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-04-26 21:59:42 -0400

    Time: 358 ms (21.12%), Space: 73.1 MB (15.17%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
-    vector<int> rank, parent;
+    static const int N = 100001;
+    vector<int> adj[N];
+    bool visited[N];
     
-    int find(int x){
-        if(x == parent[x])
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    void unionfind(int x, int y){
-        int fx = find(x);
-        int fy = find(y);
-        if(fx == fy) return;
-        else{
-            if(rank[fx] > rank[fy]){
-                parent[fy] = fx;
-            }
-            else if(rank[fx] < rank[fy]){
-                parent[fx] = fy;
-            }
-            else{
-                parent[fy] = fx;
-                rank[fx] ++;
-            }
+    void dfs(string&s, int vertex, vector<char>& characters, vector<int> &  indices){
+        characters.push_back(s[vertex]);
+        indices.push_back(vertex);
+        
+        visited[vertex] = true;
+        
+        for(int adjacent : adj[vertex]){
+            if(!visited[adjacent])
+                dfs(s, adjacent, characters, indices);
         }
+        
+        return;
     }
     
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        int n = s.length();
-        
-        for(int i = 0; i < n; i++){
-            parent.push_back(i);
-            rank.push_back(1);
+        //build adjacent list
+        for(vector<int> p : pairs){
+            adj[p[0]].push_back(p[1]);
+            adj[p[1]].push_back(p[0]);
         }
         
-        for(auto p : pairs)
-            unionfind(p[0], p[1]);
-        
-        unordered_map<int, vector<int>> mp;
-        for(int i = 0; i < n; i++)
-            mp[find(i)].push_back(i);
-        
-        for(auto p: mp){
-            string t = "";
-            for(int i : p.second)
-                t += s[i];
-            sort(t.begin(), t.end());
-            int k = 0;
-            for(int i = 0; i < t.size(); i++)
-                s[p.second[i]] = t[i];
+        for(int v = 0; v < s.size(); v++){
+            if(!visited[v]){
+                vector<char> cha;
+                vector<int> ind;
+                
+                dfs(s, v, cha, ind);
+                
+                sort(cha.begin(), cha.end());
+                sort(ind.begin(), ind.end());
+                
+                for(int i = 0; i < ind.size(); i++){
+                    s[ind[i]] = cha[i];
+                }
+            }
         }
         
         return s;
