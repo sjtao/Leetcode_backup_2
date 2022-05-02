6571f7d3ce59a9e914ae3b902f660257bbeaa09a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-01 21:41:30 -0400

    Time: 24 ms (14.10%), Space: 16.3 MB (57.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int n = nums.size();
         if(n < 2) return nums;
-        
-        vector<int> ans(n);
-        int odd = n-1, even = 0;
-        for(int a : nums){
-            if(a % 2 == 1)
-                ans[odd--] = a;
-            else
-                ans[even++] = a;
+
+        int odd = n-1;
+        for(int i = 0; i < n-1; i++){
+            while(i < odd && nums[i] % 2 == 1){
+                swap(nums[i], nums[odd--]);
+            }
+            if(i >= odd)
+                break;
         }
         
-        return ans;
+        return nums;
     }
 };
