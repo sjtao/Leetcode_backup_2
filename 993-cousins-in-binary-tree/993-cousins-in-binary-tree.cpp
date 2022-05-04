dc4a98615a9125e59a02dca34c683ef157511f88
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-03 23:09:14 -0400

    Time: 3 ms (77.85%), Space: 11.4 MB (8.39%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -12,17 +12,26 @@
 class Solution {
 public:
     unordered_map<int, pair<int, TreeNode*>> mp;
+    int fx, fy;
     void inorder(TreeNode* root, int level){
         if(!root)
             return;
         
         inorder(root->left, level+1);
-        if(root->left) mp[root->left->val] = {level, root};
-        if(root->right) mp[root->right->val] = {level, root};
+        if(root->left){
+            if(root->left->val == fx ||root->left->val == fy)
+                mp[root->left->val] = {level, root};
+        }
+        if(root->right){
+            if(root->right->val == fx ||root->right->val == fy)
+                mp[root->right->val] = {level, root};
+        }
         inorder(root->right, level+1);
         return;
     }
     bool isCousins(TreeNode* root, int x, int y) {
+        fx = x;
+        fy = y;
         inorder(root, 0);
         
         if(mp.find(x) == mp.end() || mp.find(y) == mp.end())
