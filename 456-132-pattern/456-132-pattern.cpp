10ff2dc04a88c78e7b9554282d651a708012985b
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-06 22:34:43 -0400

    Time: 109 ms (37.12%), Space: 37.8 MB (98.80%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool find132pattern(vector<int>& nums) {
+    bool find132pattern(vector<int>& nums) {   
         int n = nums.size();
         if(n < 3) return false;
         
         vector<int> minarray(n);
         minarray[0] = nums[0];
-        //0-i min element
-        for(int i = 1; i < n; ++i){
+        
+        for(int i = 1; i < n; i++)
             minarray[i] = min(minarray[i-1], nums[i]);
+        
+        int k = n;
+        for(int j = n-1; j > 0; j--){
+            if(nums[j] <= minarray[j])
+                continue;
+            auto it = lower_bound(nums.begin()+k, nums.end(), minarray[j]+1);
+            k = it - nums.begin();
+            if(k < n && nums[k] < nums[j])
+                return true;
+            k--;
+            nums[k] = nums[j];
         }
         
-        stack<int> s;
-        for(int i = n-1; i>=0; --i){
-            if(nums[i] <= minarray[i])
-                continue;
-            
-            while(!s.empty() && s.top() <= minarray[i]){
-                s.pop();
-            } //find 13
-            if(!s.empty() && s.top() < nums[i]){
-                return true;
-            } //find 2
-            s.push(nums[i]);
-        }
-        return false;
+        return false; 
     }
 };
