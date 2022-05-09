18745696bcbb6e57a6efc47ca303e40ef44decaa
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-08 21:31:03 -0400

    Time: 153 ms (28.09%), Space: 39.2 MB (78.65%) - LeetHub

--- 1634-add-two-polynomials-represented-as-linked-lists/1634-add-two-polynomials-represented-as-linked-lists.cpp
+++ 1634-add-two-polynomials-represented-as-linked-lists/1634-add-two-polynomials-represented-as-linked-lists.cpp
@@ -11,42 +11,27 @@
 
 class Solution {
 public:
+    
     PolyNode* addPoly(PolyNode* poly1, PolyNode* poly2) {
-        PolyNode* ans = new PolyNode();
-        PolyNode* p = ans;
-        while(poly1 && poly2){
-            if(poly1->power == poly2->power){
-                int coff = poly1->coefficient + poly2->coefficient;
-                if(coff != 0){
-                    PolyNode* node = new PolyNode(coff, poly1->power);
-                    p->next = node;
-                    p = p->next;
-                }
-                poly1 = poly1->next;
-                poly2 = poly2->next;
-            }
-            else if(poly1->power > poly2->power){
-                p->next = new PolyNode(poly1->coefficient, poly1->power);
-                p = p->next;
-                poly1 = poly1->next;
-            }
-            else{
-                p->next = new PolyNode(poly2->coefficient, poly2->power);
-                p = p->next;
-                poly2 = poly2->next;
-            }
+        if(!poly1 && !poly2) return NULL;
+        if(!poly1 && poly2) return poly2;
+        if(poly1 && !poly2) return poly1;
+        
+        if(poly1->power == poly2->power){
+            poly1->coefficient += poly2->coefficient;
+            if(poly1->coefficient == 0)
+                return addPoly(poly1->next, poly2->next);
+            poly1->next = addPoly(poly1->next, poly2->next);
+            return poly1;
+        }
+        else if(poly1->power > poly2->power){
+            poly1->next = addPoly(poly1->next, poly2);
+            return poly1;
+        }
+        else{
+            poly2->next = addPoly(poly1, poly2->next);
+            return poly2;
         }
         
-        if(poly1){
-            p->next = poly1;
-            p = p->next;
-        }
-        
-        if(poly2){
-            p->next = poly2;
-            p = p->next;
-        }
-        
-        return ans->next;
     }
 };
