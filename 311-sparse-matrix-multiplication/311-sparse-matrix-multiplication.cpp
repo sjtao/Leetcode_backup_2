e9956d3c7d7ba8d0284987a689f4afcf5311008b
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-10 22:46:36 -0400

    Time: 30 ms (11.78%), Space: 8.1 MB (79.35%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.cpp
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
-        int m = mat1.size();
-        int k = mat1[0].size();
-        int n = mat2[0].size();
-        vector<vector<int>> ans(m, vector<int>(n, 0));
+        int r1 = mat1.size();
+        int c1 = mat1[0].size();
+        int r2 = mat2.size();
+        int c2 = mat2[0].size();
+        vector<vector<int>> ans(r1, vector<int>(c2, 0));
         
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                for(int p = 0; p < k; ++p){
-                    ans[i][j] += mat1[i][p] * mat2[p][j];
+        for(int i = 0; i < r1; i++){
+            for(int j = 0; j < c2; j++){
+                for(int k = 0; k < c1; k++){
+                    ans[i][j] += (mat1[i][k] * mat2[k][j]);
                 }
             }
         }
