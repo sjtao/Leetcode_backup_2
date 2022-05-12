6056ebc4ba1a0bae9ede8d3e26ae36fbb6915e14
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-11 22:16:32 -0400

    Time: 8 ms (10.43%), Space: 8.4 MB (20.20%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
-    
-    vector<vector<int>> board;
-    
-    bool row(int r, int player){
-        for(int i = 0; i < 3; i++){
-            if(board[r][i] != player)
-                return false;
-        }
-        return true;
-    }
-    
-     bool col(int c, int player){
-        for(int i = 0; i < 3; i++){
-            if(board[i][c] != player)
-                return false;
-        }
-        return true;
-    }
-    
-    bool diag(int player){
-        for(int i = 0; i < 3; i++){
-            if(board[i][i] != player)
-                return false;
-        }
-        return true;
-    }
-    
-    bool antidiag(int player){
-        for(int i = 0; i < 3; i++){
-            if(board[i][2-i] != player)
-                return false;
-        }
-        return true;
-    }
-    
+   
     string tictactoe(vector<vector<int>>& moves) {
-        board = vector<vector<int>>(3, vector<int>(3, 0));
+        vector<int> row(3, 0), col(3, 0);
+        int diag = 0, antidiag = 0;
         int player = 1;
         
         for(vector<int> m : moves){
             int r = m[0];
             int c = m[1];
-            board[r][c] = player;
-            if(row(r, player) || col(c, player) || \
-               (r == c && diag(player)) || (r + c == 2 && antidiag(player))){
+            
+            row[r] += player;
+            col[c] += player;
+            if(r == c) diag += player;
+            if(r + c == 2) antidiag += player;
+            
+            if(abs(row[r]) == 3 || abs(col[c]) == 3 || \
+              abs(diag) == 3 || abs(antidiag) == 3)
                 return player == 1 ? "A" : "B";
-            }
             
             player *= -1;
         }
