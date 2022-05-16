445501c0fe2db5ac55e3073f775eb2d34737ffb0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-15 22:00:33 -0400

    Time: 71 ms (78.07%), Space: 19.3 MB (70.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
+    pair<int,int> dirs[8] = {{1,1},{1,-1},{-1,1},{-1,-1},{-1,0},{1,0},{0,-1},{0,1}};
+    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        //bfs
         int n = grid.size();
-        
-        if(grid[0][0] != 0 || grid[n-1][n-1] != 0)
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
-        
+        //bfs
         queue<pair<int,int>> q;
         q.push({0,0});
-        
-        pair<int, int> dirs[8] = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
         grid[0][0] = 1;
         while(!q.empty()){
-            auto t = q.front();
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
             
-            if(t.first == n-1 && t.second == n-1)
-                return grid[n-1][n-1];
+            if(row == n-1 && col == n-1)
+                return grid[row][col];
             
             for(int i = 0; i < 8; i++){
-                int x = t.first + dirs[i].first;
-                int y = t.second + dirs[i].second;
-                if(x < 0 || y < 0 || x > n-1 || y > n-1 || grid[x][y] != 0)
+                int x = row + dirs[i].first;
+                int y = col + dirs[i].second;
+                if(x < 0 || y < 0 || x == n || y == n || grid[x][y] != 0)
                     continue;
+                grid[x][y] = grid[row][col]+1;
                 q.push({x, y});
-                grid[x][y] = grid[t.first][t.second] + 1;
             }
         }
-        
         return -1;
     }
 };
