c81c736dcd94c2bf3ea395397c95dd2ece317804
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-21 21:28:55 -0400

    Time: 7 ms (72.70%), Space: 13.3 MB (28.37%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
+    int n;
     int countSubstrings(string s) {
-        int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for(int i = 0; i < n; i++){
-            dp[i][i] = 1;
-        }
-        
-        for(int i = 0; i < n-1; i++){
-            dp[i][i+1] = (s[i] == s[i+1]);
-        }
-        
-        for(int len = 3; len <= n; len++){
-            for(int i = 0, j = i+len-1; j < n; i++, j++)
-                dp[i][j] = dp[i+1][j-1] && (s[i] == s[j]);
-        }
-        
         int num = 0;
+        n = s.length();
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++)
-                num += dp[i][j];
+            num += palim(s, i, i); //odd;
+            num += palim(s, i, i+1); //even
         }
-        
         return num;
+    }
+    
+    int palim(string s, int l, int r){
+        int a = 0;
+        while(l >= 0 && r < n){
+            if(s[l] != s[r])
+                break;
+            l--;
+            r++;
+            a++;
+        }
+        return a;
     }
 };
