503b137256ac797e7e4255152ef788ff0aba7e86
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-21 21:23:22 -0400

    Time: 39 ms (26.27%), Space: 20.1 MB (22.12%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool palin(string s, int start, int end){
-        if(start > end)
-            return false;
-        int i = start, j = end;
-        while(i <= j){
-            if(s[i] != s[j])
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    
     int countSubstrings(string s) {
         int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; i++){
+            dp[i][i] = 1;
+        }
+        
+        for(int i = 0; i < n-1; i++){
+            dp[i][i+1] = (s[i] == s[i+1]);
+        }
+        
+        for(int len = 3; len <= n; len++){
+            for(int i = 0, j = i+len-1; j < n; i++, j++)
+                dp[i][j] = dp[i+1][j-1] && (s[i] == s[j]);
+        }
+        
         int num = 0;
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(palin(s, i, j))
-                    num ++;
-            }
+            for(int j = i; j < n; j++)
+                num += dp[i][j];
         }
+        
         return num;
     }
 };
