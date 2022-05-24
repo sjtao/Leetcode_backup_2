9a96a9bf5e1d5fc81e4d2b7d1ff6bea00b58f4af
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-23 21:27:15 -0400

    Time: 211 ms (54.80%), Space: 85 MB (87.47%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> ans(n, 0);
-        int temp = temperatures[n-1];
-        int tempid = n-1;
         for(int i = n-2; i >= 0; i--){
-           if(temperatures[i] >= temp){
-               temp = temperatures[i];
-               tempid = i;
-               //ans[i] = 0;
-           }
-            else{
-                int j = i+1;
-                while(j <= tempid){
-                    if(temperatures[i] < temperatures[j]){
-                        ans[i] = j-i;
-                        break;
-                    }         
-                    else if(temperatures[i] == temperatures[j]){
-                        ans[i] = j-i + ans[j];
-                        break;
-                    }
-                    j++;
+            for(int j = i+1; j < n; j++){
+                if(temperatures[j] > temperatures[i]){
+                    ans[i] = j-i;
+                    break;
                 }
-                
-            }    
+                else if(temperatures[j] == temperatures[i]){
+                    ans[i] = ans[j] == 0 ? 0 : ans[j] + j-i;
+                    break;
+                }
+            }
         }
+        
         return ans;
     }
 };
