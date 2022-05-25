60a093778f8cce522d3ff3b12a9d7dd097908571
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-24 23:24:14 -0400

    Time: 20 ms (76.59%), Space: 11.7 MB (97.77%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
+
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        
         int n = intervals.size();
-        if(n <= 1) return n;
-        
-        vector<int> st(n), ed(n);
-        for(int i = 0; i < n; ++i){
-            st[i] = intervals[i][0];
-            ed[i] = intervals[i][1];
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> q;
+        q.push(intervals[0][1]);
+        for(int i = 1; i < n; i++){
+            if(intervals[i][0] >= q.top())
+                q.pop();
+            q.push(intervals[i][1]);
         }
-        sort(st.begin(), st.end());
-        sort(ed.begin(), ed.end());
         
-        //min-heap
-        int s = 0, e = 0;
-        int room = 0;
-        while(s < n){
-            if(st[s] >= ed[e]){
-                e++;
-                room -= 1;
-            }
-            room+=1;
-            s+=1;
-        }
-        return room;
+        return q.size();
     }
 };
