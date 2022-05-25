68490c9c08923bbdf3741d1aa0ccaf2258ee797e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-24 22:56:31 -0400

    Time: 23 ms (98.77%), Space: 13 MB (94.15%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int, vector<int>> q;
-        int n = matrix.size();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                q.push(matrix[i][j]);
-                if(q.size() > k)
-                    q.pop();
+    int n;
+    int count(vector<vector<int>>& matrix, vector<int>& minmax, int m){
+        int num = 0;
+        int col = 0, row = n-1;
+        while(row >= 0 && col < n){
+            if(matrix[row][col] > m){
+                minmax[1] = min(minmax[1], matrix[row][col]);
+                row--;
+            }
+            else{
+                minmax[0] = max(minmax[0], matrix[row][col]);
+                col++;
+                num += (row+1);
             }
         }
-        return q.top();
+        return num;
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        n = matrix.size();
+        int l = matrix[0][0], r = matrix[n-1][n-1], m;
+        
+        int num;
+        while(l < r){
+            m = l + (r - l) / 2;
+            vector<int> minmax = {l, r};
+            num = count(matrix, minmax, m);
+            if(num == k)
+                return minmax[0];
+            else if(num > k)
+                r = minmax[0];
+            else
+                l = minmax[1];
+        }
+        
+        return l;
     }
 };
