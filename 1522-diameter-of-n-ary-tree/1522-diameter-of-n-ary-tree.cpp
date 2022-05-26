78a7b341955b9eac9cc5b1ec42b8cddf02b77b25
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-25 21:46:56 -0400

    Time: 26 ms (30.00%), Space: 10.9 MB (49.57%) - LeetHub

---
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.cpp
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int diam;
+    int dfs(Node* root){
+        if(!root)
+            return 0;
+        
+        int fm = 0, sm = 0;
+        for(Node* c : root->children){
+            int m = dfs(c);
+            if(m >= fm){
+                sm = fm;
+                fm = m;
+            }
+            else if(m >= sm){
+                sm = m;
+            }
+        }
+        
+        int loc = fm + sm;
+        diam = max(diam, loc);
+        
+        return 1+fm;
+        
+    }
+    
+    int diameter(Node* root) {
+        if(!root)
+            return 0;
+        
+        diam = 0;
+        dfs(root);
+        return diam;
+    }
+};
