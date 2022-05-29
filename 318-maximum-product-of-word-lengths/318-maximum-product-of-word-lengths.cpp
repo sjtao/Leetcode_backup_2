ccb3c8059929882732bf9ab3713f000b77a53e8f
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-05-28 22:44:57 -0400

    Time: 43 ms (94.22%), Space: 16.2 MB (51.80%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    int bitnum(char c){
-        return (c - 'a');
-    }
     
     int maxProduct(vector<string>& words) {
         int n = words.size();
         int ans = 0;
-        vector<int> len(n), mask(n);
+        unordered_map<int, int> mask;
         
         for(int i = 0; i < n; i++){
-            len[i] = words[i].length();
+            int len = words[i].length();
             int bit = 0;
             for(char c : words[i])
-                bit |= 1 << bitnum(c);
-            mask[i] = bit;
+                bit |= 1 << (c - 'a');
+            
+            for(auto it : mask){
+                if((it.first & bit) == 0)
+                    ans = max(ans, it.second * len);
+            }
+            mask[bit] = max(len, mask[bit]);
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if((mask[i] & mask[j]) == 0){
-                    ans = max(ans, len[i]*len[j]); 
-                }    
-            }
-        }
         return ans;
     }
 };
