a3805b18a9166503c5dd749561836a8d9aabb86f
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-08 21:22:19 -0400

    Time: 40 ms (23.48%), Space: 14 MB (21.58%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         int n = s.length();
-        if(n <= numRows || numRows == 1) return s;
-        vector<string> r(numRows,"");
+        if(n == numRows || numRows == 1)
+            return s;
+        
+        unordered_map<int, string> mp;
+        for(int i = 0; i < numRows; i++)
+            mp[i] = "";
+        
+        int k = 0;
+        int dir = -1;
         int i = 0;
-        int zig = -1;
-        for(char c:s){
-            r[i] += c;
-            if(i == 0 || i == numRows-1) zig = -zig;
-            i += zig;
+        while(k < n){
+            mp[i] += s[k++];
+            if(i == 0 || i == numRows-1)
+                dir *= -1;
+            i += dir;
         }
         
         string ans = "";
-        for(int i = 0; i < numRows; i++){
-            ans += r[i];
-        }
+        for(int i = 0; i < numRows; i++)
+            ans += mp[i];
+        
         return ans;
     }
 };
