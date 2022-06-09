08b781639d9afca46ffe6dd3c30aa6ecd0ce1f5a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-09 14:53:23 -0400

    Time: 14 ms (63.29%), Space: 12 MB (27.76%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
@@ -2,9 +2,18 @@ class Solution {
 public:
     vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
         vector<int> ans;
-        set<int> st1(arr1.begin(), arr1.end()), st2(arr2.begin(), arr2.end());
-        for(int a : arr3){
-            if(st1.count(a) && st2.count(a))
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+        int n3 = arr3.size();
+        int p1 = 0, p2 = 0, p3 = 0;
+        for(int a : arr1){
+            while(p2 < n2 && arr2[p2] < a)
+                p2++;
+            while(p3 < n3 && arr3[p3] < a)
+                p3++;
+            int b = p2 == n2 ? 0 : arr2[p2];
+            int c = p3 == n3 ? 0 : arr3[p3];
+            if(a == b && a == c)
                 ans.push_back(a);
         }
         return ans;
