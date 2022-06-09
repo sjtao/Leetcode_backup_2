ea566fc1332a3b713ba04b1a4c9b4d9141d5b7f2
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-09 15:25:26 -0400

    Time: 10 ms (36.00%), Space: 11.6 MB (60.89%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    vector<int> ans;
-    
-    void binary(vector<int>& nums, int target, int l, int r){
-        if(l > r) return;
-        
-        int m = l + (r - l) / 2;
-        if(nums[m] == target){
-            binary(nums, target, l, m-1);
-            ans.push_back(m);
-            binary(nums, target, m+1, r);
-        }
-        else if(nums[m] > target)
-            binary(nums, target, l, m-1);
-        else
-            binary(nums, target, m+1, r);
-        
-        return;
-    }
-    
     vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> ans;
         
-        int n = nums.size();
-        sort(nums.begin(), nums.end()); //O(nlogn)
-        binary(nums, target, 0, n-1);
+        int eq = 0, le = 0;
+        for(int a : nums){
+            eq += a == target;
+            le += a < target;
+        }
+        
+        while(eq){
+            ans.push_back(le++);
+            eq--;
+        }
+        
         return ans;
     }
 };
