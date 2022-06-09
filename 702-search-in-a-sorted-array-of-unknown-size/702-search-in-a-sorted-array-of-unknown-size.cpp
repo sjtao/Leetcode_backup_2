fdba1de17610997e836fb630939243874f837a7d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-08 22:49:47 -0400

    Time: 37 ms (57.40%), Space: 10.1 MB (45.64%) - LeetHub

--- 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.cpp
+++ 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.cpp
@@ -10,24 +10,20 @@
 class Solution {
 public:
     int search(const ArrayReader& reader, int target) {
-        if(reader.get(0) == target) return 0;
+        int l = 0, r = 1;
+        while(reader.get(r) < target)
+            r <<= 1;
         
-        int left = 0, right = 1, mid;
-        while(reader.get(right) < target){
-            left = right;
-            right *= 2;
+        while(l <= r){
+            int m = l + (r-l) / 2;
+            int nm = reader.get(m);
+            if(nm == target)
+                return m;
+            else if(nm > target)
+                r = m-1;
+            else
+                l = m+1;
         }
-        
-        int number;
-        while(left <= right){
-            mid = left + (right - left) / 2;
-            number = reader.get(mid);
-            
-            if(number == target) return mid;
-            else if(number > target) right = mid-1;
-            else                     left  = mid+1;
-        }
-        
         return -1;
     }
 };
