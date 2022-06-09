5c78f907c7fe8a0ae05a3804bff13d272ec929e0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-09 14:40:25 -0400

    Time: 47 ms (71.12%), Space: 89.7 MB (32.71%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
+    int MN = 1000001;
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        //1. brutal O(m+n)
         int m = nums1.size();
         int n = nums2.size();
-        vector<int> nums;
-        for(int p : nums1)
-            nums.push_back(p);
-        for(int p : nums2)
-            nums.push_back(p);
+        vector<int> num(m+n);
+        int i = 0, j = 0, k = 0;
+        while(k < m+n){
+            int a = i == m ? MN : nums1[i];
+            int b = j == n ? MN : nums2[j];
+            if(a < b){
+                num[k] = a;
+                i++;
+            }
+            else{
+                num[k] = b;
+                j++;
+            }
+            k++;
+        }
+        double ans = 0;
+        if((m+n) % 2){
+            ans += num[(m+n)/2];
+        }
+        else{
+            ans += 0.5 * (num[(m+n)/2] + num[(m+n)/2-1]);
+        }
         
-        sort(nums.begin(), nums.end());
-        return (m+n)%2 == 0 ? (nums[(m+n)/2-1]+nums[(m+n)/2])/2.0 : nums[(m+n)/2]*1.0;
+        return ans;
     }
 };
