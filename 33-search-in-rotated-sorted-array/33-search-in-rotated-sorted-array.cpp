05c0b4809bf8b4e30f395c2fd175c2c24268e609
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-10 22:07:23 -0400

    Time: 0 ms (100.00%), Space: 11 MB (75.25%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    int binary(vector<int>& nums, int target, int low, int high){
-        int m;
-        while(low <= high){
-            m = low + (high-low)/2;
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while(l <= r){
+            int m = l + (r - l) / 2;
             if(nums[m] == target)
                 return m;
-            else if(nums[m] > target)
-                high = m-1;
-            else
-                low = m+1;
+            else if(nums[m] >= nums[l]){
+                if(nums[l] <= target && nums[m] > target)
+                    r = m-1;
+                else
+                    l = m+1;
+            }
+            else{
+                if(nums[r] >= target && nums[m] < target)
+                    l = m+1;
+                else
+                    r = m-1;
+            }
         }
         return -1;
-    }
-    
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        if(n == 1) return nums[0] == target ? 0 : -1;
-        
-        //find the minimum
-        int l = 0, r = n-1, m;
-        while(l < r){
-            m = l + (r-l)/2;
-            if(nums[m] < nums[r])
-                r = m;
-            else if(nums[m] > nums[r])
-                l = m+1;
-        }
-        
-        if (nums[l] == target)
-            return l;
-        
-        if(l == 0)
-            return binary(nums, target, 0, n-1);
-        
-        if(target < nums[0])
-            return binary(nums, target, l, n-1);
-        
-        return binary(nums, target, 0, l);
-
     }
 };
