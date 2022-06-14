7d7d0c323e2069df412a6e3012b0a0cdaf050a00
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-13 20:14:59 -0400

    Time: 52 ms (21.00%), Space: 12.3 MB (34.50%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n1 = word1.size();
-        int n2 = word2.size();
+        //find the longest common substring
+        int n1 = word1.length();
+        int n2 = word2.length();
         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
-        
-        for(int i = 0; i <= n1; i++){
-            for(int j = 0; j <= n2; j++){
-                if(i == 0 || j == 0)
-                    dp[i][j] = i+j;
-                else if(word1[i-1] == word2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
+        for(int i = 0; i < n1; i++){
+            for(int j = 0; j < n2; j++){
+                if(word1[i] == word2[j])
+                    dp[i+1][j+1] = dp[i][j] + 1;
                 else
-                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
             }
         }
-        
-        return dp[n1][n2];
+        return n1 + n2 - dp[n1][n2] * 2;
     }
 };
