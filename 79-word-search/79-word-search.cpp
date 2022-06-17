4170ee9a7c68f6348111f593cf42f2e2e6ab04b5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-16 22:28:29 -0400

    Time: 381 ms (68.56%), Space: 8 MB (36.27%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -3,20 +3,20 @@ public:
     int m, n, l;
     pair<int,int> dirs[4] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     
-    bool dfs(vector<vector<char>>& board, vector<vector<int>>& visited, string word, int s, int e, int k){
+    bool dfs(vector<vector<char>>& board, string word, int s, int e, int k){
         if(k == l)
             return true;
         bool ans = false;
         for(int i = 0; i < 4; i++){
             int x = s + dirs[i].first;
             int y = e + dirs[i].second;
-            if(x < 0 || y < 0 || x >= m || y >= n || board[x][y] != word[k] || visited[x][y] == 1)
+            if(x < 0 || y < 0 || x >= m || y >= n || board[x][y] != word[k])
                 continue;
-            visited[x][y] = 1;
-            ans = dfs(board, visited, word, x, y, k+1);
+            board[x][y] = '#';
+            ans = dfs(board, word, x, y, k+1);
             if(ans) break;
         }
-        visited[s][e] = 0;
+        board[s][e] = word[k-1];
         return ans;
     }
     
@@ -24,12 +24,12 @@ public:
         m = board.size();
         n = board[0].size();
         l = word.length();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j] == word[0]){
-                    visited[i][j] = 1;
-                    if(dfs(board, visited, word, i, j, 1))
+                    board[i][j] = '#';
+                    if(dfs(board, word, i, j, 1))
                         return true;
                 }
             }
