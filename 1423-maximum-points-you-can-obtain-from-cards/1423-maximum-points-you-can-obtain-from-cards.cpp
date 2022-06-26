8223742a828a222d601512be2a67eaf5e7877e6d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-26 16:41:30 -0400

    Time: 61 ms (94.54%), Space: 44.1 MB (20.13%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -5,18 +5,21 @@ public:
         if(k == 1)
             return max(cardPoints[0], cardPoints[n-1]);
         
-        vector<int> forward(n+1, 0), backward(n+1,0);
-        
-        for(int i = 1; i < n+1; i++){
-            forward[i] = forward[i-1] + cardPoints[i-1];
-            backward[i] = backward[i-1] + cardPoints[n-i];
+        vector<int> num(2*k);
+        for(int i = 0; i < 2*k; i++){
+            num[i] = cardPoints[(n-k+i)%n];
         }
         
         int sum = 0;
-        for(int i = 0; i <= k; i++){
-            sum = max(sum, forward[i]+backward[k-i]);
+        for(int i = 0; i < k; i++){
+            sum += num[i];
+        }
+        int mx = sum;
+        for(int i = 0; i < k; i++){
+            sum = sum-num[i]+num[k+i];
+            mx = max(mx, sum);
         }
         
-        return sum;
+        return mx;
     }
 };
