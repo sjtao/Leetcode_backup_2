698a27d82220028c8c7df19a289473dd296c6906
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-27 21:46:41 -0400

    Time: 0 ms (100.00%), Space: 5.8 MB (67.45%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,12 +5,12 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l = 1, r = n, m;
-        while(l <= r){
+        while(l < r){
             m = l + (r - l) / 2;
             if(isBadVersion(m))
-                r = m-1;
+                r = m;
             else
-                l = m+1;
+                l = m + 1;
         }
         return l;
     }
