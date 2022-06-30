b8dfe84616072f8cb864bf13a564358b161a78e5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-29 22:25:35 -0400

    Time: 35 ms (94.98%), Space: 12.2 MB (84.16%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,64 +1,31 @@
 class Solution {
 public:
-    int island, m, n;
-    vector<int> root, rank;
-    pair<int,int> dirs[4] = {{0,1}, {0,-1}, {1,0},{-1,0}};
-    
-    int find(int x){
-        if(x == root[x])
-            return x;
-        return root[x] = find(root[x]);
-    }
-    
-    void unionset(int x, int y){
-        int fx = find(x);
-        int fy = find(y);
-        if(fx != fy){
-            if(rank[fx] > rank[fy])
-                root[fy] = fx;
-            else if(rank[fx] < rank[fy])
-                root[fx] = fy;
-            else{
-                root[fx] = fy;
-                rank[fy]++;
-            }
-            island --;
+    pair<int,int> dir[4] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    int m, n;
+    void dfs(vector<vector<char>>& grid, int x, int y){
+        grid[x][y] = '0';
+        for(int k = 0; k < 4; k ++){
+            int kx = x + dir[k].first;
+            int ky = y + dir[k].second;
+            if(kx < 0 || ky < 0 || kx >= m || ky >= n || grid[kx][ky] != '1')
+                continue;
+            dfs(grid, kx, ky);
         }
+        return;
     }
     
     int numIslands(vector<vector<char>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        island = 0;
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
+        int cnt = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    root.push_back(i * n + j);
-                    rank.push_back(1);
-                    island++;
-                }
-                else{
-                    root.push_back(-1);
-                    rank.push_back(0);
-                }
+                    cnt += 1;
+                    dfs(grid, i, j);
+                }    
             }
         }
-        
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(grid[i][j] == '1'){
-                    grid[i][j] = '0';
-                    for(int k = 0; k < 4; k++){
-                        int kx = i + dirs[k].first;
-                        int ky = j + dirs[k].second;
-                        if(kx < 0 || ky < 0 || kx >= m || ky >= n || grid[kx][ky] == '0')
-                            continue;
-                        unionset(i*n+j, kx*n+ky);
-                    }
-                }
-            }
-        }
-        
-        return island;
+        return cnt;
     }
 };
