db56359888d0ccab2e6e117c5820ffe3a86412ec
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-30 22:17:25 -0400

    Time: 3 ms (60.94%), Space: 6.4 MB (9.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     unordered_map<int,int> mp;
-    int dp(int n){
-        if(n <= 1)
-            return n;
+    int fibhelper(int n){
+        if(mp.find(n) != mp.end())
+            return mp[n];
         
-        if(mp.find(n) == mp.end()){
-            mp[n] = dp(n-1) + dp(n-2);
-        }
-        
+        if(n <= 1) return n;
+        mp[n] = fibhelper(n-1) + fibhelper(n-2);
         return mp[n];
     }
+    
     int fib(int n) {
-        return dp(n);
+        return fibhelper(n);
     }
 };
