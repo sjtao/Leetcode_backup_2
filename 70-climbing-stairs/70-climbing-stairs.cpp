cba379d1e61c2412c8fc807932dd27d5839ddd76
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-30 22:34:12 -0400

    Time: 2 ms (41.97%), Space: 6.3 MB (14.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    unordered_map<int, int> mp;
-    int dp_helper(int n){
-        if(n <= 1)
-            mp[n] = 1;
-           
-        if(mp.count(n) == 0){
-            mp[n] = dp_helper(n-1) + dp_helper(n-2);
-        }
-
-        return mp[n];
-    }
+    
     int climbStairs(int n) {
-        return dp_helper(n);
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
     }
 };
