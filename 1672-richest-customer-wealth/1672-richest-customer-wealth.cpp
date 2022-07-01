074e3b50a9baddca5bffaedbee6e9a1bcf9a4375
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-06-30 22:47:59 -0400

    Time: 8 ms (52.67%), Space: 7.7 MB (86.50%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -3,11 +3,10 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int m = accounts.size();
         int n = accounts[0].size();
-        int mx = 0;
-        int sum = 0;
-        for(int i = 0; i < m; ++i){
+        int mx = 0, sum;
+        for(int i = 0; i < m; i++){
             sum = 0;
-            for(int j = 0; j < n; ++j){
+            for(int j = 0; j < n; j++){
                 sum += accounts[i][j];
             }
             mx = max(mx, sum);
