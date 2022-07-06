dd35504d4b8b4ae8ae401d9a1d7fadf18c55d716
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-07-05 23:21:16 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (55.48%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    string decodeString(string s) {
-        stack<string> st;
-        int n = s.size();
-        
-        for(int i = 0; i < n; i++){
-            string temp;
-            if(s[i] != ']'){
-                if(s[i] >= '0' && s[i] <= '9'){
-                    if(!st.empty() && "0" <= st.top() && st.top() <= "9"){ //multiple-digit number
-                        temp = st.top() + s[i];
-                        st.pop();
-                        st.push(temp);
-                    }
-                    else
-                        st.push(temp + s[i]); //1-digit number
-                }
-                else
-                    st.push(temp + s[i]);             
-            }
+    string decode(string& s, int& index){
+        string result;
+        while(index < s.length() && s[index] != ']'){
+            if(!isdigit(s[index]))
+                result += s[index++];
             else{
-                string c;
-                while(st.top() != "["){
-                    c = st.top(); st.pop();
-                    temp = c+temp;
+                int k = 0;
+                while(index < s.length() && isdigit(s[index]))
+                    k = k * 10 + (s[index++] - '0');
+                index++; //ignore '['
+                string d = decode(s, index);
+                index++; //ignore ']'
+                while(k > 0){
+                    result += d;
+                    k--;
                 }
-                st.pop(); //pop '['
-                c = st.top(); st.pop();
-                int r = stoi(c);
-                string tp = "";
-                while(r){
-                    tp += temp; r--;
-                }
-                st.push(tp);
             }
         }
-        
-        string ans;
-        while(!st.empty()){
-            ans = st.top() + ans;
-            st.pop();
-        }
-        return ans;
+        return result;
+    }
+    
+    string decodeString(string s) {
+        int index = 0;
+        return decode(s, index);
     }
 };
