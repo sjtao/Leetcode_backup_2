085cffb3f0e818fd8d230130b27e7a7b35720224
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-07-06 14:27:21 -0400

    Time: 0 ms (100.00%), Space: 6.9 MB (71.86%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -3,37 +3,33 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> s(n*m);
-        int t = 0, b = m-1, l = 0, r = n-1;
-        int cnt = 0;
-        while(cnt < n*m){
-            //left
-            for(int i = l; i <= r; i++)
-                s[cnt++] = matrix[t][i];
-            t++;
-            if(t > b)
-                break;
+        vector<int> ans(m*n);
+        
+        int left = 0, right = n-1;
+        int top = 0, bottom = m-1;
+        int p = 0;
+        while(p < m*n){
+            for(int i = left; i <= right; i++)
+                ans[p++] = matrix[top][i];
+            top++;
+            if(top > bottom) break;
             
-            //down
-            for(int i = t; i <= b; i++)
-                s[cnt++] = matrix[i][r];
-            r--;
-            if(r < l)
-                break;
+            for(int i = top; i <= bottom; i++)
+                ans[p++] = matrix[i][right];
+            right--;
+            if(right < left) break;
             
-            //right
-            for(int i = r; i >= l; i--)
-                s[cnt++] = matrix[b][i];
-            b--;
-            if(b < t)
-                break;
-            //up
-            for(int i = b; i >= t; i--)
-                s[cnt++] = matrix[i][l];
-            l++;
-            if(l > r)
-                break;
+            for(int i = right; i >= left; i--)
+                ans[p++] = matrix[bottom][i];
+            bottom--;
+            if(bottom < top) break;
+            
+            for(int i = bottom; i >= top; i--)
+                ans[p++] = matrix[i][left];
+            left++;
+            if(left > right) break;
         }
-        return s;
+        
+        return ans;
     }
 };
