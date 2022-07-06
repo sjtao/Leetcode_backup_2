c7afb35db8cf07ef55be5acd5eb07e192fdb29ac
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-07-06 13:35:44 -0400

    Time: 18 ms (73.26%), Space: 8.6 MB (90.52%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
+    bool compare(vector<int>& va, vector<int>& vb){
+        for(int i = 0; i < 26; i++){
+            if(va[i] != vb[i])
+                return false;
+        }
+        return true;
+    }
+    
     vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
         int ns = s.length();
         int np = p.length();
-        if(ns < np) return {};
+        if(ns < np) return ans;
         
-        vector<int> ans;
-        vector<int> rs(26, 0);
-        vector<int> rp(26, 0);
+        vector<int> vs(26, 0), vp(26,0);
         for(char c : p)
-            rp[c-'a']++;
+            vp[c-'a']++;
         
-        for(int i = 0; i < np; ++i){
-            rs[s[i]-'a']++; 
-        }
-        if(equal(rp.begin(), rp.end(), rs.begin()))
-                ans.push_back(0);
-        
-        for(int i = np; i < ns; ++i){
-            rs[s[i]-'a']++;
-            rs[s[i-np]-'a']--;
-            if(equal(rp.begin(), rp.end(), rs.begin()))
+        for(int i = 0; i < ns; i++){
+            vs[s[i] - 'a']++;
+            if(i >= np)
+                vs[s[i-np] - 'a']--;
+            if(compare(vs, vp))
                 ans.push_back(i-np+1);
         }
+        
         return ans;
     }
 };
