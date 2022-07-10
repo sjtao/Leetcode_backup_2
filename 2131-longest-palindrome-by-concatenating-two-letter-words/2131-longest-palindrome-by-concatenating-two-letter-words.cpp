4d68e549e89f660785fe8e060de76db8a409f109
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-07-09 21:43:58 -0400

    Time: 560 ms (56.94%), Space: 168 MB (62.52%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         int n = words.size();
-        //words[i].length == 2
-        unordered_map<string, int> st;
+        //provided constraint: words[i].length == 2
+        
+        unordered_map<string, int> mp;
         int len = 0;
         for(string t : words){
             string rev = t;
-            reverse(rev.begin(), rev.end());
-            if(st.find(rev) == st.end() || st[rev] == 0){
-                st[t]++;
+            rev[0] = t[1];
+            rev[1] = t[0];
+            if(mp.find(rev) == mp.end() || mp[rev] == 0){ //no strings in the map having reversed form
+                mp[t]++;
             }
             else{
-                st[rev]--;
+                mp[rev]--; //having strings in the map having reversed form
                 len += 4;
             }
         }
         
+        //count if there are strings having form "aa" left in the map
         int r = 0;
-        for(auto it : st){
+        for(auto it : mp){
             if(it.second > 0 && it.first[0] == it.first[1])
                 r++;
         }
         
-        if(r > 0) len += 2;
+        if(r > 0) len += 2; //"aa" in the center
         
         return len;
         
