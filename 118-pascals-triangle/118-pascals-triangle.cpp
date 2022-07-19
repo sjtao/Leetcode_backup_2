d5b23efb6c3f838aca1e71698c2110039ada2db1
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-07-18 21:25:57 -0400

    Time: 5 ms (14.03%), Space: 6.4 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,15 +3,12 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> trig;
         
-        for(int i = 1; i <= numRows; i++){
-            vector<int> temp(i, 1);
-            trig.push_back(temp);
-        }
-        
-        for(int i = 2; i < numRows; i++){
+        for(int i = 0; i < numRows; i++){
+            vector<int> temp(i+1, 1);
             for(int j = 1; j < i; j++){
-                trig[i][j] = trig[i-1][j-1] + trig[i-1][j];
+                temp[j] = trig[i-1][j-1] + trig[i-1][j];
             }
+            trig.push_back(temp);
         }
         
         return trig;
