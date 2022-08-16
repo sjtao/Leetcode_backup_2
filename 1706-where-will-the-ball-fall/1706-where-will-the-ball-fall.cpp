000b865d1e1610415749673053c1b55438b06557
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-15 22:28:54 -0400

    Time: 34 ms (63.58%), Space: 13.1 MB (81.27%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -3,23 +3,22 @@ public:
     vector<int> findBall(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<int> fall(n, -1);
+        vector<int> ball(n, -1);
         
         for(int i = 0; i < n; i++){
             int row = 0;
             int col = i;
-            int ngb;
             while(row < m){
-                ngb = col + grid[row][col];
-                if(ngb < 0 || ngb >= n || grid[row][col] * grid[row][ngb] == -1)
+                int nextcol = col + grid[row][col];
+                if(nextcol < 0 || nextcol >= n || grid[row][col] * grid[row][nextcol] == -1)
                     break;
                 row++;
-                col = ngb; 
+                col = nextcol;
             }
-            
-            if(row == m) fall[i] = col;            
+            if(row == m)
+                ball[i] = col;
         }
         
-        return fall;
+        return ball;
     }
 };
