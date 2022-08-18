4e6106517ab03797bf73b9fc50c95337b468802d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-17 21:58:07 -0400

    Time: 214 ms (80.89%), Space: 40.4 MB (32.15%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.cpp
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -3,28 +3,29 @@ public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
         int n1 = slots1.size();
         int n2 = slots2.size();
-        if(n1 < n2)
-            return minAvailableDuration(slots2, slots1, duration);
         
         sort(slots1.begin(), slots1.end());
         sort(slots2.begin(), slots2.end());
         
         int i = 0, j = 0;
-        vector<int> ans;
         while(i < n1 && j < n2){
-            int start = max(slots1[i][0], slots2[j][0]);
-            int end = min(slots1[i][1], slots2[j][1]);
-            if(start + duration <= end){
-                ans.push_back(start);
-                ans.push_back(start+duration);
-                break;
-            }
-            
-            if(end == slots1[i][1])
-                i++;
-            if(end == slots2[j][1])
+            if(slots1[i][0] > slots2[j][1])
                 j++;
+            else if(slots1[i][1] < slots2[j][0])
+                i++;
+            else{
+                int ms = max(slots1[i][0], slots2[j][0]);
+                int me = min(slots1[i][1], slots2[j][1]);
+                if(me - ms >= duration)
+                    return {ms, ms+duration};
+                else{
+                    if(me == slots1[i][1]) i++;
+                    else j++;
+                }
+            }
         }
-        return ans;
+        
+        return {};
+        
     }
 };
