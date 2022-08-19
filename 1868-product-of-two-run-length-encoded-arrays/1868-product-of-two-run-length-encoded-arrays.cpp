00615a2f38a19070cb7dbf5da13d0f8e86ed2348
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-19 11:44:41 -0400

    Time: 680 ms (88.07%), Space: 204.4 MB (53.09%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.cpp
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> findRLEArray(vector<vector<int>>& encoded1, vector<vector<int>>& encoded2) {
-        int n1 = encoded1.size();
-        int n2 = encoded2.size();
-        
-        int i1 = 0, i2 = 0;
+        int n = encoded1.size();
+        int m = encoded2.size();
+        int i = 0, j = 0;
         vector<vector<int>> ans;
-        while(i1 < n1 && i2 < n2){
-            int a = encoded1[i1][0];
-            int b = encoded2[i2][0];
-            int r = a * b;
-            int k = min(encoded1[i1][1], encoded2[i2][1]);
-            if(k == encoded1[i1][1]) i1++;
-            else encoded1[i1][1] -= k;
-            if(k == encoded2[i2][1]) i2++;
-            else encoded2[i2][1] -= k;
-
-            if(!ans.empty() && ans.back()[0] == r)
-                ans.back()[1] += k;
+        int pre = 0;
+        while(i < n && j < m){
+            int mn = min(encoded1[i][1], encoded2[j][1]);
+            int p = encoded1[i][0] * encoded2[j][0];
+            if(p != pre)
+                ans.push_back({p, mn});
             else
-                ans.push_back({r,k});
+                ans.back()[1] += mn;
+            
+            encoded1[i][1] -= mn;
+            if(encoded1[i][1] == 0) i++;
+            encoded2[j][1] -= mn;
+            if(encoded2[j][1] == 0) j++;
+            pre = p;
         }
+        
         return ans;
     }
 };
