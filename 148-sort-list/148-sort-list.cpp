d2438919401424e8cd04e6789ff0e16da15bfdd5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-18 21:59:20 -0400

    Time: 342 ms (42.80%), Space: 74.1 MB (31.23%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -11,22 +11,43 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
+        if(!head || !head->next)
+            return head;
         
-        vector<int> v;
-        while(head){
-            v.push_back(head->val);
-            head = head->next;
-        }
-        
-        sort(v.begin(),v.end());
+        ListNode* mid = getmid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+    
+    ListNode* merge(ListNode* list1, ListNode* list2){
         ListNode* dummy = new ListNode();
         ListNode* p = dummy;
-        for(int a : v){
-            p->next = new ListNode(a);
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                p->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                p->next = list2;
+                list2 = list2->next;
+            }
             p = p->next;
         }
+        if(list1) p->next = list1;
+        else      p->next = list2;
         
         return dummy->next;
+    }
+    
+    ListNode* getmid(ListNode* head) {
+        ListNode* midPrev = nullptr;
+        while (head && head->next) {
+            midPrev = (midPrev == nullptr) ? head : midPrev->next;
+            head = head->next->next;
+        }
+        ListNode* mid = midPrev->next;
+        midPrev->next = nullptr;
+        return mid;
     }
 };
