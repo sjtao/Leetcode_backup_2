5a67877ddf028b253b63abfc9e0921d4319c673e
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-21 15:13:27 -0400

    Time: 335 ms (45.41%), Space: 74.2 MB (14.78%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,23 +10,42 @@
  */
 class Solution {
 public:
-    
     ListNode* sortList(ListNode* head) {
         if(!head || !head->next) return head;
-        
-        priority_queue<int, vector<int>, greater<int>> q;
-        while(head){
-            q.push(head->val);
-            head = head->next;
+        ListNode* mid = findhalf(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+    
+    ListNode* findhalf(ListNode* head){
+        ListNode* mid = NULL;
+        while(head && head->next){
+            mid = (mid == NULL) ? head : mid->next;
+            head = head->next->next;
         }
-        
+        ListNode* m = mid->next; //second half
+        mid->next = NULL;
+        return m;
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2){
         ListNode* dummy = new ListNode();
         ListNode* p = dummy;
-        while(!q.empty()){
-            p->next = new ListNode(q.top());
-            q.pop();
-            p = p->next;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                p->next = l1;
+                p = p->next;
+                l1 = l1->next;
+            }
+            else{
+                p->next = l2;
+                p = p->next;
+                l2 = l2->next;
+            }
         }
+        if(l1) p->next = l1;
+        else p->next = l2;
         
         return dummy->next;
     }
