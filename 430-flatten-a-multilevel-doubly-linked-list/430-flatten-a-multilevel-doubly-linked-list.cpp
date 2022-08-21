66bddbed0cc371d794de545e184bd26a13e3c3db
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-21 16:54:14 -0400

    Time: 4 ms (76.73%), Space: 7.4 MB (20.14%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -14,22 +14,22 @@ public:
     Node* flatten(Node* head) {
         if(!head) return head;
         
-        deque<Node*> dq;
-        dq.push_back(head);
+        stack<Node*> dq;
+        dq.push(head);
         
         Node* node = new Node(0, NULL, head, NULL);
         Node* cur, * pre = node;
         
         while(!dq.empty()){
-            Node* cur = dq.front();
-            dq.pop_front();
+            Node* cur = dq.top();
+            dq.pop();
             
             cur->prev = pre;
             pre->next = cur;
             
-            if(cur->next) dq.push_front(cur->next);
+            if(cur->next) dq.push(cur->next);
             if(cur->child){
-                dq.push_front(cur->child);
+                dq.push(cur->child);
                 cur->child = NULL;
             }
             
