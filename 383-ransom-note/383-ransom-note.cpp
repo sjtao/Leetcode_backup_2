c11965af0e58afd6ce009c551f4a779695002446
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-24 21:45:35 -0400

    Time: 42 ms (23.70%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        //ransomNote in magazin
         unordered_map<char, int> mp;
         for(char c : magazine)
             mp[c]++;
         for(char c : ransomNote){
-            if(mp.find(c) == mp.end() || mp[c] == 0)
+            if(mp.find(c) == mp.end() || mp[c] <= 0)
                 return false;
-            else
-                mp[c]--;
+            mp[c]--;
         }
         return true;
     }
