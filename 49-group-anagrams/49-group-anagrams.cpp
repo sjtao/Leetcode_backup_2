9cff9724dfbabb530b62cd3301ec2674379f1355
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-24 22:28:19 -0400

    Time: 76 ms (30.62%), Space: 19.6 MB (83.91%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        string t;
-        for(string s : strs){
-            t = s;
-            sort(s.begin(), s.end());
-            mp[s].push_back(t);
-        }
-        
         vector<vector<string>> ans;
-        for(auto it : mp)
+        unordered_map<string, vector<string>> mp;
+        for(string& s : strs){
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        for(auto& it : mp)
             ans.push_back(it.second);
-        
         return ans;
     }
 };
