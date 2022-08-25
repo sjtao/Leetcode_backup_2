b7ae3951b450c460a2d5f12ccb774ca2a448b9ff
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-24 22:53:57 -0400

    Time: 50 ms (71.23%), Space: 27.6 MB (12.32%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,13 +3,13 @@ public:
     int search(vector<int>& nums, int target) {
         int l = 0, r = nums.size()-1;
         while(l <= r){
-            int m = l + (r - l) / 2;
+            int m = l + (r-l)/2;
             if(nums[m] == target)
                 return m;
-            else if(nums[m] < target)
-                l = m + 1;
+            else if(nums[m] > target)
+                r--;
             else
-                r = m - 1;
+                l++;
         }
         return -1;
     }
