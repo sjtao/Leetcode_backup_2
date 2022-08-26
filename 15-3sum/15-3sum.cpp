ef043091c3790ef7f6132ee40b1111c2d311b431
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-25 22:31:16 -0400

    Time: 1499 ms (9.98%), Space: 270.6 MB (6.01%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    int n;
-    void twosum(vector<int>& nums, int s){
-        int k = - nums[s];
-        int l = s+1, r = n-1;
-        
-        while(l < r){
-            int m = nums[l] + nums[r];
-            if(m > k)
-                r--;
-            else if (m < k)
-                l++;
-            else{
-                ans.push_back({-k, nums[l++], nums[r--]});
-                while(l < r && nums[l] == nums[l-1]) l++;
-                while(l < r && nums[r] == nums[r+1]) r--;
-            }
-        }
-    
-    }
     vector<vector<int>> threeSum(vector<int>& nums) {
-        n = nums.size();
-        if(n < 3) return ans;
-
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < n; i++){
-            if(i == 0 || nums[i] != nums[i-1]){
-                twosum(nums, i);
-            }
-        }
+        int n = nums.size();
+        if(n < 3) return {};
         
+        sort(nums.begin(), nums.end()); //O(nlogn)
+        vector<vector<int>> ans;
+        
+        for(int i = 0; i < n && nums[i] <= 0; i++){
+            if(i == 0 || nums[i-1] != nums[i])
+                twosum(nums, i, ans);
+        }
+        //O(n^2)
         return ans;
+    }
+    
+    void twosum(vector<int>& nums, int k, vector<vector<int>>& ans){
+        unordered_set<int> seen;
+        for(int i = k+1; i < nums.size(); i++){
+            int c = - nums[i]-nums[k];
+            if(seen.count(c)){
+                ans.push_back({nums[k], c, nums[i]});
+                while(i < nums.size()-1 && nums[i] == nums[i+1])
+                    i++;
+            }
+            seen.insert(nums[i]);
+        }
     }
 };
