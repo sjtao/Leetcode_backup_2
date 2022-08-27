0caa89a6e15a2b9d80100258c44470b795625079
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-27 16:35:12 -0400

    Time: 6 ms (77.58%), Space: 7.9 MB (46.24%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> count(n+1, 0);
         for(int i = 1; i <= n; i++)
-            count[i] = count[i >> 1] + (i & 1);
+            count[i] = count[i & (i-1)] + 1;
         return count;
     }
 };
