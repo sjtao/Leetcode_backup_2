21877e9a566d094de6a7f2caa241bbf6a0956ff7
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-26 22:11:32 -0400

    Time: 0 ms (100.00%), Space: 7 MB (30.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -3,34 +3,33 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        int q = m * n;
-        vector<int> ans(q);
+        vector<int> spiral(m*n);
         
-        int k = 0;
         int left = 0, right = n-1;
         int top = 0, bottom = m-1;
-        while(k < q){
+        int k = 0;
+        while(k < m*n){
             for(int i = left; i <= right; i++)
-                ans[k++] = matrix[top][i];
+                spiral[k++] = matrix[top][i];
             top++;
             if(top > bottom) break;
             
             for(int i = top; i <= bottom; i++)
-                ans[k++] = matrix[i][right];
+                spiral[k++] = matrix[i][right];
             right--;
             if(right < left) break;
             
             for(int i = right; i >= left; i--)
-                ans[k++] = matrix[bottom][i];
+                spiral[k++] = matrix[bottom][i];
             bottom--;
             if(bottom < top) break;
             
             for(int i = bottom; i >= top; i--)
-                ans[k++] = matrix[i][left];
+                spiral[k++] = matrix[i][left];
             left++;
             if(left > right) break;
         }
         
-        return ans;
+        return spiral;
     }
 };
