b818c332cb4952bbf93acdec72a1533e797599e2
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-27 16:41:48 -0400

    Time: 0 ms (100.00%), Space: 5.8 MB (96.44%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t ans = 0;
-        uint32_t power = 31;
-        while(n){
-            ans += (n & 1) << power;
-            n = n >> 1;
-            power --;
+        uint32_t r = 0;
+        int k = 0;
+        while(k < 32){
+            if(k != 0) r <<= 1;
+            r += n & 1;
+            n >>= 1;
+            k++;
         }
-        return ans;
+        return r;
     }
 };
