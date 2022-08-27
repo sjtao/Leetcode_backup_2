11b4ef4e616cf22f0ad849bc367869eed1196755
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-26 22:55:06 -0400

    Time: 0 ms (100.00%), Space: 5.8 MB (96.76%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int one = 0;
         while(n){
-            one += (n & 1);
-            n >>= 1;
+            one ++;
+            n &= (n-1);
         }
         return one;
     }
