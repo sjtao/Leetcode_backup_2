cb578412e1530082b21da0c3883cc9a54258ab28
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-28 22:51:00 -0400

    Time: 8 ms (12.30%), Space: 8.7 MB (85.48%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int carry = 1;
-        int s = 0;
         int n = digits.size();
-        vector<int> sum;
-        for(int i = n-1; i >= 0; i--){
-            s = carry + digits[i];
-            sum.push_back(s % 10);
-            carry = s / 10;
+        
+        digits[n-1] += 1;
+        if(digits[n-1] < 10)
+            return digits;
+        
+        int carry = digits[n-1] / 10;
+        digits[n-1] %= 10;
+        
+        for(int i = n-2; i >= 0; i--){
+            digits[i] += carry;
+            carry = digits[i] / 10;
+            if(carry == 0)
+                return digits;
+            digits[i] %= 10;
         }
-        if(carry)
-            sum.push_back(carry);
         
-        reverse(sum.begin(), sum.end());
+        if(carry > 0)
+            digits.insert(digits.begin(), carry);
         
-        return sum;
+        return digits;
     }
 };
