7474881b6009ffa8ee1d83edda23f47aa248a76d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-08-30 19:24:49 -0400

    Time: 369 ms (51.43%), Space: 10.5 MB (44.52%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -5,15 +5,17 @@ public:
         vector<int> dp(n, 1);
         for(int i = 1; i < n; i++){
             for(int j = 0; j < i; j++){
-                if(nums[i] > nums[j])
-                    dp[i] = max(dp[i], dp[j]+1);
-            }          
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                }
+            }
         }
         
-        int lng = 1;
-        for(int d : dp)
-            lng = max(lng, d);
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            res = max(res, dp[i]);
+        }
         
-        return lng;
+        return res;
     }
 };
