c5c83b503b267fd00df9948886c894e81b13b742
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-09-04 14:29:10 -0400

    Time: 68 ms (8.99%), Space: 14.1 MB (41.55%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,57 +1,25 @@
 class Solution {
 public:
-    vector<int> root, rank;
-    
-    int find(int x){
-        while(x != root[x])
-            x = root[x];
-        return x;
-    }
-    
-    void unionset(int x, int y){
-        int rootx = find(x);
-        int rooty = find(y);
-        if(rootx != rooty){
-            if(rank[rootx] > rank[rooty])
-                root[rooty] = rootx;
-            else if(rank[rootx] < rank[rooty])
-                root[rootx] = rooty;
-            else{
-                root[rooty] = rootx;
-                rank[rootx]++;
-            }
-        }
-    }
-    
-    bool connected(int x, int y){
-        return find(x) == find(y);
-    }
-    
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        root = vector<int>(n);
-        rank = vector<int>(n, 0);
-        
+        vector<int> visited(n, 0);
+        int province = 0;
+        queue<int> q;
         for(int i = 0; i < n; i++){
-            root[i] = i;
-            rank[i] = 1;
-        }
-        
-        int province = n;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(isConnected[i][j]){
-                    int a = find(i);
-                    int b = find(j);
-                    if(a != b){
-                        root[a] = b;
-                        province--;
+            if(visited[i] == 0){
+                q.push(i);
+                while(!q.empty()){
+                    int s = q.front();
+                    q.pop();
+                    visited[s] = 1;
+                    for(int j = 0; j < n; j++){
+                        if(isConnected[s][j] && visited[j] == 0)
+                            q.push(j);
                     }
                 }
+                province++;
             }
         }
-        
         return province;
     }
 };
