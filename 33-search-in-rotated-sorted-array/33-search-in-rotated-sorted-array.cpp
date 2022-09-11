8557260bb8b2828df473278f2f540491f1e09836
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-09-10 21:46:03 -0400

    Time: 15 ms (5.75%), Space: 11.1 MB (29.32%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
         int l = 0;
-        int r = n-1;
+        int r = nums.size()-1;
         while(l <= r){
             int m = l + (r-l)/2;
             if(nums[m] == target)
                 return m;
-            else if(nums[m] >= nums[l]){
-                if(target >= nums[l] && target < nums[m])
+            else if(nums[m] > target){
+                if((target < nums[0] && nums[m] < nums[0]) || (target >= nums[0] && nums[m] >= nums[0]))
                     r = m-1;
-                else
-                    l = m+1;       
+                else if(target < nums[0] && nums[m] >= nums[0])
+                    l = m+1;
             }
             else{
-                if(target <= nums[r] && target > nums[m])
+                if((target < nums[0] && nums[m] < nums[0]) || (target >= nums[0] && nums[m] >= nums[0]))
                     l = m+1;
-                else
+                else if(target >= nums[0] && nums[m] < nums[0])
                     r = m-1;
             }
         }
