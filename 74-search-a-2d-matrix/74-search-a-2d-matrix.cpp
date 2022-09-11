053555b6988dda33a1de9cdada662ed85790de25
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-09-10 21:15:15 -0400

    Time: 8 ms (44.25%), Space: 9.5 MB (51.51%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -6,12 +6,14 @@ public:
         int l = 0, r = m*n-1;
         while(l <= r){
             int d = l + (r-l)/2;
-            if(matrix[d/n][d%n] == target)
+            int row = d / n;
+            int col = d % n;
+            if(matrix[row][col] == target)
                 return true;
-            else if(matrix[d/n][d%n] > target)
+            else if(matrix[row][col] > target)
                 r = d-1;
             else
-                l = d+1;
+                l = l+1;
         }
         return false;
     }
