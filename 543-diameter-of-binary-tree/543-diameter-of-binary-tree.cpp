022fcecc9063857577537e60a9aed5b6b674e85f
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-09-13 13:35:39 -0400

    Time: 14 ms (70.78%), Space: 20.2 MB (64.94%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -19,9 +19,8 @@ public:
         dia = max(dia, left + right);
         return 1 + max(left, right);
     }
-    
     int diameterOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
+        dia = 0;
         height(root);
         return dia;
     }
