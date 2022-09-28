6672c5bcf3caf6a421786879866da70ada15d98d
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-09-27 23:08:22 -0400

    Time: 12 ms, Memory: 7.2 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+        
         vector<int> v(26,0);
         
-        for(char c : s)
-            v[c-'a']++;
-        
-        for(char c : t)
-            v[c-'a']--;
+        for(int i = 0; i < s.length(); i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
         
         for(int i : v){
             if(i != 0)
