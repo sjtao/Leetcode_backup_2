b900684182bec6b19cbbe6fa8689ff15cf31e3f5
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-09-29 22:26:47 -0400

    Time: 93 ms (13.31%), Space: 90.2 MB (8.26%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int MN = 1000001;
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        //1. brutal O(m+n)
         int m = nums1.size();
         int n = nums2.size();
-        vector<int> num(m+n);
-        int i = 0, j = 0, k = 0;
-        while(k < m+n){
-            int a = i == m ? MN : nums1[i];
-            int b = j == n ? MN : nums2[j];
-            if(a < b){
-                num[k] = a;
-                i++;
-            }
-            else{
-                num[k] = b;
-                j++;
-            }
+        int t = (m+n)/2;
+        int r = (m+n)%2;
+        priority_queue<int, vector<int>, greater<int>> q;
+        
+        for(int i = 0; i < m; i++)
+            q.push(nums1[i]);
+        for(int i = 0; i < n; i++)
+            q.push(nums2[i]);
+        
+        int k = 0;
+        while(k < t-1+r){
+            q.pop();
             k++;
         }
-        double ans = 0;
-        if((m+n) % 2){
-            ans += num[(m+n)/2];
-        }
-        else{
-            ans += 0.5 * (num[(m+n)/2] + num[(m+n)/2-1]);
+        
+        int a = q.top();
+        q.pop();
+        int b = a;
+        if(r==0){
+            b = q.top();
+            q.pop();
         }
         
-        return ans;
+        return (a + b) / 2.0;
     }
 };
