930f1976c7b099ef748b0b5913e5ddc9c3e38581
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-10-30 21:15:01 -0400

    Time: 39 ms (21.35%), Space: 17.3 MB (85.40%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -4,16 +4,10 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        unordered_map<int, int> mp;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                int d = i-j;
-                if(mp.find(d) == mp.end())
-                    mp[d] = matrix[i][j];
-                else{
-                    if(matrix[i][j] != mp[d])
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] != matrix[i-1][j-1])
                         return false;
-                }
             }
         }
         
