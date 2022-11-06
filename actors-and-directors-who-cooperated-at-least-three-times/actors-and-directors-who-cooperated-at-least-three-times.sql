049048d1654c18e010b814e08aba73c38303a721
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-11-05 23:20:31 -0400

    Time: 488 ms, Memory: 0B - LeetHub

--- actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,7 +1,2 @@
 # Write your MySQL query statement below
-SELECT actor_id, director_id FROM (
-    SELECT actor_id, director_id, COUNT(CONCAT(actor_id, '-', director_id)) as cp
-    FROM ActorDirector
-    GROUP BY actor_id, director_id
-) as COOP
-WHERE cp >= 3;
+select actor_id,director_id from ActorDirector group by 1,2 having count(*)>=3;
