40319bee8a60952feb2ac6d211557c465b4c55ed
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-11-06 22:41:29 -0500

    Time: 778 ms (33.99%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT employee_id FROM Employees
+WHERE employee_id not in (SELECT employee_id FROM Salaries)
+UNION
+SELECT employee_id FROM Salaries
+WHERE employee_id not in (SELECT employee_id FROM Employees)
+ORDER BY 1 ASC;
