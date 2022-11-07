1b4c84b596452c539eddd8eb0bcb07d58175a2c6
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-11-06 20:38:39 -0500

    Time: 0 ms (100.00%), Space: 6.1 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s = to_string(num);
-        for(char& c:s){
-            if(c == '6'){
-                c = '9';
-                break;
-            }
+        int notnine = -1;
+        int n = num;
+        int d = 0;
+        while(n){
+            if(n % 10 != 9)
+                notnine = d;
+            n /= 10;
+            d++;
         }
-        num = stoi(s);
-        return num;
+        
+        return notnine == -1 ? num : num + pow(10, notnine) * 3;
     }
 };
