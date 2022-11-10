80099a4a31d32a5d0e43dfd903820868fc2f2a0a
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-11-09 21:59:35 -0500

    Time: 549 ms (74.35%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-SELECT employee_id FROM Employees
-WHERE employee_id not in (SELECT employee_id FROM Salaries)
+SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
 UNION
-SELECT employee_id FROM Salaries
-WHERE employee_id not in (SELECT employee_id FROM Employees)
+SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
 ORDER BY 1 ASC;
