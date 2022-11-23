85060b29e09e9d3c80030e949527705ad40c52a0
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-11-22 21:13:13 -0500

    Time: 53 ms (38.01%), Space: 19.6 MB (45.17%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<int>> row(9, vector<int>(9,0));
-        vector<vector<int>> col(9, vector<int>(9,0));
-        vector<vector<int>> box(9, vector<int>(9,0));
+        vector<vector<bool>> row(9, vector<bool>(9,false));
+        vector<vector<bool>> col(9, vector<bool>(9,false));
+        vector<vector<bool>> box(9, vector<bool>(9,false));
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 if(board[i][j] != '.'){
                     int x = board[i][j] - '1'; //board number: 1-9
                     int b = (i / 3) * 3 + (j / 3);
-                    if(row[i][x] != 0 || col[j][x] != 0 || box[b][x] != 0)
+                    if(row[i][x] || col[j][x] || box[b][x])
                         return false;
-                    row[i][x]++;
-                    col[j][x]++;
-                    box[b][x]++;
+                    row[i][x] = true;
+                    col[j][x] = true;
+                    box[b][x] = true;
                 }
             }
         }
