6c3ea65c0e7060ec68f77ae6195c8428d27979ee
sjtao98 <61641368+sjtao98@users.noreply.github.com>
2022-11-30 21:52:47 -0500

    Time: 10 ms (23.88%), Space: 6.8 MB (16.35%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    unordered_set<char> st = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    
+    bool halvesAreAlike(string s) {
+        int n = s.length() / 2;
+        int first = 0, second = 0;
+        for(int i = 0; i < n; i++){
+            if(st.find(s[i]) != st.end())
+                first++;
+            if(st.find(s[i+n]) != st.end())
+                second++;
+        }
+        return first == second;
+    }
+};
